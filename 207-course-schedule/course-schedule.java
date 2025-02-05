9987261beb671af7e0fde98863197d6a4bc4f3ad
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-05 12:56:02 -0500

    Time: 6 ms (74.52%) | Memory: 46 MB (23.80%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,66 +1,54 @@
-
-class Pair { 
-    int first;
-    int second;
-
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
 
-    //start by creating adj map using adj list
-    HashMap<Integer, List<Integer>> preMap = new HashMap<>();
-
-    //maintain a visited set
+    //declare map to store prereq and visited set
+    Map<Integer, List<Integer>> preMap = new HashMap<>();
     Set<Integer> visited = new HashSet<>();
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
-        //using DFS to detect cycles
 
+        //fill up preMap with empty lists (to ensure empty courses also have lists)
         for(int i = 0; i < numCourses; i++){
+
             preMap.put(i, new ArrayList<>());
+
         }
 
-        //for each prereq, add req course for each course into the map
+        //fill up preMap with prereqs
         for(int[] prereq : prerequisites){
 
             preMap.get(prereq[0]).add(prereq[1]);
 
         }
 
-        //cycle through courses, run dfs and if any returns false, return false
-        for(int i = 0; i < numCourses; i++){
+        //call dfs
+        for(int i = 0 ; i < numCourses; i++){
 
-            if(!dfs(i)){
-                return false;
-            }
-
+            if(!dfs(i)) return false;
+            
         }
+
         return true;
+ 
     }
 
     public boolean dfs(int course){
 
-        //check if course is visited along dfs path, if it is return false
+        //if course is visited, false
         if(visited.contains(course)) return false;
 
-        //check if preMap contains any prereqs, if its empty, return true
+        //if course is in map with empty prereq, true
         if(preMap.get(course).isEmpty()) return true;
 
-        //add course to visited and cisit each of its prereqs
+        //visited node and go through its prereqs
         visited.add(course);
 
-        for(int pre : preMap.get(course)){
+        for(int prereq : preMap.get(course)){
 
-            if(!dfs(pre)) return false;
+            if(!dfs(prereq)) return false;
 
-        }
+        };
 
-        //backtracking
+        //backtrack and return
         visited.remove(course);
         preMap.put(course, new ArrayList<>());
         return true;
