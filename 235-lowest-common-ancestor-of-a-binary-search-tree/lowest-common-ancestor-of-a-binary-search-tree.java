d3bf08cb4a2e7002cef3567d05cae1c0657b821c
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-06 23:06:59 -0500

    Time: 5 ms (100.00%) | Memory: 45.6 MB (9.26%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -11,22 +11,33 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        //approach -> use a tracker node, go left if both are lower and right if both are greater
-        TreeNode currNode = root;
+        // //approach1 -> use a tracker node, go left if both are lower and right if both are greater
+        // TreeNode currNode = root;
 
-        while(currNode != null){
-            if(p.val > currNode.val && q.val > currNode.val){
-                currNode = currNode.right;
-            }
+        // while(currNode != null){
+        //     if(p.val > currNode.val && q.val > currNode.val){
+        //         currNode = currNode.right;
+        //     }
 
-            else if(p.val < currNode.val && q.val < currNode.val){
-                currNode = currNode.left;
-            }
-            else{
-                return currNode;
-            }
+        //     else if(p.val < currNode.val && q.val < currNode.val){
+        //         currNode = currNode.left;
+        //     }
+        //     else{
+        //         return currNode;
+        //     }
+        // }
+
+        // return null;
+
+        //approach 2 -> recursive approach; recursively call on right/left based on condition and return
+        if(p.val > root.val && q.val > root.val){
+            return lowestCommonAncestor(root.right, p, q);
         }
 
-        return null;
+        if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        return root;
     }
 }
