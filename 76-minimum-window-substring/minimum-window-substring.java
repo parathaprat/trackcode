ad2c53b41adba40b199600d1db5cea141894306a
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-06 20:43:11 -0500

    Time: 19 ms (53.16%) | Memory: 45.5 MB (29.52%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,97 +1,58 @@
 class Solution {
     public String minWindow(String s, String t) {
 
-        // if(t.isEmpty()) return "";
+        if(t.isEmpty()) return "";
         
-        // //maintain countT and window maps
-        // Map<Character, Integer> countT = new HashMap<>();
-        // Map<Character, Integer> window = new HashMap<>();
-
-        // for(char c : t.toCharArray()){
-
-        //     countT.put(c, countT.getOrDefault(c, 0) + 1);
-        // }
-
-        // //have, need, res boundaries, resLen, l and r pointers
-        // int have = 0;
-        // int need = countT.size();
-
-        // int[] res = {-1, -1};
-        // int resLen = Integer.MAX_VALUE;
-        // int l = 0;
-
-        // for(int r = 0; r < s.length(); r++){
-
-        //     //getting char and updating freq in window map
-        //     char c = s.charAt(r);
-        //     window.put(c, window.getOrDefault(c, 0) + 1);
-            
-        //     //have condition
-        //     if(countT.containsKey(c) && window.get(c) == countT.get(c)) have++;
-
-        //     while(have == need){
-
-        //         if(r - l + 1 < resLen){
-        //             //update res and resLen when have and need are equal and new resLen is less than old
-        //             res[0] = l;
-        //             res[1] = r;
-
-        //             resLen = r - l + 1;
-        //         }
-
-        //         //work on reducing window size from the left -> reduce from window, l++
-        //         char leftChar = s.charAt(l);
-        //         window.put(leftChar, window.get(leftChar) - 1);
-
-        //         //reduce have IF window count of char goes below countT count of char
-        //         if(countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) have--;
-
-        //         l++;
-    
-        //     }
-
-        // }
-
-        // return resLen == Integer.MAX_VALUE? "" : s.substring(res[0], res[1] + 1);
-
-        if (t.isEmpty()) return "";
-
+        //maintain countT and window maps
         Map<Character, Integer> countT = new HashMap<>();
         Map<Character, Integer> window = new HashMap<>();
-        for (char c : t.toCharArray()) {
+
+        for(char c : t.toCharArray()){
+
             countT.put(c, countT.getOrDefault(c, 0) + 1);
         }
 
-        int have = 0, need = countT.size();
+        //have, need, res boundaries, resLen, l and r pointers
+        int have = 0;
+        int need = countT.size();
+
         int[] res = {-1, -1};
         int resLen = Integer.MAX_VALUE;
         int l = 0;
 
-        for (int r = 0; r < s.length(); r++) {
+        for(int r = 0; r < s.length(); r++){
+
+            //getting char and updating freq in window map
             char c = s.charAt(r);
             window.put(c, window.getOrDefault(c, 0) + 1);
+            
+            //have condition
+            if(countT.containsKey(c) && window.get(c).equals(countT.get(c))) have++;
 
-            if (countT.containsKey(c) && window.get(c).equals(countT.get(c))) {
-                have++;
-            }
+            while(have == need){
 
-            while (have == need) {
-                if ((r - l + 1) < resLen) {
-                    resLen = r - l + 1;
+                if(r - l + 1 < resLen){
+                    //update res and resLen when have and need are equal and new resLen is less than old
                     res[0] = l;
                     res[1] = r;
+
+                    resLen = r - l + 1;
                 }
 
+                //work on reducing window size from the left -> reduce from window, l++
                 char leftChar = s.charAt(l);
                 window.put(leftChar, window.get(leftChar) - 1);
-                if (countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) {
-                    have--;
-                }
+
+                //reduce have IF window count of char goes below countT count of char
+                if(countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) have--;
+
                 l++;
+    
             }
+
         }
 
-        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
+        return resLen == Integer.MAX_VALUE? "" : s.substring(res[0], res[1] + 1);
 
     }
 }
