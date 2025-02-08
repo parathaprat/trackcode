3a544c0a51c741a24ebf8fc4cd9672e05768562e
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-08 00:30:01 -0500

    Time: 3 ms (86.50%) | Memory: 49.5 MB (70.39%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,35 +1,34 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        
-        int count = 0;
 
-        //go through each unit and ++ count and call BFS if land occurs
+    int count = 0;
+
+    public int numIslands(char[][] grid) {
+
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
 
                 if(grid[i][j] == '1'){
                     count++;
-                    BFS(i, j, grid);
+                    countLand(grid, i, j);
                 }
-
             }
         }
 
         return count;
+        
     }
 
-    //main logic
-    public void BFS(int i, int j, char[][] grid){
+    private void countLand(char[][] grid, int i, int j){
 
-        //bounds, and if i,j = 0
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
 
-        //mark land as water
         grid[i][j] = '0';
 
-        BFS(i + 1, j, grid);
-        BFS(i - 1, j, grid);
-        BFS(i, j + 1, grid);
-        BFS(i, j - 1, grid);
+        
+            countLand(grid, i + 1, j);
+            countLand(grid, i - 1, j);
+            countLand(grid, i, j + 1);
+            countLand(grid, i, j - 1);
+     
     }
 }
