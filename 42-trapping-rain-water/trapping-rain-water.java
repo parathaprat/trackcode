b786c330ee7719a49ac84d8c30496196d409a80e
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-09 13:01:16 -0500

    Time: 0 ms (100.00%) | Memory: 46.5 MB (48.17%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,32 +1,32 @@
 class Solution {
     public int trap(int[] height) {
         
-        if(height.length == 0) return 0;
+        //main logic -> Min(Max(left, right)) - height
 
-        int left = 0;
+        int left = 0; 
         int right = height.length - 1;
-
-        int finalAns = 0;
-
+        
         int leftMax = height[left];
         int rightMax = height[right];
 
-        //smaller boundry is determining factor, so move the pointer with the smaller max
+        int ans = 0;
+
         while(left < right){
 
             if(leftMax < rightMax){
+
                 left++;
                 leftMax = Math.max(leftMax, height[left]);
-                finalAns += leftMax - height[left];
+                ans += leftMax - height[left];
             }
-
             else{
+
                 right--;
                 rightMax = Math.max(rightMax, height[right]);
-                finalAns += rightMax - height[right];
+                ans += rightMax - height[right];
             }
         }
 
-        return finalAns;
+        return ans;
     }
 }
