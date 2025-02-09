7982f066f97be7febfe649a59d61273a813d490e
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-09 13:18:31 -0500

    Time: 7 ms (16.40%) | Memory: 41.6 MB (74.84%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,53 +1,64 @@
 class Solution {
     public String reorganizeString(String s) {
         
-        //Map and maxHeap appraoch
-
+        //frequency map
         Map<Character, Integer> map = new HashMap<>();
+
+        //maxHeap to sort by freq
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+
+        //stringbuilder
+        StringBuilder result = new StringBuilder();
+
+        //fill map and heap
 
         for(char c : s.toCharArray()){
 
             map.put(c, map.getOrDefault(c, 0) + 1);
-
         }
-
-        //mapHeap to sort added chars in decending order of thier frequency
-        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
 
         maxHeap.addAll(map.keySet());
 
-        //now we have all chars sorted by their freq in our maxHeap 
-
-        StringBuilder sb = new StringBuilder();
-
+        //now we have a heap with letter sorted by freq
         while(maxHeap.size() >= 2){
-
+            
             char c1 = maxHeap.poll();
             char c2 = maxHeap.poll();
 
-            sb.append(c1);
-            sb.append(c2);
+            result.append(c1);
+            result.append(c2);
 
-            //update frequencies of chars
-            map.put(c1, map.get(c1) - 1);
-            map.put(c2, map.get(c2) - 1);
+            if(map.get(c1) != 0){
+                map.put(c1, map.get(c1) - 1);
 
-            //add them back if freqs are > 0
-            if(map.get(c1) > 0) maxHeap.add(c1);
-            if(map.get(c2) > 0) maxHeap.add(c2);
+                if(map.get(c1) != 0) maxHeap.add(c1);
+            }
+
+            if(map.get(c2) != 0){
+                map.put(c2, map.get(c2) - 1);
+
+                if(map.get(c2) != 0) maxHeap.add(c2);
+            } 
 
         }
 
-        //to process that one remaining char
-        if(maxHeap.size() != 0){
+        if(!maxHeap.isEmpty()){
 
-            char ch = maxHeap.poll();
-            if(map.get(ch) > 1) return "";
-
-            sb.append(ch);
+            if(maxHeap.size() != 1){
+                return " ";
+            } 
+            else{
+                char c = maxHeap.poll();
+                if(map.get(c) > 1) return "";
+                result.append(c);
+            }
         }
 
-        return sb.toString();
+
+
+        return result.toString();
+
+
 
     }
 }
