d968334048034a2b54171d4de540d1b73f6aca99
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-09 12:44:30 -0500

    Time: 53 ms (22.55%) | Memory: 114.8 MB (52.35%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,66 +1,74 @@
 class LRUCache {
 
-    // main logic -> Doubly LL + HashMap (key, adress)
+    //DLL + Map implementation
 
-    //define node class
-    //define head and tail nodes + capacity
-    //define insert and delete functions
     class Node{
 
-        Node prev, next;
-        int key, value;
+        Node next;
+        Node prev;
 
-        Node(int key, int value){
+        int key;
+        int val;
+
+        Node(int key, int val){
             this.key = key;
-            this.value = value;
+            this.val = val;
         }
     }
 
-    Map< Integer, Node> map = new HashMap<>();
+    Map<Integer, Node> map = new HashMap<>();
     int capacity;
 
     Node head = new Node(0, 0);
     Node tail = new Node(0, 0);
 
-    //make head and tail point to each other; initialize LRU cache
     public LRUCache(int capacity) {
-        
+
         this.capacity = capacity;
         head.next = tail;
         tail.prev = head;
-
+        
     }
     
-    //check if it exists in map, if it does, obtain node, remove and insert in DLL & return node.val
     public int get(int key) {
-        
+
+
         if(map.containsKey(key)){
 
-            Node node  = map.get(key);
+            Node node = map.get(key);
+
             remove(node);
             insert(node);
-            return node.value;
+           
+            return node.val;
+
         }
+
         else{
             return -1;
         }
+        
     }
     
-    //if it already exists, remove before insert; if capacity is full, remove node before tail
+    //if cap reached, evict tail.prev
     public void put(int key, int value) {
-        
-        if(map.containsKey(key)){
-            remove(map.get(key));
-        }
 
-        if(map.size() == capacity){
+        //if that key exists, remove first and then add
+        if(map.containsKey(key)){
+
+            Node node = map.get(key);
+            remove(node);
+
+        }
+        else if(map.size() == capacity){
+
             remove(tail.prev);
+
         }
 
         insert(new Node(key, value));
     }
 
-    //add to map, make connections
     public void insert(Node node){
 
         map.put(node.key, node);
@@ -69,10 +77,9 @@ class LRUCache {
         node.next.prev = node;
         head.next = node;
         node.prev = head;
-        
+
     }
 
-    //remove from map, adjust connections
     public void remove(Node node){
 
         map.remove(node.key);
