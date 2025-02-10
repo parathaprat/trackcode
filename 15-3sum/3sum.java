ecab08edcefb26ed26e9fb27200a42f5d688f917
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-09 21:52:15 -0500

    Time: 30 ms (68.20%) | Memory: 51.8 MB (45.24%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,43 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
-        //sort the array, for each num, find complement and run 2sum
-
         Arrays.sort(nums);
-        List<List<Integer>> output = new LinkedList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
 
         for(int i = 0; i < nums.length - 2; i++){
 
-            if(i == 0 || i > 0 && nums[i] != nums[i - 1]){
+            if(i == 0 || nums[i] != nums[i - 1]){
 
-                //target = sum, usign 2pointer approach for 2sum
-                int low = i + 1;
-                int high = nums.length - 1;
                 int sum = 0 - nums[i];
+                int left = i + 1;
+                int right = nums.length - 1;
 
-                while(low < high){
+                while(left < right){
 
-                    if(nums[low] + nums[high] == sum){
-                        output.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    if(nums[left] + nums[right] == sum){
+                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                        while(low < high && nums[low] == nums[low + 1]) low++;
-                        while(low < high && nums[high] == nums[high - 1]) high--;
+                        while(left < right && nums[left] == nums[left + 1]) left++;
+                        while(left < right && nums[right] == nums[right - 1]) right--;
 
-                        low++;
-                        high--;
+                        left++;
+                        right--;
                     }
-                    else if(nums[low] + nums[high] > sum){
-                        high--;
+                    else if(nums[left] + nums[right] > sum){
+                        right--;
                     }
-                    else{
-                        low++;
+                    else if(nums[left] + nums[right] < sum){
+                        left++;
                     }
-
                 }
-
             }
+
         }
 
-        return output;
+        return result;
     }
 }
