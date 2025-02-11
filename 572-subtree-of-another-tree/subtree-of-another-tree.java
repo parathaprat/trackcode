2e655572731218199006f1f2151a63416134d4f7
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-11 12:22:08 -0500

    Time: 3 ms (22.55%) | Memory: 44.3 MB (81.78%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -16,13 +16,10 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
 
-        //base -> if root is null, direct return false
         if(root == null) return false;
-        if(isSameTree(root, subRoot)) return true;
+        
+        return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
 
-        //give tree is not a guaranteed BST, so we will have to explore all nodes in root recursively
-        //recursively call this function on right and left so they get checked
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
     public boolean isSameTree(TreeNode p, TreeNode q){
@@ -31,9 +28,10 @@ class Solution {
         if(p != null && q == null) return false;
         if(p == null && q != null) return false;
 
-        if(p.val != q.val) return false;
+        if(p.val != q.val ) return false;
 
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
 
     }
 }
