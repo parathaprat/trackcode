abc793a80184417bbedcd9c2d280502ab8e963bb
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-11 12:10:11 -0500

    Time: 0 ms (100.00%) | Memory: 44.2 MB (76.78%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -15,38 +15,27 @@
  */
 class Solution {
 
-    //globally declare sum variable
-    int maxPathSum;
+    int sum = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
-
-        //assign smallest value to sum
-        maxPathSum = Integer.MIN_VALUE;
-
+        
         getPathSum(root);
 
-        return maxPathSum;
-        
+        return sum;
     }
 
     public int getPathSum(TreeNode node){
 
-        //base -> if node is null, path sum = 0
         if(node == null) return 0;
 
-        //path sum of subtrees may be -ve, so max bw getPathSum and 0
         int leftSum = Math.max(0, getPathSum(node.left));
         int rightSum = Math.max(0, getPathSum(node.right));
 
-        //now we have the maxPathSum of left and right subtrees -> 
-        //cases -> breaks at node and does not break at node
+        //2cases -> node involved, node not involved
+
+        sum = Math.max(sum, node.val + leftSum + rightSum);
         
-        //breaks at node -> left + right + node; also update maxPathSum
-        maxPathSum = Math.max(maxPathSum, leftSum + rightSum + node.val);
-
-        //return node.val + max(left, right)
         return node.val + Math.max(leftSum, rightSum);
+
     }
-
-
 }
