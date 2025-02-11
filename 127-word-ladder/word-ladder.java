ac5b376114f77d756cab089d0f30bb8bd0025360
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-10 20:28:22 -0500

    Time: 166 ms (28.67%) | Memory: 46.8 MB (53.35%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -2,22 +2,15 @@ class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         
         if(!wordList.contains(endWord) || beginWord.equals(endWord)) return 0;
-        Set<String> set = new HashSet<>();
-
-        for(String word : wordList){
-
-            set.add(word);
-        }
-
-        //loop through each letter of beginWord
-        //replace with each letter of the alphabet
-        //check if this new word is in our wordList
-        //return min
 
         int ans = 0;
 
-        //queue to do BFS -> to find shortest path
+        //BFS -> the shortest path -> Set(wordList), queue to iterate 
+
+        Set<String> set = new HashSet<>(wordList);
+
         Queue<String> q = new LinkedList<>();
+
         q.add(beginWord);
 
         while(!q.isEmpty()){
@@ -26,32 +19,30 @@ class Solution {
 
             int level = q.size();
 
-            for(int j = 0; j < level; j++){
+            for(int i = 0; i < level; i++){
 
                 String word = q.poll();
 
                 if(word.equals(endWord)) return ans;
 
-                for(int i = 0; i < word.length(); i++){
-
+                for(int j = 0; j < word.length(); j++){
                     for(char c = 'a'; c <= 'z'; c++){
 
-                        if(c == word.charAt(i)) continue;
+                        //skip cus this will be in the set
+                        if(c == word.charAt(j)) continue;
 
-                        String nei = word.substring(0, i) + c + word.substring(i + 1);
+                        String nei = word.substring(0, j) + c + word.substring(j + 1);
+
                         if(set.contains(nei)){
+
                             set.remove(nei);
                             q.add(nei);
                         }
-
                     }
-
-
                 }
-            }
+            }   
         }
 
         return 0;
-
     }
 }
