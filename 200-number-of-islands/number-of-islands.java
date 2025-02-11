d44806471eb08b5c86662dfdc15be5724d9aa435
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-11 13:28:26 -0500

    Time: 3 ms (86.63%) | Memory: 49.9 MB (39.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,34 +1,32 @@
 class Solution {
-
-    int count = 0;
-
     public int numIslands(char[][] grid) {
+        
+        int ans = 0;
 
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
 
                 if(grid[i][j] == '1'){
-                    count++;
+                    ans++;
                     countLand(grid, i, j);
                 }
             }
         }
 
-        return count;
-        
+        return ans;
     }
 
-    private void countLand(char[][] grid, int i, int j){
+    public void countLand(char[][] grid, int i, int j){
 
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0'){
+            return;
+        }
 
         grid[i][j] = '0';
 
-        
-            countLand(grid, i + 1, j);
-            countLand(grid, i - 1, j);
-            countLand(grid, i, j + 1);
-            countLand(grid, i, j - 1);
-     
+        countLand(grid, i + 1, j);
+        countLand(grid, i - 1, j);
+        countLand(grid, i, j + 1);
+        countLand(grid, i, j - 1);
     }
 }
