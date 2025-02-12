f7c47c93a14cf4ca205559ab86f3672b5dbc866c
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-12 16:10:01 -0500

    Time: 1 ms (89.76%) | Memory: 44.8 MB (94.30%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -16,54 +16,35 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         
-        //q and list required
-        Queue<TreeNode> q = new LinkedList<>();
-        List<List<Integer>> ansList = new ArrayList<>();
+        //Simple BFS
+    
+        List<List<Integer>> res = new ArrayList<>();
 
-        if(root == null) return new ArrayList<>();
+        if(root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
 
         q.add(root);
 
-        getLevelOrd(q, ansList);
-
-        return ansList;
-
-    }
-
-    public void getLevelOrd(Queue<TreeNode> q, List<List<Integer>> ansList){
-
         while(!q.isEmpty()){
 
-            //maintain level size for popping
-            int levelSize = q.size();
+            int level = q.size();
 
-            //list gets reset at every iteration/ level
-            List<Integer> thisLevel = new ArrayList<>();
+            List<Integer> ans = new ArrayList<>();
 
-            for(int i = 0; i < levelSize; i++){
+            for(int i = 0; i < level; i++){
 
-                //get nodes at that level and add to list
                 TreeNode node = q.poll();
+                ans.add(node.val);
 
-                thisLevel.add(node.val);
-
-                //if left and right are not null, add to q
-                if(node.left != null){
-                    q.add(node.left);
-                }
-
-                if(node.right != null){
-                    q.add(node.right);
-                }
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
 
             }
 
-            //add that particular level to final list
-            ansList.add(thisLevel);
+            res.add(ans);
         }
+
+        return res;
     }
-
-
-
-
 }
