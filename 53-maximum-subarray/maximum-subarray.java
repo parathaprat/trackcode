84e4a37b497ee9d99a1fe8b2923d047cf913a4ff
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-11 20:04:22 -0500

    Time: 1 ms (99.47%) | Memory: 56.9 MB (80.26%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,23 +1,24 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         
-        //maintain max and sum variables
-        int max = Integer.MIN_VALUE;
+        //even if there is huge number on the left, it cant be picked it it gets -gated
+        //so update sum = 0, each time the linear sum <= 0
+        //also track maxSum, at each iteration, since sum can be smaller in the +ve range
+
 
         int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
 
-        for(int i = 0 ; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++){
 
-            //add elements to sum and update max if greater
             sum += nums[i];
 
-            if(sum > max) max = sum;
+            maxSum = Math.max(sum, maxSum);
 
-            //if sum ever goes <0, update to 0 [handles cases where -ve numbers start]
-            if(sum < 0) sum = 0;
+            if(sum <= 0) sum = 0;
 
         }
 
-        return max;
+        return maxSum;
     }
 }
