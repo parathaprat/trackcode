9b3fbadea0be33314101db8b9a03eb99be3be7b3
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-13 16:30:51 -0500

    Time: 7 ms (50.75%) | Memory: 46 MB (18.47%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,17 +1,17 @@
 class Solution {
 
-    //make a prereq map
     Map<Integer, List<Integer>> map = new HashMap<>();
-        
-    //make a visited set
     Set<Integer> visited = new HashSet<>();
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        //detect loop in graph
+
+        //adj map, visited
 
         for(int i = 0; i < numCourses; i++){
 
             map.put(i, new ArrayList<>());
-    
         }
 
         for(int[] pre : prerequisites){
@@ -19,29 +19,25 @@ class Solution {
             map.get(pre[0]).add(pre[1]);
         }
 
-        //go through each course and check for cycles
-
         for(int i = 0; i < numCourses; i++){
 
             if(!dfs(i)) return false;
+
         }
 
         return true;
-
-
     }
 
     private boolean dfs(int course){
 
         if(visited.contains(course)) return false;
 
-        if(map.get(course).isEmpty()) return true;
-
         visited.add(course);
 
         for(int pre : map.get(course)){
 
             if(!dfs(pre)) return false;
+
         }
 
         visited.remove(course);
