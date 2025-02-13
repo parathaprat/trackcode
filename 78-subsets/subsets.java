d121d25d839eeba16a1f168b7fa5c1fd07ebb386
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-12 19:47:35 -0500

    Time: 0 ms (100.00%) | Memory: 42.9 MB (46.58%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> set = new ArrayList<>();
+
+        dfs(nums, 0, res, set);
+
+        return res;
+    }
+
+    private void dfs(int[] nums, int index, List<List<Integer>> res, List<Integer> set){
+
+        //reached the end for a particular subset
+        if(index >= nums.length){
+
+            res.add(new ArrayList<>(set));
+            return;
+        }
+
+        //current element -> part of the subset
+        set.add(nums[index]);
+        dfs(nums, index + 1, res, set);
+
+        //current element -> not part of the subset
+        set.remove(set.size() - 1);
+        dfs(nums, index + 1, res, set);
+    }
+}
