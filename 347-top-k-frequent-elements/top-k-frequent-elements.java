e969a7b2e397563cba269f59377249c734634932
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-13 22:31:58 -0500

    Time: 11 ms (91.33%) | Memory: 49.9 MB (6.30%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,27 +1,54 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-    
-        Map<Integer, Integer> map = new HashMap<>();
+        
+        //HashMap for freq
+        //Array of Lists for bucks
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        //bucket sort -> each index reps count, and each count holds a list of elements with that count
+
+        //freq map
+        Map<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < nums.length; i++){
 
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
         }
 
-        maxHeap.addAll(map.keySet());
+        //buckets -> Array of Lists
+        List<Integer>[] bucks = new ArrayList[nums.length + 1];
+
+        //initialise each buck
+        for(int i = 0; i <= nums.length; i++){
+
+            bucks[i] = new ArrayList<>();
+        }
+
+        //populating bucks using map
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+
+            int num = entry.getKey();
+            int freq = entry.getValue();
+
+            bucks[freq].add(num);
+        }
+
+        //populating ans but looking at elements with highest freqs
+
+        int index = 0;
 
         int[] ans = new int[k];
 
-        for(int i = 0; i < k; i++){
+        for(int i = bucks.length - 1; i >= 0; i--){
 
-            ans[i] = maxHeap.poll();
+            for(int num : bucks[i]){
+                ans[index] = num;
+                index++;
 
+                if(index == k) return ans;
+            }
         }
 
         return ans;
-
-
     }
 }
