6868a4c01f48ee7b4f0714fe7bebeb30a5a1ee60
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-13 20:57:06 -0500

    Time: 0 ms (100.00%) | Memory: 42.5 MB (26.33%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,24 +1,33 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        //logic -> merge from the back
+        if(n == 0) return;
+        
+
         int p1 = m - 1;
         int p2 = n - 1;
-        int i = m + n - 1;
+
+        int ans = m + n - 1;
+
+        while(p1 >= 0 && p2 >= 0){
+
+            if(nums1[p1] > nums2[p2]){
+                nums1[ans] = nums1[p1];
+                p1--;
+            }
+            else{
+                nums1[ans] = nums2[p2];
+                p2--;
+            }
+
+            ans--;
+        }
 
         while(p2 >= 0){
 
-            if(p1 >= 0 && nums1[p1] > nums2[p2]){
-                nums1[i] = nums1[p1];
-                p1--;
-                i--;
-            }
-
-            else{
-                nums1[i] = nums2[p2];
-                p2--;
-                i--;
-            }
+            nums1[ans] = nums2[p2];
+            p2--;
+            ans--;
         }
     }
 }
