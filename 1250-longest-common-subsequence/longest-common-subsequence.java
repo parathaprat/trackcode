176bac66d7e2905bd8a64cb61e632e5f54a41626
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-14 21:31:17 -0500

    Time: 19 ms (88.01%) | Memory: 50.9 MB (66.15%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,32 +1,23 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         
-        //get length of both strings
-        int n = text1.length();
-        int m = text2.length();
+        //bottom up dp approach
+        //if char at i and j are equal, dp[i][j] = 1 + dp[i + 1][j + 1];
+        //if char at i and j !=, dp[i] = Math.max(dp[i][j +1], dp[i + 1][j]);
 
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
 
-        for(int[] row : dp) Arrays.fill(row, -1);
+        //last row and last col == 0, since there are 0 LCS bw the other string and nothing
 
-        return getAns(n - 1, m - 1, text1, text2, dp);
+        for(int i = text1.length() - 1; i >= 0; i--){
+            for(int j = text2.length() - 1; j >= 0; j--){
 
-    }
-
-    public int getAns(int n, int m, String text1, String text2, int[][] dp){
-
-        //base case
-        if(n < 0 || m < 0) return 0;
-
-        if(dp[n][m] != -1) return dp[n][m];
-
-        //case where char is same
-        if(text1.charAt(n) == text2.charAt(m)){
-            return dp[n][m] = 1 + getAns(n - 1, m - 1, text1, text2, dp);
-        }
-        else{
-            return dp[n][m] = Math.max(getAns(n - 1, m, text1, text2, dp), getAns(n, m - 1, text1, text2, dp));
+                if(text1.charAt(i) == text2.charAt(j)) dp[i][j] = 1 + dp[i + 1][j + 1];
+                else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                
+            }
         }
 
+        return dp[0][0];
     }
 }
