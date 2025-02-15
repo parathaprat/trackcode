29b0b61aab96bd6fd347ff659a5b455e3879d3a1
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-15 18:32:04 -0500

    Time: 42 ms (52.43%) | Memory: 44 MB (93.29%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,35 +1,35 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         
-        //bottom up
-        //LIS at last element = 1;
-        //is cur < other elemetns after; dp[cur] = Math.max(dp[cur]. 1 + dp[other elements])
+        //brute force bottom up dp
+        //LIS at last index => 1
+        //dp[i] => LIS starting at index i
 
         int[] dp = new int[nums.length];
-
         Arrays.fill(dp, 1);
 
+        //go through entire array from the back
         for(int i = nums.length - 1; i >= 0; i--){
 
+            //iterate from i to end and get LIS for that index
             for(int j = i + 1; j < nums.length; j++){
 
                 if(nums[i] < nums[j]){
 
                     dp[i] = Math.max(dp[i], 1 + dp[j]);
-
                 }
             }
         }
 
-        //each dp shows LIS at each index, return max
-
+        //return max of dp array
         int ans = 0;
-
         for(int num : dp){
             
             if(num > ans) ans = num;
         }
 
         return ans;
+
+
     }
 }
