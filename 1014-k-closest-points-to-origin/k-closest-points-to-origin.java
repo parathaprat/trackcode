05e956ea64732f39a072c09c61cfaf97d8df40da
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-15 14:59:33 -0500

    Time: 21 ms (90.47%) | Memory: 47.8 MB (97.10%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         
-        //Priority Queue that sorts the points in ascending order from distance from origin
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> getDist(a) - getDist(b));
-
-        for(int[] ptSet : points){
-            minHeap.add(ptSet);
-        }
 
         int[][] ans = new int[k][2];
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> getDist(a) - getDist(b));
+
+        for(int[] pt : points){
+            minHeap.add(pt);
+        }
 
         for(int i = 0; i < k; i++){
             ans[i] = minHeap.poll();
@@ -16,13 +16,10 @@ class Solution {
 
         return ans;
 
-
     }
 
     private int getDist(int[] pt){
 
         return pt[0] * pt[0] + pt[1] * pt[1];
     }
-
-
 }
