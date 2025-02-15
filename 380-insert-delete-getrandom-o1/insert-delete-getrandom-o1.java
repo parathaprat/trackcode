5f72f6b1cd48af158df28063c2abf9b0b8a39e54
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-15 16:12:40 -0500

    Time: 35 ms (36.19%) | Memory: 90.3 MB (87.83%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,8 +1,6 @@
 class RandomizedSet {
 
-    //we will use a map and a Arraylist to store our elements
-
-    //{Node, Index in Array}
+    //map stores -> {element, Index}
     Map<Integer, Integer> map;
     List<Integer> list;
 
@@ -10,54 +8,46 @@ class RandomizedSet {
 
         map = new HashMap<>();
         list = new ArrayList<>();
-
+        
     }
     
     public boolean insert(int val) {
 
-        //if map contains val, return false;
         if(map.containsKey(val)) return false;
 
         list.add(val);
-
-        //add element and last index to map
-        map.put(val, list.size() - 1); 
-
+        map.put(val, list.size() - 1);
         return true;
         
     }
     
     public boolean remove(int val) {
+        
+        //move last element to the index of removed element
+        //update position in map
+        //remove from map 
 
-        //if map does not contain val, return false;
         if(!map.containsKey(val)) return false;
 
-        //if map contans val, remove
-        //put last index of list to the position of the removed element
-
-        //get index of element to remove
         int index = map.get(val);
 
-        //move last element to the index 
         list.set(index, list.get(list.size() - 1));
 
-        //update the change in the map
         map.put(list.get(index), index);
 
-        //remove last element
         list.remove(list.size() - 1);
 
-        //remove the val
         map.remove(val);
-        
+
         return true;
     }
     
     public int getRandom() {
-        
-        Random rand = new Random();
 
-        return list.get(rand.nextInt(list.size()));
+        Random rd = new Random();
+
+        return list.get(rd.nextInt(list.size()));
+        
     }
 }
 
