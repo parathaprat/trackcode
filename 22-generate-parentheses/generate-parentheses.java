707f8f387375c539e108d76b01d1bc2688dcdfb7
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-16 16:54:58 -0500

    Time: 0 ms (100.00%) | Memory: 43.5 MB (37.63%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,35 +1,28 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         
-        // if open == close == n, return
-        //open < n -> open
-        //open > close -> close
-
         List<String> ans = new ArrayList<>();
 
         StringBuilder sb = new StringBuilder();
 
-        getAns(0, 0, ans, n, sb);
+        getAns(0, 0, ans, sb, n);
 
         return ans;
     }
 
-    private void getAns(int open, int close, List<String> ans, int n, StringBuilder sb){
+    private void getAns(int open, int close, List<String> ans, StringBuilder sb, int n){
 
-        if(open == close && close == n){
-            ans.add(sb.toString());
-            return;
-        }
+        if(open == close && close == n) ans.add(sb.toString());
 
         if(open < n){
             sb.append('(');
-            getAns(open + 1, close, ans, n, sb);
+            getAns(open + 1, close, ans, sb, n);
             sb.deleteCharAt(sb.length() - 1);
         }
 
         if(open > close){
             sb.append(')');
-            getAns(open, close + 1, ans, n, sb);
+            getAns(open, close + 1, ans, sb, n);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
