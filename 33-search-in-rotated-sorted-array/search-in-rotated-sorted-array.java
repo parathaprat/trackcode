b6b4ae12d9916a4d70442b9e325522abd23ee970
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-16 13:28:22 -0500

    Time: 0 ms (100.00%) | Memory: 42 MB (81.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,54 +1,57 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        //find pivot element using BS, BS again to find target 
+        //approach -> Binary search to find pivot, find out which side our element is on and BS again
 
-        int left = 0; 
-        int right = nums.length - 1;
+        //finding pivot
 
-        //pivot will be on left after this loop
-        while(left < right){
+        int l = 0;
+        int r = nums.length - 1;
 
-            int mid = left + (right - left) / 2;
+        while(l < r){
 
-            if(nums[mid] > nums[right]){
-                left = mid + 1;
-            }
-            else{
-                right = mid;
-            }
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] > nums[r]) l = mid + 1;
+            else r = mid;
 
         }
 
-        //locate which side our target is on
+        //pivot on left pointer
+        // if(target >= nums[0]){
+        //     r = l - 1;
+        //     l = 0;
+        // }
+        // else{
+        //     r = nums.length - 1;
+        // }
 
-        int start = left;
-        left = 0;
-        right = nums.length - 1;
+          int pivot = l;
 
-        if(target >= nums[start] && target <= nums[right]){
-            left = start;
-        }else{
-            right = start - 1;
+        // Step 2: Determine which half to search
+        if (target >= nums[pivot] && target <= nums[nums.length - 1]) {
+            l = pivot;
+            r = nums.length - 1;
+        } else {
+            l = 0;
+            r = pivot - 1;
         }
 
-        while(left <= right){
 
-            int mid = left + (right - left) / 2;
+        //BS ont that portion 
 
-            if(nums[mid] > target){
-                right = mid - 1;
-            }
-            else if(nums[mid] == target){
-                return mid;
-            }
-            else{
-                left = mid + 1;
-            }
+        while(l <= r){
+
+            int mid = l + (r - l) / 2;
+
+            if(target > nums[mid]) l = mid + 1;
+            else if(target < nums[mid]) r = mid - 1;
+            else return mid;
 
         }
 
         return -1;
+
 
     }
 }
