5c3e435344a25f5bed1bc3a013a3c3671c2033b4
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-16 00:43:41 -0500

    Time: 87 ms (5.03%) | Memory: 46 MB (71.96%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,55 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        int p1 = 0;
+        int p2 = 0;
+        int k = 0;
+
+        int finalArray[] = new int[nums1.length + nums2.length];
+
+        while(p1 < nums1.length && p2 < nums2.length){
+
+            if(nums1[p1] < nums2[p2]){
+                finalArray[k] = nums1[p1];
+                p1++;
+            }
+            else{
+                finalArray[k] = nums2[p2];
+                p2++;
+            }
+            k++;
+        }
+
+        while(p1 < nums1.length){
+            finalArray[k] = nums1[p1];
+            p1++;
+            k++;
+        }
+
+        while(p2 < nums2.length){
+            finalArray[k] = nums2[p2];
+            p2++;
+            k++;
+        }
+
+        for(int i = 0 ; i < finalArray.length; i++){
+            System.out.println(finalArray[i]);
+        }
+
+        if(finalArray.length % 2 == 1){
+            return finalArray[(finalArray.length) / 2];
+        }
+        else{
+            int x = finalArray.length/2;
+
+            System.out.println("X = " + x);
+
+            double p = finalArray[x];
+            double q = finalArray[x - 1];
+            
+            double ans = (p + q)/2;
+
+            return ans;
+        }
+    }
+}
