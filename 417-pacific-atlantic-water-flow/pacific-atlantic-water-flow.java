82047acaf74837d8166caa28c73f3dfc2c3c12ed
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-17 16:54:13 -0500

    Time: 4 ms (97.85%) | Memory: 45.7 MB (62.45%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,27 +1,29 @@
 class Solution {
 
-    //direction array
-    int[][] direction = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-
+    int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        
+        //DFS, find what cells visit both oceans
+        //return the cells that visit both
 
-        //dfs search
         int rows = heights.length;
         int cols = heights[0].length;
 
-        //visited boolean arrays for both
+        List<List<Integer>> res = new ArrayList<>();
+
+        //canVisit arrays 
         boolean[][] pac = new boolean[rows][cols];
         boolean[][] atl = new boolean[rows][cols];
 
-        //base -> top row will touch pacific, bottom row will touch atlantic
+        //we know top -> pac; bot -> atl
         for(int c = 0; c < cols; c++){
 
             dfs(0, c, pac, heights);
             dfs(rows - 1, c, atl, heights);
 
-        }   
+        }
 
-        //base -> left col will touch pacific, right col will touch atlantic
+        //left -> pac; right -> atl
         for(int r = 0; r < rows; r++){
 
             dfs(r, 0, pac, heights);
@@ -29,38 +31,30 @@ class Solution {
 
         }
 
-        List<List<Integer>> res = new ArrayList<>();
+        //now we have 2 matrix which show values that can visit both oceans
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
 
-        //add coordinates to result array if it can reach both oceans
-        for(int r = 0; r < rows; r++){
-            for(int c = 0; c < cols; c++){
-
-                if(pac[r][c] && atl[r][c]){
-                    res.add(Arrays.asList(r, c));
-                }
+                if(pac[i][j] && atl[i][j]) res.add(Arrays.asList(i, j));
             }
         }
 
         return res;
+        
     }
 
-    public void dfs(int r, int c, boolean[][] visited, int[][] heights){
+    private void dfs(int r, int c, boolean[][] visited, int[][] heights){
 
-        //mark visited
         visited[r][c] = true;
 
-        //call dfs if valid in each direction
-        for(int[] dir : direction){
+        for(int[] dir : directions){
 
             int nr = r + dir[0];
             int nc = c + dir[1];
 
-            //check if new coord are out of bounds; if new height is >= old height and if new height has already been visited
             if(nr >= 0 && nr < heights.length && nc >= 0 && nc < heights[0].length && heights[nr][nc] >= heights[r][c] && !visited[nr][nc]){
                 dfs(nr, nc, visited, heights);
             }
         }
-
-        
     }
 }
