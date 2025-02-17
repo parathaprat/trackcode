625de13b79625b78f7c3e19e5ffbe06a84e26e6d
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-17 16:03:02 -0500

    Time: 5 ms (98.15%) | Memory: 44.4 MB (69.97%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,22 +1,18 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         
-        //bellman ford algorithm 
-        //maintian a prices array and a temp prices array
-        //at each iteration copy the temp prices to prices/
-        //BFS at its core
+        //BFS bellman ford
+        //Maintian a prices and temp array 
+        //assign temp to prices at each iteration 
 
-        //Initialise prices array and set it to infinity
         int[] prices = new int[n];
-        Arrays.fill (prices, Integer.MAX_VALUE);
 
-        //since cost to get from src -> src is 0
+        Arrays.fill(prices, Integer.MAX_VALUE);
+
         prices[src] = 0;
 
-        //iterate k + 1 times 
         for(int i = 0; i <= k; i++){
 
-            //create temp prices to make changes on
             int[] temPrices = Arrays.copyOf(prices, n);
 
             for(int[] flight : flights){
@@ -25,12 +21,9 @@ class Solution {
                 int destination = flight[1];
                 int price = flight[2];
 
-                //if the source city itself is not reachable, skip
                 if(prices[source] == Integer.MAX_VALUE) continue;
 
-                //if source is reachable, check if updated cost < current cost, and if it is, update in temp
                 if(prices[source] + price < temPrices[destination]){
-
                     temPrices[destination] = prices[source] + price;
                 }
             }
@@ -38,7 +31,8 @@ class Solution {
             prices = temPrices;
         }
 
-        //return -1 if price to get to dest is infinity
         return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
+
+
     }
 }
