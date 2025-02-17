71c4df46da73a1e845be4e7e2db4dd9511780afd
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-17 18:15:56 -0500

    Time: 3 ms (86.79%) | Memory: 49.8 MB (45.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,16 +1,13 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        
-        //output = number of islands
-        //update count for each land mass
+
 
         int count = 0;
-
+        
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
 
                 if(grid[i][j] == '1'){
-
                     count++;
                     dfs(grid, i, j);
                 }
@@ -22,16 +19,13 @@ class Solution {
 
     private void dfs(char[][] grid, int i, int j){
 
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0'){
-            return;
-        }
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
 
         grid[i][j] = '0';
 
-        dfs(grid, i - 1, j);
         dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
         dfs(grid, i, j + 1);
         dfs(grid, i, j - 1);
-
     }
 }
