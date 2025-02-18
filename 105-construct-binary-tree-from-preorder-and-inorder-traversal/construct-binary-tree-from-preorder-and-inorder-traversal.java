c2136ffccca8c8b09edd3ba7cea8bea9bfd2c553
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-02-17 19:53:33 -0500

    Time: 0 ms (100.00%) | Memory: 44.3 MB (62.18%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,38 +15,32 @@
  */
 class Solution {
 
-    //make gloabl variables for tracking pre and in
     int preInd = 0;
     int inInd = 0;
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
-        //last value == limit, determines the partition that divides left and right subtree in inorder
-        return dfs(preorder, inorder, Integer.MAX_VALUE);
 
+        return getAns(preorder, inorder, Integer.MAX_VALUE);
+        
     }
 
-    public TreeNode dfs(int[] preorder, int[] inorder, int limit){
+    private TreeNode getAns(int[] preorder, int[] inorder, int limit){
 
-        //base-> if preInd > pre.len return null
         if(preInd >= preorder.length) return null;
 
-        //to stop building left subtree, null when inorder element == limit (everything to left of limit is left ST & rigth is right ST)
+        //approached the end of left subtree, sop return null
+        //we are trating the limit as the value itseld
         if(inorder[inInd] == limit){
-
-            //to build right subtree, we have to move past the limit
             inInd++;
             return null;
         }
 
-        TreeNode root = new TreeNode(preorder[preInd]);
+        TreeNode node = new TreeNode(preorder[preInd]);
         preInd++;
 
-        root.left = dfs(preorder, inorder, root.val); //set limit == root.val to build the left subtree
-        root.right = dfs(preorder, inorder, limit); // leave limit since inInd has been ++ till after limit to build right subtree
+        node.left = getAns(preorder, inorder, node.val);
+        node.right = getAns(preorder, inorder, limit);
 
-        return root;
-
-
+        return node;
     }
 }
