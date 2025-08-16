e742259e82b1e0b8c9a1bf41f879a78710c3ad8c
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-16 19:13:26 -0400

    Time: 13 ms (21.39%) | Memory: 45.1 MB (17.16%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         
-        //use a hashset and encode when numbers are present along with row, col, block
-        //row -> number + "row" + i
-        //col -> number + "col" + j
-        //block -> number + "block" + i/3 + "," + j/3
+        //create a set (string) to store row, col and block postions
+        //row = number + "row" + i;
+        //col = number + "col" + j;
+        //block = number + "block" + i/3 + j/3;
 
-        Set set = new HashSet<>();
+        Set<String> set = new HashSet<>();
 
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
 
-                //skip if no number present
-                if(board[i][j] == '.') continue;
+                if(board[i][j] != '.'){
 
-                if(
-                    !set.add(board[i][j] + "row" + i) ||
-                    !set.add(board[i][j] + "col" + j) ||
-                    !set.add(board[i][j] + "block" + i/3 + "," + j/3)
-                )
-                return false;
+                    if(
+                        !set.add(board[i][j] + "row" + i) ||
+                        !set.add(board[i][j] + "col" + j) ||
+                        !set.add(board[i][j] + "block" + i/3 + j/3)
+                    )
+                    return false;
+                }
             }
         }
 
