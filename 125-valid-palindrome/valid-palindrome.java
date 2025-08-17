74777168be78664dd5b23eadd41dc2a5d7bbe295
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-16 23:01:44 -0400

    Time: 125 ms (8.40%) | Memory: 46.5 MB (5.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,29 +3,23 @@ class Solution {
         
         String ans = "";
 
-        //iterate thru each char, add only if it is digit or letter
-        for(char c : s.toCharArray()){
-
-            if(Character.isDigit(c) || Character.isLetter(c)){
-                ans += c;
+        for(char ch : s.toCharArray()){
+            if(Character.isDigit(ch) || Character.isLetter(ch)){
+                ans += ch;
             }
-
         }
 
         ans = ans.toLowerCase();
 
-        int left = 0;
         int right = ans.length() - 1;
+        int left = 0;
 
         while(left <= right){
-
             if(ans.charAt(left) != ans.charAt(right)) return false;
-
             left++;
             right--;
         }
 
         return true;
-
     }
 }
