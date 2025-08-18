8403d690a6af1d3e4c0d091690f460e917d70ab6
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-18 14:11:52 -0400

    Time: 31 ms (56.65%) | Memory: 52.1 MB (30.07%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,33 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
-        Arrays.sort(nums);
+        //sort the nums, iterate through each and treat target as -ve that num
+        //use 2 pointer approach, since array is sorted
 
-        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
         for(int i = 0; i < nums.length - 2; i++){
 
+            //making sure elements are not equal and accounting for 0th edge case
             if(i == 0 || nums[i] != nums[i - 1]){
 
+                //left pointer starts from +1, hence loop runs till -2
                 int left = i + 1;
                 int right = nums.length - 1;
 
-                int target = -nums[i];
+                int target = - nums[i];
 
                 while(left < right){
 
                     if(nums[left] + nums[right] == target){
+                        ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                        result.add(Arrays.asList(nums[i], nums[right], nums[left]));
-
+                        //pair found, iterate till duplicates are skipped, move pointers
                         while(left < right && nums[left] == nums[left + 1]) left++;
                         while(left < right && nums[right] == nums[right - 1]) right--;
 
                         left++;
                         right--;
+
                     }
                     else if(nums[left] + nums[right] > target){
-
                         right--;
                     }
                     else{
@@ -36,7 +40,6 @@ class Solution {
                 }
             }
         }
-
-        return result;
+        return ans;
     }
 }
