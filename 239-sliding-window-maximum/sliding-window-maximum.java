4893420139888ea3fab7f190fd1f35f437e307a3
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-19 16:01:11 -0400

    Time: 43 ms (18.12%) | Memory: 62.3 MB (21.43%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         
-        //use a montonic queue
-        //index -> q, result -> arrayList
-        //front of the q stores max index
-        //removeFirst if out of window
-        //removeLast if greater element appears
-
         LinkedList<Integer> index = new LinkedList<>();
         List<Integer> res = new ArrayList<>();
 
         for(int i = 0; i < nums.length; i++){
 
-            //keep removing front when outside the window
             while(!index.isEmpty() && index.getFirst() < i - k + 1){
                 index.removeFirst();
             }
 
-            //keep removing back when nums[i]>
             while(!index.isEmpty() && nums[i] > nums[index.getLast()]){
                 index.removeLast();
             }
 
             index.add(i);
 
-            //if window is satisfied, pop front to res
             if(i >= k - 1){
                 res.add(nums[index.getFirst()]);
             }
