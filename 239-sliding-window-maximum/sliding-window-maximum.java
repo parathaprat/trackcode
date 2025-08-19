cd104b7fefe99c6fbf38b5c5989dc05801830548
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-19 15:49:38 -0400

    Time: 41 ms (19.99%) | Memory: 62.2 MB (22.32%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,40 +1,33 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         
-        //de-q approach
-        LinkedList<Integer> index = new LinkedList<>();
+        //linkedlist monotonic queue strategy
+        //maintain deque, add to result when window is size k
+        //remove leftmost when outside window, remove rigthmost when nums[i] >
 
+        List<Integer> index = new LinkedList<>();
         List<Integer> result = new ArrayList<>();
 
         for(int i = 0; i < nums.length; i++){
 
-            //while index of the front of the dq is outside the window, remove
-            while(!index.isEmpty() && index.getFirst() <= i - k){
+            //remove from the front when it goes outside the window 
+            while(!index.isEmpty() && index.getFirst() < i - k + 1){
                 index.removeFirst();
             }
 
-            //while the current num is >= rigthmost element in dq popRight
-            //since we are maintaining a strickly decreasing dq
-            while(!index.isEmpty() && nums[i] >= nums[index.getLast()]){
+            //remove from the end if new element is > (new will always get picked in the window)
+            while(!index.isEmpty() && nums[i] > nums[index.getLast()]){
                 index.removeLast();
             }
 
-            //add index
+            //add valid index after all removals
             index.add(i);
 
-            //start recording maximum only when the first full window is formed
             if(i >= k - 1){
                 result.add(nums[index.getFirst()]);
             }
         }
 
-        int[] ans = new int[result.size()];
-
-        for(int i = 0; i < result.size(); i++){
-            ans[i] = result.get(i);
-        }
-
-        return ans;
-
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
