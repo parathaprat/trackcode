936649c669018cb243d58dd2f655bb896c968dc8
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-19 21:44:58 -0400

    Time: 4 ms (97.80%) | Memory: 44.4 MB (99.61%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -9,43 +9,39 @@ class MinStack {
             this.num = num;
             this.min = min;
         }
-
     }
 
     Stack<tuple> st;
 
     public MinStack() {
-        
         st = new Stack<>();
-
     }
     
     public void push(int val) {
+
+        //push tuple
+
+        if(st.isEmpty()){
+            st.push(new tuple(val, val));
+        }
+        else{
+            st.push(new tuple(val, Math.min(val, st.peek().min)));
+        }
         
-        //if stack is empty -> push {val, val}
-        //if its not -> push {val, Math.min(val, st.peek())};
-
-        if(st.isEmpty()) st.push(new tuple(val, val));
-        else st.push(new tuple(val, Math.min(val, st.peek().min)));
-
     }
     
     public void pop() {
-
-        if(!st.isEmpty()) st.pop();
         
+        if(!st.isEmpty()) st.pop();
+
     }
     
     public int top() {
-
         return st.peek().num;
-        
     }
     
     public int getMin() {
-
         return st.peek().min;
-        
     }
 }
 
