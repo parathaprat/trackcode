590b0b54110cf33605018f6deb9e355725151642
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-23 02:08:51 -0400

    Time: 71 ms (53.39%) | Memory: 58.1 MB (18.97%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,24 +1,34 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         
+        //use stack to keep track of heights
+        //run loop, add height to a monotonic increasing stack
+        //if shorter stack is excountered, while loop all areas and update
+        //same while loop again with end as right boundary for remaining 
+
         Stack<Integer> st = new Stack<>();
         st.push(-1);
+
         int maxArea = 0;
 
         for(int i = 0; i < heights.length; i++){
 
+            //<= to account for counting max height
             while(st.peek() != -1 && heights[i] <= heights[st.peek()]){
-                int height = heights[st.pop()];
-                int width = i - 1 - st.peek(); //i-1 right; st.peek left
-                maxArea = Math.max(maxArea, height * width);
+
+                int h = heights[st.pop()];
+                int w = i - st.peek() - 1;
+                maxArea = Math.max(maxArea, h * w);
             }
+
             st.push(i);
         }
 
+        //remaining bars
         while(st.peek() != -1){
-            int height = heights[st.pop()];
-            int width = heights.length - 1 - st.peek();
-            maxArea = Math.max(maxArea, height * width);
+            int h = heights[st.pop()];
+            int w = heights.length - st.peek() - 1;
+            maxArea = Math.max(maxArea, h * w);
         }
 
         return maxArea;
