e753c49556d4efe577a01ca3dc69a76acae78960
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-22 23:16:22 -0400

    Time: 83 ms (54.38%) | Memory: 58 MB (41.48%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,30 +1,25 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
+        
+        //create a car[position][speed]
+        //sort by descending otder of position 
+        //add to stack if time by lower postion > st.peek()
+        //return st.size
 
-        //Sort accroding to position in [position, speed]
-        //higher position first since we want to iterate from the back
-        //Create stack, add and merge according to time
+        int[][] cars = new int[speed.length][2];
 
-        //[position][speed]
-        int[][] cars = new int[position.length][2];
-
-        for(int i = 0; i < position.length; i++){
+        for(int i = 0; i < speed.length; i++){
             cars[i][0] = position[i];
             cars[i][1] = speed[i];
         }
 
-        //sorting in descending order of position
         Arrays.sort(cars, (a, b) -> Integer.compare(b[0], a[0]));
 
-        //use double to handle decimals
         Stack<Double> st = new Stack<>();
 
-        //calculate times of arrival and merge
         for(int[] car : cars){
-            int pos = car[0];
-            int spd = car[1];
 
-            double time = (double)(target - pos) / spd;
+            double time = (double)(target - car[0])/car[1];
 
             if(st.isEmpty() || time > st.peek()){
                 st.push(time);
