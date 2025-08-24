55bd6a6e9657fb30a335a45f3e8e6e4fa504dda8
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-24 13:44:26 -0400

    Time: 0 ms (100.00%) | Memory: 42.6 MB (11.72%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,51 +1,48 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        //approach -> Binary search to find pivot, find out which side our element is on and BS again
-
-        //finding pivot
-
         int l = 0;
         int r = nums.length - 1;
 
         while(l < r){
 
-            int mid = l + (r - l) / 2;
+            int m = l + (r - l)/2;
 
-            if(nums[mid] > nums[r]) l = mid + 1;
-            else r = mid;
 
+                if(nums[m] > nums[r]){
+                    l = m + 1;
+                }
+                else{
+                    r = m;
+                }
         }
-
+        
         int pivot = l;
 
         if(target >= nums[pivot] && target <= nums[nums.length - 1]){
-
             l = pivot;
             r = nums.length - 1;
-            
         }
         else{
             r = pivot - 1;
             l = 0;
         }
 
-
-
-        //BS ont that portion 
-
         while(l <= r){
 
-            int mid = l + (r - l) / 2;
+            int m = l + (r - l)/2;
 
-            if(target > nums[mid]) l = mid + 1;
-            else if(target < nums[mid]) r = mid - 1;
-            else return mid;
-
+            if(nums[m] == target){
+                return m;
+            }
+            else if(nums[m] < target){
+                l = m + 1;
+            }
+            else{
+                r = m - 1;
+            }
         }
 
-        return -1;
-
-
+        return - 1;
     }
 }
