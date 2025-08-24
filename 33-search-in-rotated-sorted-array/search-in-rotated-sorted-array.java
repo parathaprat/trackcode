5365194e01b0c00966d47ecd6abeef9905254edd
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-24 13:52:08 -0400

    Time: 0 ms (100.00%) | Memory: 41.9 MB (83.78%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,24 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
         
+        //BS to find pivot, then BS again on the correct side to find element
+
         int l = 0;
         int r = nums.length - 1;
 
+        //finding pivot
         while(l < r){
 
             int m = l + (r - l)/2;
 
-
-                if(nums[m] > nums[r]){
-                    l = m + 1;
-                }
-                else{
-                    r = m;
-                }
+            if(nums[m] > nums[r]){
+                l = m + 1;
+            }
+            else{
+                r = m;
+            }
         }
-        
+
         int pivot = l;
 
+        //finding partition
         if(target >= nums[pivot] && target <= nums[nums.length - 1]){
             l = pivot;
             r = nums.length - 1;
@@ -28,6 +31,7 @@ class Solution {
             l = 0;
         }
 
+        //finding element
         while(l <= r){
 
             int m = l + (r - l)/2;
@@ -43,6 +47,6 @@ class Solution {
             }
         }
 
-        return - 1;
+        return -1;
     }
 }
