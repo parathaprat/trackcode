f96d21026ba7117f9e9f34dbcf825fa8e7f97651
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-26 14:01:24 -0400

    Time: 0 ms (100.00%) | Memory: 42.3 MB (88.07%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -11,45 +11,34 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
-        //using a temp node to store head's position (we return temp.next)
+        //iterate using 2 pointers
         ListNode temp = new ListNode(0);
-
-        //current node tracker
-        ListNode currentNode = temp;
+        ListNode current = temp;
 
         while(list1 != null && list2 != null){
 
-            //append smaller value 
             if(list1.val < list2.val){
-
-                currentNode.next = list1;
+                current.next = list1;
                 list1 = list1.next;
-
             }
             else{
-
-                currentNode.next = list2;
+                current.next = list2;
                 list2 = list2.next;
-
             }
 
-            currentNode = currentNode.next;
+            current = current.next;
         }
 
         while(list1 != null){
-
-            currentNode.next = list1;
+            current.next = list1;
             list1 = list1.next;
-            currentNode = currentNode.next;
-
+            current = current.next;
         }
 
         while(list2 != null){
-
-            currentNode.next = list2;
+            current.next = list2;
             list2 = list2.next;
-            currentNode = currentNode.next;
-            
+            current = current.next;
         }
 
         return temp.next;
