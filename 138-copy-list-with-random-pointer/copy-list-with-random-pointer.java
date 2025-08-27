be0fde3a60e4aef6c4c8bc747e22b784f0099ddc
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-08-26 21:00:59 -0400

    Time: 0 ms (100.00%) | Memory: 44.3 MB (93.47%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -16,31 +16,22 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         
-        //make a Map<node, node>
-        //1st pass = make copy nodes
-        //2nd pass = make connections 
-
         Map<Node, Node> map = new HashMap<>();
 
         Node track = head;
 
-        //1st pass = copy nodes and add to map
         while(track != null){
-
             Node node = new Node(track.val);
             map.put(track, node);
             track = track.next;
-
         }
 
         track = head;
 
         while(track != null){
-
             Node copy = map.get(track);
             copy.next = map.get(track.next);
             copy.random = map.get(track.random);
-
             track = track.next;
         }
 
