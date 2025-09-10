0c75a21baa5d54312d38d551d1c2133ba2e6d820
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-10 15:23:34 -0400

    Time: 21 ms (57.21%) | Memory: 46 MB (31.97%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,27 +1,21 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         
-        //HashMap {Task, Frequency} WRONG
+        //maxHeap storing highest freq tasks
+        //queue storing eack task and its exit times
 
-        //Int freq array -> we dont need to deal with chars
-        //maxHeap {Frequency}
-        //Queue {Task, outTime}
-
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
 
         int[] freq = new int[26];
 
-        for(char c : tasks){
-
-            freq[c - 'A']++;
+        for(char t : tasks){
+            freq[t - 'A']++;
         }
 
         for(int fre : freq){
-
-            if(fre != 0) maxHeap.add(fre);
+            if(fre != 0) maxHeap.offer(fre);
         }
 
-        //{freq, timeOut}
         Queue<int[]> q = new LinkedList<>();
 
         int time = 0;
@@ -29,31 +23,28 @@ class Solution {
         while(!q.isEmpty() || !maxHeap.isEmpty()){
 
             if(maxHeap.isEmpty()){
-
                 time = q.peek()[1];
-
             }
 
             if(!maxHeap.isEmpty()){
 
                 int task = maxHeap.poll();
-                task -= 1;
+                task-= 1;
 
-                if(task > 0){
-                    q.add(new int[]{task, time + n});
-                }
+                if(task > 0) q.add(new int[]{task, time + n});
             }
 
+            if(!q.isEmpty() && q.peek()[1] == time){
 
-            if(!q.isEmpty() && time == q.peek()[1]){
                 maxHeap.add(q.poll()[0]);
+                
             }
+
 
             time++;
+            
         }
 
-
         return time;
-
     }
 }
