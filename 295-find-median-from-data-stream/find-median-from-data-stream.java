9d11ed8fd3c11ced5364dccddf6346c59347b0de
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-10 17:30:38 -0400

    Time: 127 ms (47.36%) | Memory: 63.8 MB (58.71%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,44 +1,43 @@
 class MedianFinder {
 
-    //2 heaps needed
-    
-    //maxHeap -> first half
-    //minHeap -> 2nd half
-    
-    //keep balancing elements, until they are about the same length
+    //2 heaps
+    //1st half -> maxHeap
+    //2nd half -> minHeap
 
     PriorityQueue<Integer> maxHeap;
     PriorityQueue<Integer> minHeap;
 
     public MedianFinder() {
 
-        maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        minHeap = new PriorityQueue<>((a, b) -> a - b);
-
+        maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
+        
     }
     
     public void addNum(int num) {
 
-        //add to 1st heap, then balance
-        maxHeap.add(num);
+        maxHeap.offer(num);
 
-        //if first half is larger || first half got bigger elements than 2nd half, 2nd.add(1st)
-        if(maxHeap.size() - minHeap.size() > 1 || !minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) minHeap.add(maxHeap.poll());
+        while(maxHeap.size() - minHeap.size() > 1 || !minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()){
+            minHeap.offer(maxHeap.poll());
+        }
 
-        //if 2nd half is larger, 1st half.add(2nd half)
-        if(minHeap.size() - maxHeap.size() > 1) maxHeap.add(minHeap.poll());
-
+        while(minHeap.size() - maxHeap.size() > 1){
+            maxHeap.offer(minHeap.poll());
+        }
+        
     }
     
     public double findMedian() {
 
         double ans = 0;
-        
+
         if(maxHeap.size() == minHeap.size()) ans = (double)(maxHeap.peek() + minHeap.peek()) / 2;
         else if(maxHeap.size() > minHeap.size()) ans = (double)maxHeap.peek();
-        else ans = minHeap.peek();
+        else ans = (double)minHeap.peek();
 
         return (double)ans;
+        
     }
 }
 
