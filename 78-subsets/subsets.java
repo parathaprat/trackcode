712cfb39e81b286dbb830f38dba3a11d0e8f9e81
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-10 18:34:56 -0400

    Time: 0 ms (100.00%) | Memory: 42.8 MB (66.16%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,28 +1,25 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        
+
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> set = new ArrayList<>();
 
-        dfs(nums, 0, res, set);
+        backtrack(nums, 0, res, set);
 
         return res;
     }
 
-    private void dfs(int[] nums, int index, List<List<Integer>> res, List<Integer> set){
+    private void backtrack(int[] nums, int index, List<List<Integer>> res, List<Integer> set){
 
-        //reached the end for a particular subset
-        if(index >= nums.length){
-
+        if(index == nums.length){
             res.add(new ArrayList<>(set));
             return;
         }
 
-        //current element -> part of the subset
         set.add(nums[index]);
-        dfs(nums, index + 1, res, set);
+        backtrack(nums, index + 1, res, set);
 
         set.remove(set.size() - 1);
-        dfs(nums, index + 1, res, set);
+        backtrack(nums, index + 1, res, set);
     }
 }
