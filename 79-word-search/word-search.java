ffd6686e512cb10ebbc0ba932dc76224184f8700
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-11 00:02:48 -0400

    Time: 151 ms (41.37%) | Memory: 42 MB (25.62%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,34 +1,32 @@
 class Solution {
-
     public boolean exist(char[][] board, String word) {
-        
-        //optimal -> brute force
-        boolean[][] visit = new boolean[board.length][board[0].length];
+
+        boolean[][] visited = new boolean[board.length][board[0].length];
 
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
 
-                if(dfs(board, i, j, 0, word, visit)) return true;
+                if(dfs(board, i, j, 0, visited, word)) return true;
             }
         }
 
         return false;
     }
 
-    private boolean dfs(char[][] board, int i, int j, int ind, String word, boolean[][] visit){
+    private boolean dfs(char[][] board, int i, int j, int ind, boolean[][] visited, String word){
 
-        if(ind == word.length()) return true;
+        if(ind >= word.length()) return true;
 
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(ind) || visit[i][j]) return false;
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(ind) || visited[i][j]) return false;
 
-        visit[i][j] = true;
+        visited[i][j] = true;
 
-        boolean rest = dfs(board, i + 1, j, ind + 1, word, visit) ||
-                       dfs(board, i - 1, j, ind + 1, word, visit) ||
-                       dfs(board, i, j + 1, ind + 1, word, visit) ||
-                       dfs(board, i, j - 1, ind + 1, word, visit);
+        boolean rest = dfs(board, i + 1, j, ind + 1, visited, word) ||
+                        dfs(board, i - 1, j, ind + 1, visited, word) ||
+                        dfs(board, i, j + 1, ind + 1, visited, word) ||
+                        dfs(board, i, j - 1, ind + 1, visited, word);
 
-        visit[i][j] = false;
+        visited[i][j] = false;
 
         return rest;
     }
