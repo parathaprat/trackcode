8f01cdaada95a8daa070671de12346833209b119
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-11 19:45:00 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (37.76%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -6,18 +6,19 @@ class Solution {
 
         dp[s.length()] = 1;
 
-        return getAns(0, s, dp);
+        return getAns(dp, s, 0);
     }
 
-    public int getAns(int i, String s, int[] dp){
+    private int getAns(int[] dp, String s, int i){
 
-        if(dp[i] != -1) return dp[i];
+        if(dp[i] != - 1) return dp[i];
+
         if(s.charAt(i) == '0') return 0;
 
-        int res = getAns(i + 1, s, dp);
+        int res = getAns(dp, s, i + 1);
 
         if(i + 1 < s.length() && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')){
-            res += getAns(i + 2, s, dp);
+            res += getAns(dp, s, i + 2);
         }
 
         return dp[i] = res;
