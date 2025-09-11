8692d074b57872cfdc82ecf10ce5eec6b429e03c
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-11 00:09:09 -0400

    Time: 151 ms (41.37%) | Memory: 41.6 MB (78.34%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-
+        
         boolean[][] visited = new boolean[board.length][board[0].length];
 
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
 
-                if(dfs(board, i, j, 0, visited, word)) return true;
+                if(dfs(board, i, j, word, visited, 0)) return true;
             }
         }
 
         return false;
     }
 
-    private boolean dfs(char[][] board, int i, int j, int ind, boolean[][] visited, String word){
+    private boolean dfs(char[][] board, int i, int j, String word, boolean[][] visited, int ind){
 
         if(ind >= word.length()) return true;
 
@@ -21,10 +21,10 @@ class Solution {
 
         visited[i][j] = true;
 
-        boolean rest = dfs(board, i + 1, j, ind + 1, visited, word) ||
-                        dfs(board, i - 1, j, ind + 1, visited, word) ||
-                        dfs(board, i, j + 1, ind + 1, visited, word) ||
-                        dfs(board, i, j - 1, ind + 1, visited, word);
+        boolean rest = dfs(board, i + 1, j, word, visited, ind + 1) ||
+                        dfs(board, i - 1, j, word, visited, ind + 1) ||
+                        dfs(board, i, j + 1, word, visited, ind + 1) ||
+                        dfs(board, i, j - 1, word, visited, ind + 1);
 
         visited[i][j] = false;
 
