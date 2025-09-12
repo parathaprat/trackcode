33ce32648797bfc17a89ce42e591b539dad04c61
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-11 20:33:29 -0400

    Time: 2 ms (54.92%) | Memory: 45.4 MB (13.30%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,32 +1,25 @@
 class Solution {
     public int maxProduct(int[] nums) {
         
-        //product -> can switch max to min with one -ve sign// min only maintained because of this
-
         int max = nums[0];
         int min = nums[0];
         int result = nums[0];
 
         for(int i = 1; i < nums.length; i++){
 
-            //if -ve encountered, switch max and min
             if(nums[i] < 0){
-                
                 int temp = max;
                 max = min;
                 min = temp;
-
             }
 
-            //update max and min between product with next num or jsut next num
-            max = Math.max(nums[i], max * nums[i]);
-            min = Math.min(nums[i], min * nums[i]);
+            max = Math.max( nums[i], nums[i] * max);
+            min = Math.min(nums[i], nums[i] * min);
 
-            //update result at each iteration
             result = Math.max(result, max);
-
         }
 
         return result;
+
     }
 }
