5f74ce0abe4da59c96b0a96f1409403d4c507030
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-13 16:31:54 -0400

    Time: 1 ms (100.00%) | Memory: 43 MB (10.95%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,52 +1,49 @@
 class Solution {
+    int fresh = 0;
+
     public int orangesRotting(int[][] grid) {
-        
-        //brute force bfs
+        int time = 0;
 
         Queue<int[]> q = new LinkedList<>();
-
-        int fresh = 0;
 
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
 
-                if(grid[i][j] == 2) q.add(new int[]{i, j});
-                if(grid[i][j] == 1) fresh++;
-
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i, j});
+                }
+                if(grid[i][j] == 1){
+                    fresh++;
+                }
             }
         }
 
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-        int time = 0;
         while(!q.isEmpty() && fresh != 0){
 
             int level = q.size();
 
             for(int i = 0; i < level; i++){
 
-                int[] rot = q.poll();
+                int[] coord = q.poll();
 
-                for(int[] dir : directions){
-
-                    int nr = rot[0] + dir[0];
-                    int nc = rot[1] + dir[1];
-
-                    if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && grid[nr][nc] == 1) {
-
-                        grid[nr][nc] = 2;
-                        fresh--;
-                        q.add(new int[]{nr, nc});
-
-                    }
-                }
-
-
+                addOrange(coord[0] + 1, coord[1], grid, q);
+                addOrange(coord[0] - 1, coord[1], grid, q);
+                addOrange(coord[0], coord[1] + 1, grid, q);
+                addOrange(coord[0], coord[1] - 1, grid, q);
             }
 
-            time++;
+            if(!q.isEmpty()) time++;
         }
 
         return fresh == 0 ? time : -1;
+    }
+
+    private void addOrange(int i, int j, int[][] grid, Queue<int[]> q){
+
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || grid[i][j] == 2) return;
+
+        grid[i][j] = 2;
+        q.offer(new int[]{i, j});
+        fresh--;
     }
 }
