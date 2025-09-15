4fe49de33fc32ee349dbc10e1202fd86219ed353
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-15 19:16:06 -0400

    Time: 2 ms (83.26%) | Memory: 45.5 MB (58.88%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public void solve(char[][] board) {
+        
+        //turn boundry connected O to T, switch O to x and T to O and return
+
+        int r = board.length;
+        int c = board[0].length;
+
+        //dfs to convert boundry connected Os
+        for(int i = 0; i < r; i++){
+            if(board[i][0] == 'O') dfs(i, 0, board);
+            if(board[i][c - 1] == 'O') dfs(i, c - 1, board);
+        }
+
+        for(int i = 0; i < c; i++){
+            if(board[0][i] == 'O') dfs(0, i, board);
+            if(board[r - 1][i] == 'O') dfs(r - 1, i, board);
+        }
+
+        //change remainging Os to Xs, change Ts to Os
+        for(int i = 0; i < r; i ++){
+            for(int j = 0; j < c; j++){
+                if(board[i][j] == 'O') board[i][j] = 'X';
+                if(board[i][j] == 'T') board[i][j] = 'O';
+            }
+        }
+    }
+
+    private void dfs(int i, int j, char[][] board){
+
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') return;
+
+        board[i][j] = 'T';
+
+        for(int[] dir : directions){
+            int nr = i + dir[0];
+            int nc = j + dir[1];
+
+            dfs(nr, nc, board);
+        }
+    }
+}
