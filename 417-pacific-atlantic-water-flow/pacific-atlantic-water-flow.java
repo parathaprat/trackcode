ee96ff2153a252d68967d0b91e127a234ea0d649
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-15 16:11:30 -0400

    Time: 4 ms (97.70%) | Memory: 46.1 MB (16.21%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,32 +1,29 @@
 class Solution {
 
-    int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    int[][] directions = {{1, 0}, {0 , 1}, {-1, 0}, {0, -1}};
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        
-        int rows = heights.length;
-        int cols = heights[0].length;
 
         List<List<Integer>> res = new ArrayList<>();
 
+        int rows = heights.length;
+        int cols = heights[0].length;
+        
         boolean[][] pac = new boolean[rows][cols];
         boolean[][] atl = new boolean[rows][cols];
 
         for(int c = 0; c < cols; c++){
-
             dfs(0, c, pac, heights);
             dfs(rows - 1, c, atl, heights);
         }
 
         for(int r = 0; r < rows; r++){
-
             dfs(r, 0, pac, heights);
             dfs(r, cols - 1, atl, heights);
         }
 
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
-
                 if(pac[i][j] && atl[i][j]) res.add(Arrays.asList(i, j));
             }
         }
@@ -34,18 +31,21 @@ class Solution {
         return res;
     }
 
-    private void dfs(int r, int c, boolean[][] visit, int[][] heights){
+    private void dfs(int i, int j, boolean[][] visit, int[][]heights){
 
-        visit[r][c] = true;
+        visit[i][j] = true;
 
         for(int[] dir : directions){
 
-            int nr = r + dir[0];
-            int nc = c + dir[1];
+            int nr = i + dir[0];
+            int nc = j + dir[1];
 
-            if(nr >= 0 && nr < heights.length && nc >= 0 && nc < heights[0].length && heights[nr][nc] >= heights[r][c] && !visit[nr][nc]){
-                dfs(nr, nc, visit, heights);
-            }
+            if(nr >= 0 && nr < heights.length &&
+                nc >= 0 && nc < heights[0].length &&
+                heights[nr][nc] >= heights[i][j] &&
+                !visit[nr][nc]){
+                    dfs(nr, nc, visit, heights);
+                }
         }
     }
 }
