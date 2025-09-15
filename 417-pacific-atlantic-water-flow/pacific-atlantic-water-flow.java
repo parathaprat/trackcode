49fabb215ebb78cc029f8227ea2a54e2b8b75b89
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-15 15:51:52 -0400

    Time: 4 ms (97.70%) | Memory: 45.6 MB (74.77%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,37 +1,29 @@
 class Solution {
 
     int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         
-        //DFS, find what cells visit both oceans
-        //return the cells that visit both
-
         int rows = heights.length;
         int cols = heights[0].length;
 
         List<List<Integer>> res = new ArrayList<>();
 
-        //canVisit arrays 
         boolean[][] pac = new boolean[rows][cols];
         boolean[][] atl = new boolean[rows][cols];
 
-        //we know top -> pac; bot -> atl
         for(int c = 0; c < cols; c++){
 
             dfs(0, c, pac, heights);
             dfs(rows - 1, c, atl, heights);
-
         }
 
-        //left -> pac; right -> atl
         for(int r = 0; r < rows; r++){
 
             dfs(r, 0, pac, heights);
             dfs(r, cols - 1, atl, heights);
-
         }
 
-        //now we have 2 matrix which show values that can visit both oceans
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
 
@@ -40,20 +32,19 @@ class Solution {
         }
 
         return res;
-        
     }
 
-    private void dfs(int r, int c, boolean[][] visited, int[][] heights){
+    private void dfs(int r, int c, boolean[][] visit, int[][] heights){
 
-        visited[r][c] = true;
+        visit[r][c] = true;
 
         for(int[] dir : directions){
 
             int nr = r + dir[0];
             int nc = c + dir[1];
 
-            if(nr >= 0 && nr < heights.length && nc >= 0 && nc < heights[0].length && heights[nr][nc] >= heights[r][c] && !visited[nr][nc]){
-                dfs(nr, nc, visited, heights);
+            if(nr >= 0 && nr < heights.length && nc >= 0 && nc < heights[0].length && heights[nr][nc] >= heights[r][c] && !visit[nr][nc]){
+                dfs(nr, nc, visit, heights);
             }
         }
     }
