6a4b990f3f2b48d769c7b8a7a333c1876bc097c3
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-15 23:36:01 -0400

    Time: 9 ms (10.45%) | Memory: 46.3 MB (31.13%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -3,59 +3,43 @@ class Solution {
     Map<Integer, List<Integer>> map = new HashMap<>();
     Set<Integer> visited = new HashSet<>();
     Set<Integer> pros = new HashSet<>();
-    List<Integer> res = new ArrayList<>();
+    List<Integer> res = new LinkedList<>();
 
 
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         
-        //detect loop in graph
-
-        //adj map, visited
-
         for(int i = 0; i < numCourses; i++){
-
             map.put(i, new ArrayList<>());
         }
-
         for(int[] pre : prerequisites){
-
             map.get(pre[0]).add(pre[1]);
         }
 
         for(int i = 0; i < numCourses; i++){
-
             if(!dfs(i)) return new int[0];
-
         }
 
         int[] ans = new int[res.size()];
 
-        for(int i = 0; i < ans.length; i++){
-
+        for(int i = 0; i < res.size(); i++){
             ans[i] = res.get(i);
-
         }
 
         return ans;
-
     }
 
     private boolean dfs(int course){
 
         if(visited.contains(course)) return false;
-
         if(pros.contains(course)) return true;
 
         visited.add(course);
 
-        for(int pre : map.get(course)){
-
-            if(!dfs(pre)) return false;
-
+        for(int nei : map.get(course)){
+            if(!dfs(nei)) return false;
         }
 
         visited.remove(course);
-        // map.put(course, new ArrayList<>());
         pros.add(course);
         res.add(course);
         return true;
