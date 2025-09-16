c61332b616d5d7cc72271832178059239d7ff50f
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-15 21:12:23 -0400

    Time: 7 ms (44.31%) | Memory: 45.7 MB (39.64%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,28 +1,20 @@
 class Solution {
 
     Map<Integer, List<Integer>> map = new HashMap<>();
-    Set<Integer> visited = new HashSet<>();
+    Set<Integer> set = new HashSet<>();
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         
-        //detect loop in graph
-
-        //adj map, visited
-
         for(int i = 0; i < numCourses; i++){
-
             map.put(i, new ArrayList<>());
         }
 
         for(int[] pre : prerequisites){
-
             map.get(pre[0]).add(pre[1]);
         }
 
         for(int i = 0; i < numCourses; i++){
-
             if(!dfs(i)) return false;
-
         }
 
         return true;
@@ -30,18 +22,18 @@ class Solution {
 
     private boolean dfs(int course){
 
-        if(visited.contains(course)) return false;
+        if(set.contains(course)) return false;
 
-        visited.add(course);
+        set.add(course);
 
-        for(int pre : map.get(course)){
-
-            if(!dfs(pre)) return false;
-
+        for(int nei : map.get(course)){
+            if(!dfs(nei)) return false;
         }
 
-        visited.remove(course);
+        set.remove(course);
         map.put(course, new ArrayList<>());
         return true;
     }
+
+
 }
