51fc8e0a10cb76a937db62d38e611d1b82e46af3
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-29 18:11:47 -0400

    Time: 18 ms (24.18%) | Memory: 49.7 MB (5.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,33 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        //Approach: freq array for each word
-        //map<freq.toString, List<words>>
-        //return values of map 
 
-        HashMap<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
 
-        for(int i = 0; i < strs.length; i++){
+        for(String str : strs){
 
-            int[] freq = new int[26];
+            int freq[] = new int[26];
 
-            //populating freq array
-            for(char c : strs[i].toCharArray()){
-
-                freq[c - 'a']++;
-
+            for(char ch : str.toCharArray()){
+                freq[ch - 'a']++;
             }
 
             String key = Arrays.toString(freq);
 
-            //if map does not have a arrayList for a key, add it in
             if(!map.containsKey(key)){
                 map.put(key, new ArrayList<>());
             }
 
-            map.get(key).add(strs[i]);
+            map.get(key).add(str);
         }
 
         return new ArrayList<>(map.values());
+        
     }
 }
