c88b6ec793abfab21ba45588fb4339e108dcc641
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-09-29 18:04:42 -0400

    Time: 3 ms (90.57%) | Memory: 43.7 MB (59.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,29 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        //cannot use hashset: 1 of the words can have multiple letters, hence we use frequency arrays
+        int freq[] = new int[26];
 
-        int[] freq = new int[26];
-
-        if(s.length() != t.length()) return false;
-
-        //populate frequencies for s
-        for(int i = 0; i < s.length(); i++){
-
-            freq[s.charAt(i) - 'a']++;
-
+        for(char c : s.toCharArray()){
+            freq[c - 'a']++;
         }
 
-        //subtract chars of t, if it ever goes below 0, ret false
-        for(int i = 0; i < t.length(); i++){
+        for(char c : t.toCharArray()){
+            freq[c - 'a']--;
+        }
 
-            freq[t.charAt(i) - 'a']--;
-            if(freq[t.charAt(i) - 'a'] < 0) return false;
-
+        for(int ind : freq){
+            if(ind != 0) return false;
         }
 
         return true;
-
-        
     }
 }
