744d0977ed3fc306626359a9f840a71f4030dc01
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-01 12:44:43 -0400

    Time: 11 ms (91.76%) | Memory: 49.8 MB (6.20%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,38 +1,32 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         
-        //Approach: Create frequency map using HashMap
-        //Use bucket sort to group frequencies
-        //get from the back
+        //freq map
+        //Bucket sort -> freq is index, and index holds value
+        //loop from the back
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        //populate freq map
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        //Array of lists for buckets
         List<Integer>[] buck = new ArrayList[nums.length + 1];
 
-        //initialize
         for(int i = 0; i <= nums.length; i++){
             buck[i] = new ArrayList<>();
         }
 
-        //populate bucks
         for(HashMap.Entry<Integer, Integer> entry : map.entrySet()){
-
             int key = entry.getKey();
-            int freq = entry.getValue();
+            int val = entry.getValue();
 
-            buck[freq].add(key);
+            buck[val].add(key);
         }
 
         int[] ans = new int[k];
 
-        //go through bucks from the back 
-        int count = 0; 
+        int count = 0;
         for(int i = buck.length - 1; i >= 0; i--){
 
             for(int num : buck[i]){
