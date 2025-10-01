ff2e094f63f26d1a5c0bea2da8ada30cdbaaa6fd
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-01 13:12:35 -0400

    Time: 2 ms (87.27%) | Memory: 55.8 MB (31.53%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,29 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         
-        //calculate prefix left and prefix right
-        //multiply them tog
-
-        //to avoid space complexity, prefixLeft in one pass from the left, prefixRight in another pass from the right
+        //2 passes, left and right and return ans
 
         int[] ans = new int[nums.length];
-
         ans[0] = 1;
 
-        //first pass
         for(int i = 1; i < nums.length; i++){
-
             ans[i] = nums[i - 1] * ans[i - 1];
-
         }
 
         int post = 1;
-        //2nd pass
-        for(int i = nums.length - 1; i >= 0; i--){
 
+        for(int i = nums.length - 1; i >= 0; i--){
             ans[i] = ans[i] * post;
             post *= nums[i];
-
         }
 
         return ans;
