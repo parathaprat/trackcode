a1544d198b8fd5bef6dbf4b069d7e6f501435ca9
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-01 19:17:46 -0400

    Time: 31 ms (57.62%) | Memory: 67.6 MB (41.92%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,31 +1,24 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         
-        //use a set to store nums
-        //track longest var
-        //if -1 not in set, check if consec nums present with length
-        //update and return longest
-
         Set<Integer> set = new HashSet<>();
         int longest = 0;
 
-        //adding nums to set
-        for(int i = 0; i < nums.length; i++){
-            set.add(nums[i]);
+        for(int num : nums){
+            set.add(num);
         }
 
-        //if not iterated over the entire set (iterate over nums array) we get hit with a TLE in java due to duplicates
         for(int num : set){
 
             if(!set.contains(num - 1)){
 
-                int length = 0;
+                int len = 1;
 
-                while(set.contains(num + length)) {
-                    length++;
+                while(set.contains(num + len)){
+                    len++;
                 }
 
-                longest = Math.max(longest, length);
+                longest = Math.max(longest, len);
             }
         }
 
