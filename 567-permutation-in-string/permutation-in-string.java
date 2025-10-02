9f539cf8eca82d93e9b4d3264a21997aba6fd5ff
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-02 17:59:33 -0400

    Time: 6 ms (84.75%) | Memory: 42.8 MB (69.73%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
+
         if(s2.length() < s1.length()) return false;
 
         int[] freq1 = new int[26];
@@ -14,11 +14,12 @@ class Solution {
 
         for(int i = 0; i < len; i++){
             freq2[s2.charAt(i) - 'a']++;
-            if(Arrays.equals(freq1, freq2)) return true;
         }
 
-        for(int i = len; i < s2.length(); i++){
+        if(Arrays.equals(freq1, freq2)) return true;
 
+        for(int i = len ; i < s2.length(); i++){
+            
             freq2[s2.charAt(i) - 'a']++;
             freq2[s2.charAt(i - len) - 'a']--;
 
