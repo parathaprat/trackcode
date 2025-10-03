59dc2a810a9f83ce384705216c69f62613beb986
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-03 14:17:07 -0400

    Time: 42 ms (20.01%) | Memory: 61.9 MB (23.19%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         
-        LinkedList<Integer> index = new LinkedList<>();
+        List<Integer> index = new LinkedList<>();
         List<Integer> res = new ArrayList<>();
 
         for(int i = 0; i < nums.length; i++){
 
+            //if index out of window, remove
             while(!index.isEmpty() && index.getFirst() < i - k + 1){
                 index.removeFirst();
             }
 
+            //if last element is lower than current nums[i], will not be needed
             while(!index.isEmpty() && nums[i] > nums[index.getLast()]){
                 index.removeLast();
             }
-
+            
             index.add(i);
 
+            //if valid window, add last to res
             if(i >= k - 1){
                 res.add(nums[index.getFirst()]);
             }
