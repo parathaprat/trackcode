e51cced31818f24674a17e10ea213cb0df0495e7
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-03 13:47:13 -0400

    Time: 21 ms (33.76%) | Memory: 45.1 MB (53.05%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,36 +1,28 @@
 class Solution {
     public String minWindow(String s, String t) {
         
-        //sliding window problem
-
         int left = 0;
 
-        //maps for t and window
         Map<Character, Integer> countT = new HashMap<>();
         Map<Character, Integer> window = new HashMap<>();
 
-        //populating t map
         for(char c : t.toCharArray()){
             countT.put(c, countT.getOrDefault(c, 0) + 1);
         }
 
-        int have = 0; 
+        int have = 0;
         int need = countT.size();
 
-        //res = indices of substring, resLen = minLen
         int[] res = {-1, -1};
         int resLen = Integer.MAX_VALUE;
 
-        //slide window and compare freqs
         for(int right = 0; right < s.length(); right++){
 
             char c = s.charAt(right);
             window.put(c, window.getOrDefault(c, 0) + 1);
 
-            //satisfactory condition for that char
             if(countT.containsKey(c) && countT.get(c).equals(window.get(c))) have++;
 
-            //update minLen whenever condition met
             while(have == need){
 
                 if(right - left + 1 < resLen){
@@ -39,18 +31,14 @@ class Solution {
                     resLen = right - left + 1;
                 }
 
-                //reduce window
                 char leftc = s.charAt(left);
                 window.put(leftc, window.getOrDefault(leftc, 0) - 1);
 
                 if(countT.containsKey(leftc) && countT.get(leftc) > window.get(leftc)) have--;
-
                 left++;
             }
         }
 
         return resLen == Integer.MAX_VALUE? "" : s.substring(res[0], res[1] + 1);
-
-
     }
 }
