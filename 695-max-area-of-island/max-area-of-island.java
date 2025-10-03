786c4920275ac3cc38c9c70cf996ac495375faf3
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-03 16:11:19 -0400

    Time: 1 ms (100.00%) | Memory: 44.5 MB (82.45%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,17 +1,19 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        
+
         int max = 0;
 
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
 
-                int cur = dfs(i, j, grid);
-                max = Math.max(cur, max);
+                if(grid[i][j] == 1){
+                    int cur = dfs(i, j, grid);
+                    max = Math.max(cur, max);
+                }
             }
         }
 
-        return max;
+        return max; 
     }
 
     private int dfs(int i, int j, int[][] grid){
@@ -20,9 +22,6 @@ class Solution {
 
         grid[i][j] = 0;
 
-        return 1 + dfs(i + 1, j, grid)
-        + dfs(i - 1, j, grid)
-        + dfs(i, j + 1, grid)
-        + dfs(i, j - 1, grid);
+        return 1 + dfs(i + 1, j, grid) + dfs(i - 1, j, grid) + dfs(i, j + 1, grid) + dfs(i, j - 1, grid); 
     }
 }
