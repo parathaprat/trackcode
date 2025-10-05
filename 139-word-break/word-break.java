ea0e7dd8c18eac0b7efb7a49dcd331317edaefa4
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-04 21:08:03 -0400

    Time: 6 ms (60.69%) | Memory: 45.2 MB (7.86%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -2,10 +2,9 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         
         Set<String> set = new HashSet<>(wordDict);
-
         Boolean[] dp = new Boolean[s.length()];
-
         return getAns(s, dp, 0, set);
+
     }
 
     private boolean getAns(String s, Boolean[] dp, int ind, Set<String> set){
@@ -19,12 +18,11 @@ class Solution {
             if(set.contains(s.substring(ind, i + 1))){
 
                 if(getAns(s, dp, i + 1, set)){
-
                     return dp[ind] = true;
                 }
             }
         }
 
         return dp[ind] = false;
-    }
+    } 
 }
