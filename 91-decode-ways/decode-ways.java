a78ec7c79449e5a76a814054ce101f3648acde14
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-04 20:41:20 -0400

    Time: 0 ms (100.00%) | Memory: 41.9 MB (51.51%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numDecodings(String s) {
-
+        
         int[] dp = new int[s.length() + 1];
         Arrays.fill(dp, -1);
 
@@ -11,13 +11,13 @@ class Solution {
 
     private int getAns(int[] dp, String s, int i){
 
-        if(dp[i] != - 1) return dp[i];
+        if(dp[i] != -1) return dp[i];
 
         if(s.charAt(i) == '0') return 0;
 
         int res = getAns(dp, s, i + 1);
 
-        if(i + 1 < s.length() && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')){
+        if(i + 1 < s.length() && (s.charAt(i) == '1' || s.charAt(i) == '2' &&s.charAt(i + 1) < '7')){
             res += getAns(dp, s, i + 2);
         }
 
