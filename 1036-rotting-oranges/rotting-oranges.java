37810ba07da293d6b36d58d02d56b633f355d055
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-10-30 15:30:24 -0400

    Time: 1 ms (100.00%) | Memory: 42.9 MB (21.08%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,21 +1,21 @@
 class Solution {
 
     int fresh = 0;
+
     public int orangesRotting(int[][] grid) {
 
-        int time = 0;
         Queue<int[]> q = new LinkedList<>();
 
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
 
-                if(grid[i][j] == 2){
-                    q.offer(new int[]{i, j});
-                }
+                if(grid[i][j] == 2) q.offer(new int[]{i, j});
 
                 if(grid[i][j] == 1) fresh++;
             }
         }
+
+        int time = 0;
 
         while(!q.isEmpty() && fresh != 0){
 
@@ -25,24 +25,29 @@ class Solution {
 
                 int[] coord = q.poll();
 
-                addOrange(coord[0] + 1, coord[1], grid, q);
-                addOrange(coord[0], coord[1] + 1, grid, q);
-                addOrange(coord[0], coord[1] - 1, grid, q);
-                addOrange(coord[0] - 1, coord[1], grid, q);
+                addOranges(coord[0] + 1, coord[1], grid, q);
+                addOranges(coord[0] - 1, coord[1], grid, q);
+                addOranges(coord[0], coord[1] + 1, grid, q);
+                addOranges(coord[0], coord[1] - 1, grid, q);
+
             }
 
             time++;
         }
 
         return fresh == 0 ? time : -1;
+        
     }
 
-    private void addOrange(int i, int j, int[][] grid, Queue<int[]> q){
+    private void addOranges(int i, int j, int[][] grid, Queue<int[]> q){
 
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) return;
 
         grid[i][j] = 2;
         fresh--;
+
         q.offer(new int[]{i, j});
+
+        return;
     }
 }
