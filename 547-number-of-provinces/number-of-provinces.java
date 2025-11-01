e800f809cc7b966553975c61d278d8f781e5125f
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-01 19:36:36 -0400

    Time: 14 ms (5.05%) | Memory: 49.9 MB (5.40%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,60 +1,37 @@
 class Solution {
-
-    int[] parent;
-    int[] rank;
-
     public int findCircleNum(int[][] isConnected) {
 
-        int n = isConnected.length;
-        parent = new int[n];
-        rank = new int[n];
+        //BFS
 
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-            rank[i] = i;
-        }
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
 
-        int prov = n;
+        int prov = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
+        for(int i = 0; i < isConnected.length; i++){
 
-                if(isConnected[i][j] == 1){
-                    if(union(i, j)) prov--;
+            if(!visited.contains(i)){
+
+                q.offer(i);
+
+                while(!q.isEmpty()){
+
+                    int node = q.poll();
+                    visited.add(node);
+
+                    for(int j = 0; j < isConnected[node].length; j++){
+
+                        if(isConnected[node][j] == 1 && !visited.contains(j)){
+                            q.offer(j);
+                        }
+                    }
                 }
+
+                prov++;
             }
         }
 
         return prov;
-    }
-
-    private int find(int node){
-
-        int cur = node;
-
-        while(cur != parent[cur]){
-            parent[cur] = parent[parent[cur]];
-            cur = parent[cur];
-        }
-
-        return cur;
-    }
-
-    private boolean union(int u, int v){
-
-        int pu = find(u);
-        int pv = find(v);
-
-        if(pu == pv) return false;
-
-        if(rank[pu] < rank[pv]){
-            int temp = pv;
-            pv = pu;
-            pu = temp;
-        }
-
-        parent[pv] = pu;
-        rank[pu] += rank[pv];
-        return true;
+        
     }
 }
