2b275d40b76d70ca2d692856e097f110888d2aa7
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-14 17:32:36 -0500

    Time: 1 ms (95.37%) | Memory: 43.2 MB (12.99%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -5,26 +5,25 @@ class Solution {
         List<String> res = new ArrayList<>();
         List<String> cur = new ArrayList<>();
 
-        helper(s, 0, dict, cur, res);
+        helper(s, 0, dict, res, cur);
 
         return res;
+        
     }
 
-    private void helper(String s, int i, Set<String> dict, List<String> cur, List<String> res){
+    private void helper(String s, int i, Set<String> dict, List<String> res, List<String> cur){
 
         if(i == s.length()){
 
             if(cur.size() > 0){
 
                 StringBuilder sb = new StringBuilder();
-
                 for(int j = 0; j < cur.size(); j++){
 
                     if(j > 0) sb.append(' ');
-
                     sb.append(cur.get(j));
                 }
-                
+
                 res.add(sb.toString());
             }
 
@@ -36,8 +35,8 @@ class Solution {
             if(dict.contains(s.substring(i, j))){
 
                 cur.add(s.substring(i, j));
-                helper(s, j, dict, cur, res);
-                cur.remove(cur.size() - 1); //backtracking
+                helper(s, j, dict, res, cur);
+                cur.remove(cur.size() - 1);
             }
         }
     }
