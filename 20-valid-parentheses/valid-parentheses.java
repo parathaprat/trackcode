7622f322f4a9375cacb10ab7a9327252b1e736ed
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-14 15:00:48 -0500

    Time: 3 ms (64.82%) | Memory: 43.6 MB (5.79%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,23 +5,25 @@ class Solution {
 
         for(char c : s.toCharArray()){
 
-            if(c =='[' || c == '{' || c == '('){
-                st.add(c);
+            if(c == '[' || c == '{' || c == '('){
+                st.push(c);
             }
             else{
 
                 if(st.isEmpty()) return false;
 
-                if(c ==')' && st.peek() =='(' ||
-                    c == '}' && st.peek() == '{' ||
-                    c == ']' && st.peek() == '['){
-                
+                else if(c == '}' && st.peek() == '{' ||
+                        c == ']' && st.peek() == '[' ||
+                        c == ')' && st.peek() == '('
+                ){
                     st.pop();
                 }
+
                 else{
                     return false;
                 }
             }
+
         }
 
         return st.isEmpty();
