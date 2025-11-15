7848c8dfd095e254040d6127840350d2df6acfc4
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-15 16:32:15 -0500

    Time: 18 ms (68.67%) | Memory: 47.1 MB (16.27%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,11 +1,10 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        
+
         StringBuilder sb = new StringBuilder();
 
         int open = 0;
 
-        //first pass removed unmatched )
         for(char c : s.toCharArray()){
 
             if(c == '(') open++;
@@ -19,14 +18,14 @@ class Solution {
 
         StringBuilder result = new StringBuilder();
 
-        //2nd pass removed unmatched (
-        for(int i = sb.length() - 1; i >= 0; i--){
-            
+        for(int i = sb.length() - 1; i>= 0; i--){
+
             if(sb.charAt(i) == '(' && open-- > 0) continue;
 
             result.append(sb.charAt(i));
         }
 
         return result.reverse().toString();
+        
     }
 }
