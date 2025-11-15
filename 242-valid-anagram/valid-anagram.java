889db33d8bdd01bd7edd4b9958fd51fe19696b41
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-14 19:05:30 -0500

    Time: 5 ms (56.73%) | Memory: 44.6 MB (45.89%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        int freq[] = new int[26];
+
+        int[] freq = new int[26];
 
         for(char c : s.toCharArray()){
             freq[c - 'a']++;
@@ -16,5 +16,6 @@ class Solution {
         }
 
         return true;
+        
     }
 }
