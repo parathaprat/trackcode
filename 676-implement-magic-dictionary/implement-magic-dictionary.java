be8811dfc275af5ecc3e663312f7ef37abc7a43d
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-15 15:19:54 -0500

    Time: 20 ms (97.83%) | Memory: 47.1 MB (54.70%) - LeetSync

---
+++ 676-implement-magic-dictionary/implement-magic-dictionary.java
@@ -0,0 +1,54 @@
+class MagicDictionary {
+
+    //group words by length into a map
+    //compare only same letter words
+
+    Map<Integer, List<String>> map;
+
+    public MagicDictionary() {
+        map = new HashMap<>();
+    }
+    
+    public void buildDict(String[] dictionary) {
+
+        for(String word : dictionary){
+
+            if(!map.containsKey(word.length())){
+                map.put(word.length(), new ArrayList<>());
+            }
+
+            map.get(word.length()).add(word);
+        }
+        
+    }
+    
+    public boolean search(String searchWord) {
+
+        int len = searchWord.length();
+
+        if(!map.containsKey(len)) return false;
+
+        for(String word : map.get(len)){
+
+            int diff = 0;
+
+            for(int i = 0; i < len; i++){
+
+                if(word.charAt(i) != searchWord.charAt(i)) diff++;
+
+                if(diff > 1) break;
+            }
+
+            if(diff == 1) return true;
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary obj = new MagicDictionary();
+ * obj.buildDict(dictionary);
+ * boolean param_2 = obj.search(searchWord);
+ */
