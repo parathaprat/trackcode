044e6c219561b671d069e9aac98211c29ecb8817
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-16 18:56:09 -0500

    Time: 7 ms (24.04%) | Memory: 48.5 MB (5.08%) - LeetSync

---
+++ 252-meeting-rooms/meeting-rooms.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+
+        if(intervals.length == 0) return true;
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int[] in = intervals[0];
+
+        for(int i = 1; i < intervals.length; i++){
+
+            if(in[1] > intervals[i][0]){
+                return false;
+            }
+            
+            in = intervals[i];
+        }
+
+        return true;
+        
+    }
+}
