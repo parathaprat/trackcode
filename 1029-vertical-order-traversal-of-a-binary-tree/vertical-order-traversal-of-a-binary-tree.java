c42c1a66a5ace8b05a6b52845d0096c4a29337e4
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-17 15:20:53 -0500

    Time: 4 ms (42.92%) | Memory: 44.1 MB (10.45%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+        //col -> row, val
+        Map<Integer, PriorityQueue<int[]>> map = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        Queue<int[]> meta = new LinkedList<>(); //row, col
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        if(root == null) return res;
+
+        q.add(root);
+        meta.add(new int[]{0, 0});
+
+        int min = 0;
+        int max = 0;
+
+        while(!q.isEmpty()){
+
+            TreeNode node = q.poll();
+            int[] rc = meta.poll();
+
+            int row = rc[0];
+            int col = rc[1];
+
+            map.putIfAbsent(col, new PriorityQueue<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]));
+
+            map.get(col).add(new int[]{row, node.val});
+
+            if(node.left != null){
+                q.add(node.left);
+                meta.add(new int[]{row + 1, col - 1});
+                min = Math.min(min, col - 1);
+            }
+
+            if(node.right != null){
+                q.add(node.right);
+                meta.add(new int[]{row + 1, col + 1});
+                max = Math.max(max, col + 1);
+            }      
+        }
+
+        for(int i = min; i <= max; i++){
+            PriorityQueue<int[]> pq = map.get(i);
+            List<Integer> list = new ArrayList<>();
+
+            while(!pq.isEmpty()){
+                list.add(pq.poll()[1]);
+            }
+
+            res.add(list);
+        }
+
+        
+
+        return res;
+
+        
+    }
+}
