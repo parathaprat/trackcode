0a5d17ecadc839375f9bd1f9c3c28d4cfaa3c1f3
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-18 18:32:08 -0500

    Time: 6 ms (66.33%) | Memory: 42.9 MB (38.03%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -4,33 +4,24 @@ class Solution {
 
     public int countSubstrings(String s) {
 
-        //odd palindrome: 1 char center
-        //even palindrome: 2 char center
-
-        //go through each char, check for odd and even palindroms, ret ans
-
         for(int i = 0; i < s.length(); i++){
+
             checkPal(i, i, s);
             checkPal(i, i + 1, s);
+
         }
 
-        return ans;        
+        return ans;
     }
 
-    private void checkPal(int start, int end, String s){
+    private void checkPal(int l, int r, String s){
 
-        // if(s == null || start > end) return;
-
-        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end) && s != null && start <= end){
+        while(l >= 0 && r < s.length() && l <= r && s != null && s.charAt(l) == s.charAt(r)){
             ans++;
-            start--;
-            end++;
+            l--;
+            r++;
         }
 
         return;
-
-
-
-        
     }
 }
