3c9ab5e23405b0ee21ae8cd5627a1a6321cc4179
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-18 17:13:42 -0500

    Time: 0 ms (100.00%) | Memory: 47.3 MB (24.41%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -16,8 +16,10 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
 
-        //helper -> returns replacement subtree
-        //helper finds rightMost node in left subtree of node to be deleted, connects the right subtree to it and returns root.left(root deleted, left subtree with right attached returned)
+        //traverse tree, find key
+        //find rightmost of left subtree OR leftMost of right subtree
+        //rightMostLeft.right = rightST, leftMostRight.left = leftMost
+        //return root.left, return root.right
 
         if(root == null) return null;
         if(root.val == key) return helper(root);
@@ -27,44 +29,43 @@ class Solution {
         while(root != null){
 
             if(root.val < key){
+
                 if(root.right != null && root.right.val == key){
                     root.right = helper(root.right);
                     break;
                 }
-                else{
-                    root = root.right;
-                }
+                else root = root.right;
             }
             else{
+
                 if(root.left != null && root.left.val == key){
                     root.left = helper(root.left);
                     break;
                 }
-                else{
-                    root= root.left;
-                }
+                else root = root.left;
             }
         }
 
         return dummy;
+        
     }
 
     private TreeNode helper(TreeNode root){
 
-        if(root.left == null) return root.right;
         if(root.right == null) return root.left;
+        if(root.left == null) return root.right;
 
-        TreeNode rightChild = root.right;
-        TreeNode lastRight = findRight(root.left);
+        TreeNode leftSubtree = root.left;
+        TreeNode leftMost = findLeft(root.right);
 
-        lastRight.right = rightChild;
+        leftMost.left = leftSubtree;
 
-        return root.left;
+        return root.right;
     }
 
-    private TreeNode findRight(TreeNode root){
+    private TreeNode findLeft(TreeNode root){
 
-        if(root.right == null) return root;
-        return findRight(root.right);
+        if(root.left == null) return root;
+        return findLeft(root.left);
     }
 }
