1c54410536d37f90f311818c7248c0b1600e4a4f
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-17 20:31:44 -0500

    Time: 0 ms (100.00%) | Memory: 47.1 MB (20.52%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,32 +1,19 @@
 class Solution {
     public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+        int sum = 0;
+
+        for(int i = 0; i <= n; i++){
+            sum += i;
+        }
+
+        for(int num : nums){
+            sum -= num;
+        }
+
+        return sum;
         
-        //cyclic sort
-        //nums[i] should be placed at i if possible
-
-        for(int i = 0; i < nums.length; i++){
-
-            //if num is not equal to its index and is in range, swap
-            if(nums[i] != i && nums[i] < nums.length){
-
-                int temp = nums[nums[i]];
-
-                nums[nums[i]] = nums[i];
-
-                nums[i] = temp;
-
-                i--;
-                
-            }
-        }
-
-        for(int i = 0; i < nums.length; i++){
-
-            if(nums[i] != i){
-                return i;
-            }
-        }
-
-        return nums.length;
     }
 }
