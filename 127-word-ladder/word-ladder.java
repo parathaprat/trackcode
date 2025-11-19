d1355054d37dc08d9f9528be9785e434f0d8c414
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-19 13:31:55 -0500

    Time: 194 ms (18.19%) | Memory: 48.4 MB (28.06%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,17 +1,12 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         
-        //Hashset -> wordList
-        //Queue -> BFS
-        
         if(beginWord.equals(endWord) || !wordList.contains(endWord)) return 0;
 
         Set<String> dict = new HashSet<>(wordList);
-
         Queue<String> q = new LinkedList<>();
 
         q.add(beginWord);
-
         int ans = 0;
 
         while(!q.isEmpty()){
@@ -22,28 +17,25 @@ class Solution {
 
             for(int i = 0; i < level; i++){
 
-                String word = q.poll();
+                
 
+                String word = q.poll();
                 if(word.equals(endWord)) return ans;
 
                 for(int j = 0; j < word.length(); j++){
-
                     for(char c = 'a'; c <= 'z'; c++){
 
                         if(word.charAt(j) == c) continue;
 
                         String nei = word.substring(0, j) + c + word.substring(j + 1);
-                        
-                        if(dict.contains(nei)){
 
+                        if(dict.contains(nei)){
                             dict.remove(nei);
                             q.add(nei);
                         }
-                    
                     }
                 }
             }
-
         }
 
         return 0;
