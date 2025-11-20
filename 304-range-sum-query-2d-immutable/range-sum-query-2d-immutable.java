3be4a7d901c824d097e2922c75c4dc2eb7ebfaa0
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-19 20:59:47 -0500

    Time: 109 ms (33.19%) | Memory: 140.2 MB (29.04%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -21,13 +21,15 @@ class NumMatrix {
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        
+
         int total = matrix[row2][col2];
-        int extra = (col1 != 0 ? matrix[row2][col1 - 1] : 0) + 
-                    (row1 != 0 ? matrix[row1 - 1][col2] : 0) -
-                    ((row1 != 0 && col1 != 0) ? matrix[row1 - 1][col1 - 1] : 0); 
+
+        int extra = (row1 == 0 ? 0 : matrix[row1 - 1][col2]) + 
+                    (col1 == 0 ? 0 : matrix[row2][col1 - 1]) - 
+                    (row1 != 0 && col1 != 0 ? matrix[row1 - 1][col1 -1] : 0);
 
         return total - extra;
+        
     }
 }
 
