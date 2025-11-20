6c59c21deacce5333a4dfe1b23431e419958b94d
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-20 16:54:25 -0500

    Time: 11 ms (69.87%) | Memory: 47.2 MB (20.52%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.java
+++ 1272-invalid-transactions/invalid-transactions.java
@@ -1,5 +1,9 @@
 class Solution {
 
+    //create a transaction class for easy access
+    //1st pass transactions to populate map (name -> list transaction)
+    //2nd pass to check validity, adding to ans list
+
     class Transaction{
 
         String name;
@@ -8,21 +12,21 @@ class Solution {
         String city;
 
         Transaction(String line){
-            String[] tran = line.split(",");
-            name = tran[0];
-            time = Integer.parseInt(tran[1]);
-            amount = Integer.parseInt(tran[2]);
-            city = tran[3];
-        }
 
+            String[] split = line.split(",");
+            name = split[0];
+            time = Integer.parseInt(split[1]);
+            amount = Integer.parseInt(split[2]);
+            city = split[3];
+
+        }
     }
+
     public List<String> invalidTransactions(String[] transactions) {
 
-        List<String> invalid = new ArrayList<>();
-        //name -> transactions
-        Map<String, List<Transaction>> map = new HashMap<>();
+        List<String> res = new ArrayList<>();
+        HashMap<String, List<Transaction>> map = new HashMap<>();
 
-        //first pass -> populate map
         for(String transaction : transactions){
 
             Transaction t = new Transaction(transaction);
@@ -32,26 +36,24 @@ class Solution {
 
         }
 
-        //2nd pass -> check validity
         for(String transaction : transactions){
 
             Transaction t = new Transaction(transaction);
 
-            if(!isValid(t, map.getOrDefault(t.name, new ArrayList<>()))){
-                invalid.add(transaction);
-            }
+            if(!isValid(t, map)) res.add(transaction);
         }
 
-        return invalid;
+        return res;
     }
 
-    private boolean isValid(Transaction t, List<Transaction> list){
+    private boolean isValid(Transaction t, Map<String, List<Transaction>> map){
 
         if(t.amount > 1000) return false;
 
-        for(Transaction ta : list){
-
+        for(Transaction ta : map.get(t.name)){
+            
             if(Math.abs(ta.time - t.time) <= 60 && !ta.city.equals(t.city)) return false;
+
         }
 
         return true;
