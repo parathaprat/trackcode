a3797adacf283d12d29a72e584fa241d252de1e0
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-21 00:06:19 -0500

    Time: 0 ms (100.00%) | Memory: 42.8 MB (21.72%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.java
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.java
@@ -1,43 +1,34 @@
 class Solution {
     public boolean equalFrequency(String word) {
 
-        //get a freq array
-        //reduce freq of each letter, check if all freq vals match
-
         int[] freq = new int[26];
 
         for(char c : word.toCharArray()){
-
             freq[c - 'a']++;
         }
 
         for(char c : word.toCharArray()){
-
             freq[c - 'a']--;
 
-            if(checkFreq(freq)) return true;
+            if(check(freq)) return true;
 
             freq[c - 'a']++;
-
         }
 
-        return false;
-        
+        return false;   
     }
 
-    private boolean checkFreq(int[] freq){
+    private boolean check(int[] freq){
 
         int fr = 0;
 
         for(int num : freq){
 
             if(num == 0) continue;
-
             if(fr == 0) fr = num;
-
             if(fr == num) continue;
-
             else return false;
+
         }
 
         return true;
