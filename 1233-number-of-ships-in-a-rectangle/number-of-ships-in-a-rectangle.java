e81954ce7d43b27283349e220200b8c2578e577e
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-21 02:29:51 -0500

    Time: 0 ms (100.00%) | Memory: 43.2 MB (8.90%) - LeetSync

--- 1233-number-of-ships-in-a-rectangle/number-of-ships-in-a-rectangle.java
+++ 1233-number-of-ships-in-a-rectangle/number-of-ships-in-a-rectangle.java
@@ -11,28 +11,39 @@ class Solution {
 
         int res = 0;
 
-        if(bottomLeft[0] <= topRight[0] && bottomLeft[1] <= topRight[1] && sea.hasShips(topRight, bottomLeft) == true){
+        if(bottomLeft[0] <= topRight[0] && bottomLeft[1] <= topRight[1] && sea.hasShips(topRight, bottomLeft)){
 
-            if(topRight[0] == bottomLeft[0] && topRight[1] == bottomLeft[1]) return 1;
+            if(bottomLeft[1] == topRight[1] && bottomLeft[1] == topRight[1]) return 1;
 
             int newX = (topRight[0] + bottomLeft[0])/2;
             int newY = (topRight[1] + bottomLeft[1])/2;
 
             //topLeft
-            res += countShips(sea, new int[]{newX, topRight[1]}, new int[]{bottomLeft[0], newY + 1});
+            int[] topLeftTopRight = {newX, topRight[1]};
+            int[] topLeftBottomRight = {bottomLeft[0], newY + 1};
+
+            res += countShips(sea, topLeftTopRight, topLeftBottomRight);
 
             //topRight
-            res += countShips(sea, topRight, new int[]{newX + 1, newY + 1});
+            int[] topRightTopRight = topRight;
+            int[] topRightBottomLeft = {newX + 1, newY + 1};
+
+            res += countShips(sea, topRightTopRight, topRightBottomLeft);
 
             //bottomRight
-            res += countShips(sea, new int[]{topRight[0], newY}, new int[]{newX + 1, bottomLeft[1]});
+            int[] bottomRightTopRight = {topRight[0], newY};
+            int[] bottomRightBottomLeft = {newX + 1, bottomLeft[1]};
+
+            res += countShips(sea, bottomRightTopRight, bottomRightBottomLeft);
 
             //bottomLeft
-            res += countShips(sea, new int[]{newX, newY}, bottomLeft);
+            int[] bottomLeftTopRight = {newX, newY};
+            int[] bottomLeftBottomLeft = bottomLeft;
 
+            res += countShips(sea, bottomLeftTopRight, bottomLeftBottomLeft);
         }
 
         return res;
-
+        
     }
 }
