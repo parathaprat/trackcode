72ba272b3f88e85b1edb18fc7b42be4457403d90
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-20 19:03:19 -0500

    Time: 1 ms (95.56%) | Memory: 42.9 MB (27.70%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,27 +1,32 @@
 class Solution {
+
+    //form valid words in cur list recrusively
+    //use sb to concatenate, and add to list when wne do the string is reached
+
     public List<String> wordBreak(String s, List<String> wordDict) {
 
-        List<String> res = new ArrayList<>();
-        List<String> cur =  new ArrayList<>();
         Set<String> set = new HashSet<>(wordDict);
+        List<String> res = new ArrayList<>();
+        List<String> cur = new ArrayList<>();
 
-        helper(0, res, cur, set, s);
+        helper(0, set, res, cur, s);
 
         return res;
-        
     }
 
-    private void helper(int ind, List<String> res, List<String> cur, Set<String> set, String s){
+    private void helper(int ind, Set<String> set, List<String> res, List<String> cur, String s){
 
         if(ind >= s.length()){
 
-            if(cur.size() > 0){
-                
+            if(cur.size() != 0){
+
                 StringBuilder sb = new StringBuilder();
+
                 for(int i = 0; i < cur.size(); i++){
 
                     if(i > 0) sb.append(" ");
                     sb.append(cur.get(i));
+
                 }
 
                 res.add(sb.toString());
@@ -31,12 +36,11 @@ class Solution {
         for(int i = ind; i < s.length(); i++){
 
             if(set.contains(s.substring(ind, i + 1))){
+
                 cur.add(s.substring(ind, i + 1));
-                helper(i + 1, res, cur, set, s);
+                helper(i + 1, set, res, cur, s);
                 cur.remove(cur.size() - 1);
             }
         }
-
-
     }
 }
