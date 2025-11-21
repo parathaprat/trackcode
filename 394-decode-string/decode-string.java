e7a61133a9547bc40255149096da3d1278075d85
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-21 16:47:42 -0500

    Time: 1 ms (68.02%) | Memory: 42.8 MB (17.32%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -2,46 +2,42 @@ class Solution {
     public String decodeString(String s) {
 
         Stack<Integer> st = new Stack<>();
-        Stack<StringBuilder> st1 = new Stack<>();
-        StringBuilder sb = new StringBuilder();
+        Stack<StringBuilder> sbStack = new Stack<>();
+
         int n = 0;
+
+        StringBuilder sb = new StringBuilder();
 
         for(char c : s.toCharArray()){
 
-            //if number store and wait for more digits
             if(Character.isDigit(c)){
                 n = n * 10 + (c - '0');
             }
-
-            //if opening, push to stack, reset number and sb
             else if(c == '['){
+
                 st.push(n);
                 n = 0;
 
-                st1.push(sb);
+                sbStack.push(sb);
                 sb = new StringBuilder();
             }
-
-            //if closing, take top number, store sb in temp; take top of sb stack, and append temp k times to sb.
             else if(c == ']'){
-                
+
                 int k = st.pop();
+
                 StringBuilder temp = sb;
+                sb = sbStack.pop();
 
-                sb = st1.pop();
-
-                while(k-- > 0){
+                while(k > 0){
                     sb.append(temp);
+                    k--;
                 }
             }
-            //if nothing, just append to sb
-            else{
-                sb.append(c);
-            }
+
+            else sb.append(c);
         }
 
         return sb.toString();
-
         
     }
 }
