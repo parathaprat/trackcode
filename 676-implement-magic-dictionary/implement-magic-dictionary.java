cf09a238002e212e9de79613cf91e54726d24caf
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-20 21:55:07 -0500

    Time: 26 ms (71.79%) | Memory: 46.8 MB (67.61%) - LeetSync

--- 676-implement-magic-dictionary/implement-magic-dictionary.java
+++ 676-implement-magic-dictionary/implement-magic-dictionary.java
@@ -1,48 +1,42 @@
 class MagicDictionary {
 
-    //map -> len to list of words in dictionary
-
+    //map -> length to list of words of that length in a dictionary
     Map<Integer, List<String>> map;
 
     public MagicDictionary() {
-
         map = new HashMap<>();
-        
     }
     
     public void buildDict(String[] dictionary) {
-        
+
         for(String word : dictionary){
 
             int len = word.length();
 
             map.putIfAbsent(len, new ArrayList<>());
             map.get(len).add(word);
+
         }
+        
     }
     
     public boolean search(String searchWord) {
 
         int len = searchWord.length();
 
-        if(!map.containsKey(len)) return false;
+        for(String word : map.getOrDefault(len, new ArrayList<>())){
 
-        int diff = 0;
-
-        for(String words : map.get(len)){
-
-            diff = 0;
+            int diff = 0;
 
             for(int i = 0; i < len; i++){
-
-                if(words.charAt(i) != searchWord.charAt(i)) diff++;
+                
+                if(word.charAt(i) != searchWord.charAt(i)) diff++;
 
                 if(diff > 1) continue;
-
             }
 
-            if(diff == 1) return true;
 
+            if(diff == 1) return true;
         }
 
         return false;
