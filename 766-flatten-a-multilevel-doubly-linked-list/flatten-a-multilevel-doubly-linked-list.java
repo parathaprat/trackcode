255adf142df1c92232718bd71ea91a7064272664
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-20 19:40:49 -0500

    Time: 0 ms (100.00%) | Memory: 43.4 MB (21.72%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -11,33 +11,27 @@ class Node {
 class Solution {
     public Node flatten(Node head) {
 
-        //traverse with a cur
-        //if child, store next, find child tail
-        //childTail.next = next; next.prev to child tail
-        //cur.next to child
-
-        if(head == null) return null;
+        //if child, navigate to end of child
+        //childlast.next = node.next
+        //node.next.prev = childlast
+        //node.next = child
 
         Node cur = head;
 
         while(cur != null){
 
             if(cur.child != null){
-
-                Node next = cur.next;
+                
                 Node childLast = cur.child;
 
                 while(childLast.next != null) childLast = childLast.next;
 
-                if(next != null){
-                    childLast.next = next;
-                    next.prev = childLast;
-                }
-
+                childLast.next = cur.next;
+                if(cur.next != null) cur.next.prev = childLast;
                 cur.child.prev = cur;
                 cur.next = cur.child;
-                cur.child = null;
 
+                cur.child = null;
             }
 
             cur = cur.next;
