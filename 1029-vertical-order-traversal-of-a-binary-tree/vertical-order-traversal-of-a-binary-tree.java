d69077884e9ce0047cb2df3a73c46db7982e0a69
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-22 01:04:57 -0500

    Time: 3 ms (82.40%) | Memory: 44.2 MB (8.93%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -16,12 +16,13 @@
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
 
-        //vertical order trav -> nodes to be sorted by col, row, val
-        //use min. max to sort by col, and pq to sort by row then val
+        //sort nodes by col, then row, then val
+        //cols = min to max iterations
+        //row, val through a pq
 
-        Map<Integer, PriorityQueue<int[]>> map = new HashMap<>(); //col -> row, val;
+        Map<Integer, PriorityQueue<int[]>> map = new HashMap<>(); //col -> row, val
         Queue<TreeNode> q = new LinkedList<>();
-        Queue<int[]> meta = new LinkedList<>(); //row, col
+        Queue<int[]> meta = new LinkedList<>();
 
         List<List<Integer>> res = new ArrayList<>();
         if(root == null) return res;
@@ -29,7 +30,6 @@ class Solution {
         q.add(root);
         meta.add(new int[]{0, 0});
 
-        //min and max col to iterate through for final res building
         int min = 0;
         int max = 0;
 
@@ -41,17 +41,14 @@ class Solution {
             int row = rc[0];
             int col = rc[1];
 
-            //populate map; pq sorts by row, if same sorts by val
             map.putIfAbsent(col, new PriorityQueue<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]));
             map.get(col).add(new int[]{row, node.val});
 
-            //add child nodes to q, and update min and max for traversal later
             if(node.left != null){
                 q.add(node.left);
                 meta.add(new int[]{row + 1, col - 1});
                 min = Math.min(min, col - 1);
             }
-
             if(node.right != null){
                 q.add(node.right);
                 meta.add(new int[]{row + 1, col + 1});
@@ -61,14 +58,15 @@ class Solution {
 
         for(int i = min; i <= max; i++){
 
+            List<Integer> level = new ArrayList<>();
             PriorityQueue<int[]> pq = map.get(i);
-            List<Integer> list = new ArrayList<>();
 
             while(!pq.isEmpty()){
-                list.add(pq.poll()[1]);
+                level.add(pq.poll()[1]);
             }
 
-            res.add(list);
+            res.add(level);
+
         }
 
         return res;
