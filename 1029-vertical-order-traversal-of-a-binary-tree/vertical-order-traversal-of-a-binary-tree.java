0e64b9e657d937957cf28157972db7fbf7955c41
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-22 00:52:58 -0500

    Time: 4 ms (44.38%) | Memory: 43.7 MB (28.14%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -16,18 +16,20 @@
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
 
-        //col -> row, val
-        Map<Integer, PriorityQueue<int[]>> map = new HashMap<>();
+        //vertical order trav -> nodes to be sorted by col, row, val
+        //use min. max to sort by col, and pq to sort by row then val
+
+        Map<Integer, PriorityQueue<int[]>> map = new HashMap<>(); //col -> row, val;
         Queue<TreeNode> q = new LinkedList<>();
         Queue<int[]> meta = new LinkedList<>(); //row, col
 
         List<List<Integer>> res = new ArrayList<>();
-
         if(root == null) return res;
 
         q.add(root);
         meta.add(new int[]{0, 0});
 
+        //min and max col to iterate through for final res building
         int min = 0;
         int max = 0;
 
@@ -39,10 +41,11 @@ class Solution {
             int row = rc[0];
             int col = rc[1];
 
+            //populate map; pq sorts by row, if same sorts by val
             map.putIfAbsent(col, new PriorityQueue<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]));
-
             map.get(col).add(new int[]{row, node.val});
 
+            //add child nodes to q, and update min and max for traversal later
             if(node.left != null){
                 q.add(node.left);
                 meta.add(new int[]{row + 1, col - 1});
@@ -53,10 +56,11 @@ class Solution {
                 q.add(node.right);
                 meta.add(new int[]{row + 1, col + 1});
                 max = Math.max(max, col + 1);
-            }      
+            }
         }
 
         for(int i = min; i <= max; i++){
+
             PriorityQueue<int[]> pq = map.get(i);
             List<Integer> list = new ArrayList<>();
 
@@ -67,10 +71,6 @@ class Solution {
             res.add(list);
         }
 
-        
-
         return res;
-
-        
     }
 }
