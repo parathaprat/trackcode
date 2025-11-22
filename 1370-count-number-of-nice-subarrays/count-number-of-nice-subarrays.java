39ff2b828cad78138b1d5cfed37a1378094bd54b
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-22 15:56:02 -0500

    Time: 6 ms (96.52%) | Memory: 55.6 MB (39.78%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -0,0 +1,24 @@
+
+import java.lang.classfile.AnnotationValue.OfAnnotation;class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+
+        int[] prefixCount = new int[nums.length + 1];
+        prefixCount[0] = 1;
+
+        int oddCountSoFar = 0;
+        int result = 0;
+
+        for(int num : nums){
+
+            if(num % 2 == 1) oddCountSoFar++;
+
+            if(oddCountSoFar - k >= 0){
+                result += prefixCount[oddCountSoFar - k];
+            }
+
+            prefixCount[oddCountSoFar]++;
+        }        
+
+        return result;
+    }
+}
