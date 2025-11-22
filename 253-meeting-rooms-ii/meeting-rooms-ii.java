faeb4862953f76f738067bc51f166700871cdbcf
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-21 19:29:33 -0500

    Time: 8 ms (76.74%) | Memory: 48.5 MB (18.88%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,23 +1,21 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
 
-        //we want to reuse the room with the earliest end times
-        //sort meetings by start times, create a min heap of end times
-        //if ending <= start(next), pq.poll(), since room can be resused
-        //add end times to meetings, return pq.size()
+        //sort by start times
+        //min heap of end times to determine if room can be freed
+        //return size of heap 
 
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int[] in : intervals){
 
-            if(!pq.isEmpty() && pq.peek() <= in[0]){
-                pq.poll();
-            }
-
-            pq.offer(in[1]);
+            if(!pq.isEmpty() && pq.peek() <= in[0]) pq.poll();
+            pq.add(in[1]);
         }
 
         return pq.size();
+        
     }
 }
