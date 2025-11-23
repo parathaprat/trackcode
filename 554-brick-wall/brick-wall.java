3b2a22de17166b2a098c62a6bd7420c1950906d7
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-22 22:09:49 -0500

    Time: 15 ms (33.54%) | Memory: 51.5 MB (5.34%) - LeetSync

--- 554-brick-wall/brick-wall.java
+++ 554-brick-wall/brick-wall.java
@@ -1,16 +1,16 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
 
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>(); //position -> no of gaps
+
         int max = 0;
-        
-        for(List<Integer> rows : wall){
+
+        for(List<Integer> row : wall){
 
             int gapPos = 0;
 
-            for(int i = 0; i < rows.size() - 1; i++){
-
-                gapPos += rows.get(i);
+            for(int i = 0; i < row.size() - 1; i++){
+                gapPos += row.get(i);
 
                 map.put(gapPos, map.getOrDefault(gapPos, 0) + 1);
                 max = Math.max(max, map.get(gapPos));
@@ -18,6 +18,7 @@ class Solution {
         }
 
         return wall.size() - max;
+
         
     }
 }
