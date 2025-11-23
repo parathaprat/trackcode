dee287739eaba0b82ad787e3962df5a888a62a59
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-22 20:28:32 -0500

    Time: 0 ms (100.00%) | Memory: 47.4 MB (24.11%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -16,56 +16,53 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
 
-        //traverse tree, find key
-        //find rightmost of left subtree OR leftMost of right subtree
-        //rightMostLeft.right = rightST, leftMostRight.left = leftMost
-        //return root.left, return root.right
-
         if(root == null) return null;
         if(root.val == key) return helper(root);
 
         TreeNode dummy = root;
 
         while(root != null){
-
             if(root.val < key){
 
                 if(root.right != null && root.right.val == key){
                     root.right = helper(root.right);
                     break;
                 }
-                else root = root.right;
+                else{
+                    root = root.right;
+                }
             }
             else{
-
                 if(root.left != null && root.left.val == key){
                     root.left = helper(root.left);
                     break;
                 }
-                else root = root.left;
+                else{
+                    root = root.left;
+                }
             }
         }
 
         return dummy;
-        
     }
 
+    //connect leftMost of right subtree to left subtree
     private TreeNode helper(TreeNode root){
 
-        if(root.right == null) return root.left;
         if(root.left == null) return root.right;
+        if(root.right == null) return root.left;
 
         TreeNode leftSubtree = root.left;
-        TreeNode leftMost = findLeft(root.right);
+        TreeNode leftMost = findLeftMost(root.right);
 
         leftMost.left = leftSubtree;
 
         return root.right;
     }
 
-    private TreeNode findLeft(TreeNode root){
+    private TreeNode findLeftMost(TreeNode root){
 
         if(root.left == null) return root;
-        return findLeft(root.left);
+        return findLeftMost(root.left);
     }
 }
