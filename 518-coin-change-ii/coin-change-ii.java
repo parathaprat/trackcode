35791e4c0b098a492bfd86c808063e95c17c51a4
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-22 23:14:02 -0500

    Time: 5 ms (98.99%) | Memory: 43.1 MB (66.03%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,17 +1,19 @@
 class Solution {
     public int change(int amount, int[] coins) {
 
-        // dp[x] = number of ways to form amount x using the coins seen so far.
-        
         int[] dp = new int[amount + 1];
         dp[0] = 1;
 
         for(int coin : coins){
-            for(int j = coin; j <= amount; j++){
-                dp[j] += dp[j - coin];
+
+            for(int i = coin; i <= amount; i++){
+                dp[i] += dp[i - coin];
             }
         }
-        
+
         return dp[amount];
+
+        
+        
     }
 }
