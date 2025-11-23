8d6265bfd3aa7527246392eb6974505dbe10a4a6
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-23 02:10:02 -0500

    Time: 3 ms (91.48%) | Memory: 46.4 MB (28.82%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,30 +1,24 @@
 class Solution {
     public String convert(String s, int numRows) {
 
-        //2 * (numRows - 1)
-        //Intermidete = row + (cycle - 2 * row)
-
         if(numRows == 1) return s;
 
         StringBuilder sb = new StringBuilder();
-
         int cycle = 2 * (numRows - 1);
 
         for(int row = 0; row < numRows; row++){
-            for(int j = row; j < s.length(); j += cycle){
+            for(int j = row; j < s.length(); j+= cycle){
 
                 sb.append(s.charAt(j));
 
+                boolean isMiddle = row > 0 && row < numRows - 1;
                 int diagonalIndex = j + (cycle - 2 * row);
 
-                boolean isMiddleRow = row > 0 && row < numRows - 1;
-
-                if(isMiddleRow && diagonalIndex < s.length()){
-                    sb.append(s.charAt(diagonalIndex));
-                }
+                if(isMiddle && diagonalIndex < s.length()) sb.append(s.charAt(diagonalIndex));
             }
         }
 
         return sb.toString();
+        
     }
 }
