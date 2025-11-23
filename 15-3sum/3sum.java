95c98135493d8cdf6ccbc90143bb6d670dc8dae8
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-23 03:16:16 -0500

    Time: 35 ms (31.82%) | Memory: 58.8 MB (29.88%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -2,36 +2,35 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
         Arrays.sort(nums);
-
         List<List<Integer>> res = new ArrayList<>();
 
         for(int i = 0; i < nums.length; i++){
 
-            if(i == 0 || nums[i] != nums[i - 1]){
-                
-                int left = i + 1;
-                int right = nums.length - 1;
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
 
-                int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
 
-                while(left < right){
+            int target = -nums[i];
 
-                    if(nums[left] + nums[right] == target){
-                        res.add(Arrays.asList(nums[left], nums[right], nums[i]));
+            while(left < right){
 
-                        while(left < right && nums[left] == nums[left + 1]) left++;
-                        while(left < right && nums[right] == nums[right - 1]) right--;
+                if(nums[left] + nums[right] == target){
 
-                        left++;
-                        right--;
-                    }
-                    else if(nums[left] + nums[right] < target){
-                        left++;
-                    }
-                    else{
-                        right--;
-                    }
-                }   
+                    res.add(Arrays.asList(nums[left], nums[right], nums[i]));
+
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if(nums[left] + nums[right] < target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
             }
         }
 
