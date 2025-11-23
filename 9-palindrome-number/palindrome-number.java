4245c8ac45af7a62ac552c37f7d6395891cc1532
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-23 03:43:50 -0500

    Time: 4 ms (100.00%) | Memory: 45.7 MB (26.35%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
 
-        //-ve, numbers ending in 0 cannot be palindroms
-        //reverse hald the list, return true accordingly 
-
-        if(x < 0 || (x % 10 == 0 && x != 0)) return false;
+        if(x < 0 || (x%10 == 0 && x != 0)) return false;
 
         int reverse = 0;
 
         while(x > reverse){
+
             reverse = reverse * 10 + x%10;
-            x /= 10;
+            x = x/10;
         }
 
         return x == reverse || x == reverse/10;
+
+
         
     }
 }
