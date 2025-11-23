fc7d33f3af1b7e44ad3cda73244b4f7e80097ec6
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-22 19:25:33 -0500

    Time: 2 ms (97.77%) | Memory: 43.3 MB (19.94%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,14 +5,16 @@ class Solution {
 
         for(char c : s.toCharArray()){
 
-            if(c == '[' || c == '{' || c == '(') st.add(c);
+            if(c == '[' || c == '(' || c == '{') st.push(c);
             else if(st.isEmpty()) return false;
             else if(
+                c == ']' && st.peek() == '[' || 
                 c == ')' && st.peek() == '(' || 
-                c == '}' && st.peek() == '{' || 
-                c == ']' && st.peek() == '['
+                c == '}' && st.peek() == '{'
             ) st.pop();
             else return false;
+
+
         }
 
         return st.isEmpty();
