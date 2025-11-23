e6a7cb117580f2cba70c188193da10c38cdcd7a0
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-23 16:23:25 -0500

    Time: 7 ms (51.81%) | Memory: 47.1 MB (17.75%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,10 +1,11 @@
 class Solution {
 
     HashMap<Integer, List<Integer>> map = new HashMap<>();
-    Set<Integer> visit = new HashSet<>();
+    HashSet<Integer> visit = new HashSet<>();
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
+        //populate map
         for(int i = 0; i < numCourses; i++){
             map.put(i, new ArrayList<>());
         }
@@ -13,12 +14,12 @@ class Solution {
             map.get(pre[0]).add(pre[1]);
         }
 
+        //check for cycle at each numCourses
         for(int i = 0; i < numCourses; i++){
             if(!dfs(i)) return false;
         }
 
         return true;
-
     }
 
     private boolean dfs(int course){
