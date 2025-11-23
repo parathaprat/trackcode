36708271ae36f06542aa499849ee9fbacff084a2
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-23 00:49:53 -0500

    Time: 0 ms (100.00%) | Memory: 44.9 MB (5.85%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -6,13 +6,20 @@ class Solution {
 
         while(left <= right){
 
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left)/2;
 
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
         }
 
         return left;
+        
     }
 }
