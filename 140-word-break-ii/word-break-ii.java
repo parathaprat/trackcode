91eea5aa3c85ba4d3e50325e52c73bd456ba037e
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-24 00:30:54 -0500

    Time: 1 ms (95.98%) | Memory: 42.6 MB (35.61%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -2,43 +2,39 @@ class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
 
         List<String> res = new ArrayList<>();
+        Set<String> set = new HashSet<>(wordDict);
         List<String> cur = new ArrayList<>();
-        HashSet<String> set = new HashSet<>(wordDict);
 
-        dfs(0, s, res, cur, set);
+        dfs(0, s, res, set, cur);
 
         return res;
-        
     }
 
-    private void dfs(int ind, String s, List<String> res, List<String> cur, HashSet<String> set){
+    public void dfs(int ind, String s, List<String> res, Set<String> set, List<String> cur){
 
         if(ind >= s.length()){
 
-            if(cur.size() > 0){
+            StringBuilder sb = new StringBuilder();
 
-                StringBuilder sb = new StringBuilder();
-
-                for(int i = 0; i < cur.size(); i++){
-
-                    if(i > 0) sb.append(" ");
-                    sb.append(cur.get(i));
-
-                }
-
-                res.add(sb.toString());
+            for(int i = 0; i < cur.size(); i++){
+                if(i > 0) sb.append(" ");
+                sb.append(cur.get(i));
             }
+
+            res.add(sb.toString());
         }
 
         for(int i = ind; i < s.length(); i++){
 
             if(set.contains(s.substring(ind, i + 1))){
                 cur.add(s.substring(ind, i + 1));
-                dfs(i + 1, s, res, cur, set);
+                dfs(i + 1, s, res, set, cur);
                 cur.remove(cur.size() - 1);
             }
         }
 
         return;
+
+
     }
 }
