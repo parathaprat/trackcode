c8ef68178191c94faa81f6ee4be4a1ea120aa65c
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-24 01:16:16 -0500

    Time: 199 ms (23.13%) | Memory: 43 MB (28.56%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -8,33 +8,31 @@ class Solution {
 
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
-                if(dfs(i, j, board, word, visit, 0)) return true;
+                
+                if(dfs(i, j, 0, visit, word, board)) return true;
+
             }
         }
 
         return false;
     }
 
-    private boolean dfs(int i, int j, char[][] board, String word, boolean[][] visit, int ind){
+    private boolean dfs(int i, int j, int ind, boolean[][] visit, String word, char[][] board){
 
         if(ind >= word.length()) return true;
 
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || visit[i][j] || board[i][j] != word.charAt(ind)) return false;
-
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(ind) || visit[i][j]) return false;
 
         visit[i][j] = true;
 
         for(int[] dir : directions){
-
             int nr = i + dir[0];
             int nc = j + dir[1];
 
-            if(dfs(nr, nc, board, word, visit, ind + 1)) return true;
+            if(dfs(nr, nc, ind + 1, visit, word, board)) return true;
         }
 
         visit[i][j] = false;
-
         return false;
-
     }
 }
