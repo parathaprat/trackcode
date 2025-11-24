0aa2b85ef880edcf50942de9014c9997844d58e2
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-24 16:31:09 -0500

    Time: 53 ms (20.64%) | Memory: 129.6 MB (40.75%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,8 +1,5 @@
 class LRUCache {
 
-    //DLL = key, val, next, prev
-    //map = key, node
-
     class Node{
 
         int key;
@@ -13,22 +10,23 @@ class LRUCache {
         Node(int key, int val){
             this.key = key;
             this.val = val;
-            next = null;
-            prev = null;
         }
     }
 
-    Map<Integer, Node> map = new HashMap<>();
-    
     Node head = new Node(0, 0);
     Node tail = new Node(0, 0);
 
+    Map<Integer, Node> map;
     int capacity;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        map = new HashMap<>();
+
         head.next = tail;
         tail.prev = head;
+
+        
     }
     
     public int get(int key) {
@@ -36,10 +34,11 @@ class LRUCache {
         if(!map.containsKey(key)) return -1;
 
         Node node = map.get(key);
+
         remove(node);
         insert(node);
 
-        return node.val; 
+        return node.val;
         
     }
     
@@ -49,31 +48,30 @@ class LRUCache {
             Node node = map.get(key);
             remove(node);
         }
-        if(map.size() == capacity){
-            remove(tail.prev);
-        }
+        if(map.size() == capacity) remove(tail.prev);
 
         insert(new Node(key, value));
         
     }
 
-    private void insert(Node node){
+    public void insert(Node node){
+
+        head.next.prev = node;
+        node.next = head.next;
+
+        head.next = node;
+        node.prev = head;
 
         map.put(node.key, node);
-
-        node.next = head.next;
-        head.next.prev = node;
-
-        node.prev = head;
-        head.next = node;
     }
 
-    private void remove(Node node){
+    public void remove(Node node){
 
         node.prev.next = node.next;
         node.next.prev = node.prev;
 
         map.remove(node.key);
+
     }
 }
 
