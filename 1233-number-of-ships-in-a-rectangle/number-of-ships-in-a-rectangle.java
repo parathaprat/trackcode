30be423e2ce14be96ae741aee8be5cf423b5e095
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-24 16:20:47 -0500

    Time: 0 ms (100.00%) | Memory: 43.2 MB (9.41%) - LeetSync

--- 1233-number-of-ships-in-a-rectangle/number-of-ships-in-a-rectangle.java
+++ 1233-number-of-ships-in-a-rectangle/number-of-ships-in-a-rectangle.java
@@ -8,38 +8,41 @@
 
 class Solution {
     public int countShips(Sea sea, int[] topRight, int[] bottomLeft) {
-
+        
         int res = 0;
 
-        if(bottomLeft[0] <= topRight[0] && bottomLeft[1] <= topRight[1] && sea.hasShips(topRight, bottomLeft)){
+        if(topRight[0] >= bottomLeft[0] && topRight[1] >= bottomLeft[1] && sea.hasShips(topRight, bottomLeft)){
 
-            if(bottomLeft[0] == topRight[0] && bottomLeft[1] == topRight[1]) return 1;
+            if(topRight[0] == bottomLeft[0] && topRight[1] == bottomLeft[1]) return 1;
 
-            int newX = (bottomLeft[0] + topRight[0])/2;
-            int newY = (bottomLeft[1] + topRight[1])/2;
+            int newX = (topRight[0] + bottomLeft[0]) / 2;
+            int newY = (topRight[1] + bottomLeft[1]) / 2;
 
             //topLeft
             int[] topLeftTR = {newX, topRight[1]};
             int[] topLeftBL = {bottomLeft[0], newY + 1};
+
             res += countShips(sea, topLeftTR, topLeftBL);
 
             //topRight
             int[] topRightTR = topRight;
             int[] topRightBL = {newX + 1, newY + 1};
+
             res += countShips(sea, topRightTR, topRightBL);
 
             //bottomRight
             int[] bottomRightTR = {topRight[0], newY};
             int[] bottomRightBL = {newX + 1, bottomLeft[1]};
+
             res += countShips(sea, bottomRightTR, bottomRightBL);
 
             //bottomLeft
             int[] bottomLeftTR = {newX, newY};
             int[] bottomLeftBL = bottomLeft;
-            res+= countShips(sea, bottomLeftTR, bottomLeftBL);
+
+            res += countShips(sea, bottomLeftTR, bottomLeftBL);
         }
 
         return res;
-        
     }
 }
