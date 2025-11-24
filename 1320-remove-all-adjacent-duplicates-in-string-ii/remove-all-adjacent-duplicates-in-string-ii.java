5fe701d85288a909b57df690dcf48ee1f95fed55
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-24 17:05:45 -0500

    Time: 26 ms (69.84%) | Memory: 47.2 MB (25.55%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -9,29 +9,26 @@ class Solution {
             this.c = c;
             this.freq = freq;
         }
-
     }
-
-    Stack<tuple> st;
 
     public String removeDuplicates(String s, int k) {
 
-        st = new Stack<>();
+        Stack<tuple> st = new Stack<>();
 
         for(char c : s.toCharArray()){
 
-            if(!st.isEmpty() && st.peek().c == c) st.peek().freq++;
-            else{
-                st.add(new tuple(c, 1));
-            }
+            if(!st.isEmpty() && st.peek().c == c){
+                st.peek().freq++;
 
-            if(st.peek().freq == k) st.pop();
+                if(st.peek().freq == k) st.pop();
+            }
+            else st.push(new tuple(c, 1));
         }
 
         StringBuilder sb = new StringBuilder();
 
         for(tuple tp : st){
-
+            
             int f = tp.freq;
 
             while(f > 0){
