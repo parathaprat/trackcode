a542d5c955414611fd3af38bc2c808c32a3b0a8c
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-23 20:00:19 -0500

    Time: 9 ms (89.59%) | Memory: 47.2 MB (17.41%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.java
+++ 1272-invalid-transactions/invalid-transactions.java
@@ -1,9 +1,5 @@
 class Solution {
 
-    //create a transaction class for easy access
-    //1st pass transactions to populate map (name -> list transaction)
-    //2nd pass to check validity, adding to ans list
-
     class Transaction{
 
         String name;
@@ -13,19 +9,22 @@ class Solution {
 
         Transaction(String line){
 
-            String[] split = line.split(",");
-            name = split[0];
-            time = Integer.parseInt(split[1]);
-            amount = Integer.parseInt(split[2]);
-            city = split[3];
+            String[] details = line.split(",");
+
+            this.name = details[0];
+            this.time = Integer.parseInt(details[1]);
+            this.amount = Integer.parseInt(details[2]);
+            this.city = details[3];
 
         }
     }
 
+    //map(name -> transactions)
+
     public List<String> invalidTransactions(String[] transactions) {
 
-        List<String> res = new ArrayList<>();
-        HashMap<String, List<Transaction>> map = new HashMap<>();
+        List<String> invalid = new ArrayList<>();
+        Map<String, List<Transaction>> map = new HashMap<>();
 
         for(String transaction : transactions){
 
@@ -40,20 +39,19 @@ class Solution {
 
             Transaction t = new Transaction(transaction);
 
-            if(!isValid(t, map)) res.add(transaction);
+            if(!isValid(t, map)) invalid.add(transaction);
         }
 
-        return res;
+        return invalid;
     }
 
     private boolean isValid(Transaction t, Map<String, List<Transaction>> map){
 
         if(t.amount > 1000) return false;
 
-        for(Transaction ta : map.get(t.name)){
-            
-            if(Math.abs(ta.time - t.time) <= 60 && !ta.city.equals(t.city)) return false;
+        for(Transaction tp : map.get(t.name)){
 
+            if(Math.abs(tp.time - t.time) <= 60 && !t.city.equals(tp.city)) return false;
         }
 
         return true;
