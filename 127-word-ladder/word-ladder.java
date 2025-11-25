29cce79a4cd45525766438663df5925a9c031741
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-25 17:33:58 -0500

    Time: 205 ms (17.68%) | Memory: 48.2 MB (34.57%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,40 +1,41 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
-        if(beginWord.equals(endWord) || !wordList.contains(endWord)) return 0;
-        
-        Set<String> dict = new HashSet<>(wordList);
+        //BFS -> start word to end word and return steps
         Queue<String> q = new LinkedList<>();
+        Set<String> set = new HashSet<>(wordList);
+
+        if(beginWord.equals(endWord) || !wordList.contains(endWord)) return 0;
 
         q.add(beginWord);
 
         int steps = 1;
 
         while(!q.isEmpty()){
-            
+
             int level = q.size();
 
             for(int i = 0; i < level; i++){
+                
+                String word = q.poll();
+                if(word.equals(endWord)) return steps;
 
-                String cur = q.poll();
-                if(cur.equals(endWord)) return steps;
-
-                for(int j = 0; j < cur.length(); j++){
+                for(int j = 0; j < word.length(); j++){
                     for(char c = 'a'; c <= 'z'; c++){
-                        
-                        if(c == cur.charAt(j)) continue;
 
-                        String nei = cur.substring(0, j) + c + cur.substring(j + 1);
-                        
-                        if(dict.contains(nei)){
+                        if(c == word.charAt(j)) continue;
+
+                        String nei = word.substring(0, j) + c + word.substring(j + 1); 
+
+                        if(set.contains(nei)){
                             q.offer(nei);
-                            dict.remove(nei);
-
-                        } 
+                            set.remove(nei);
+                        }
                     }
                 }
+                
             }
-
+            
             steps++;
         }
 
