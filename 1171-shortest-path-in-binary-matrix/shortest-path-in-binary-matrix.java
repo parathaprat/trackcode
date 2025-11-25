ac7ed5b2579736c9f675e3c713b01f151181f167
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-25 02:56:39 -0500

    Time: 18 ms (36.35%) | Memory: 47.5 MB (28.48%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,46 +1,49 @@
 class Solution {
 
-    int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
+    int[][] directions = {{0, 1}, {1, 0}, {0, - 1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 
     public int shortestPathBinaryMatrix(int[][] grid) {
 
-        if(grid[0][0] == 1 || grid[grid.length - 1][grid.length - 1] == 1) return -1;
-        
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        if(grid[0][0] != 0 || grid[grid.length - 1][grid[0].length -1] != 0) return -1;
+
         Queue<int[]> q = new LinkedList<>();
+        boolean[][] visit = new boolean[grid.length][grid[0].length];
 
         q.add(new int[]{0, 0});
-        visited[0][0] = true;
+        visit[0][0] = true;
 
-        int steps = 1;
-
+        int path = 1;
+        
         while(!q.isEmpty()){
 
-            int size = q.size();
+            int level = q.size();
 
-            for(int i = 0; i < size; i++){
+            for(int i = 0; i < level; i++){
 
-                int[] cur = q.poll();
+                int[] rc = q.poll();
+                int row = rc[0];
+                int col = rc[1];
 
-                if(cur[0] == grid.length - 1 && cur[1] == grid.length - 1) return steps;
-                
+                if(row == grid.length - 1 && col == grid[0].length - 1) return path;
+
                 for(int[] dir : directions){
-                    int nr = cur[0] + dir[0];
-                    int nc = cur[1] + dir[1];
-
-                    if(nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length || visited[nr][nc] || grid[nr][nc] == 1) continue;
-
-                    visited[nr][nc] = true;
-                    q.add(new int[]{nr, nc});
                     
+                    int nr = row + dir[0];
+                    int nc = col + dir[1];
+
+                    if(nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length || visit[nr][nc] || grid[nr][nc] != 0) continue;
+
+                    q.add(new int[]{nr, nc});
+                    visit[nr][nc] = true;
+
+
                 }
             }
 
-            steps++;
+            path++;
         }
 
         return -1;
-
-
+        
     }
 }
