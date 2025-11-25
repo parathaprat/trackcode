424114d18bfc7ba4a086e0269cd3f313d64b74f9
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-24 23:05:04 -0500

    Time: 2 ms (87.65%) | Memory: 49.4 MB (7.44%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -13,31 +13,27 @@ class Solution {
 
         if(head == null || head.next == null) return;
 
-        //slow and fast pointers
+        //find middle of list
         //reverse 2nd half
-        //merge both lists
+        //merge both
 
-        ListNode l1 = head;
-        ListNode prev = head;
-
-        ListNode slow = head;
-        ListNode fast = head;
+        ListNode l1 = head; //head of 1st half
+        ListNode prev = head; //tail of 1st half
+        ListNode slow = head; //head of 2nd half
+        ListNode fast = head; //tail of 2nd half
 
         while(fast != null && fast.next != null){
-
             prev = slow;
             slow = slow.next;
             fast = fast.next.next;
-
         }
 
         prev.next = null;
 
         ListNode l2 = reverse(slow);
-
         merge(l1, l2);
 
-        
+        return;
     }
 
     private ListNode reverse(ListNode head){
@@ -45,13 +41,11 @@ class Solution {
         ListNode prev = null;
 
         while(head != null){
-
             ListNode next = head.next;
 
             head.next = prev;
             prev = head;
             head = next;
-
         }
 
         return prev;
@@ -59,19 +53,18 @@ class Solution {
 
     private void merge(ListNode l1, ListNode l2){
 
-        while(l1 != null){
+        while(l1 != null && l2 != null){
 
             ListNode l1next = l1.next;
             ListNode l2next = l2.next;
 
             l1.next = l2;
-            if(l1next == null) break;
-            l2.next = l1next;
+            if(l1next != null) l2.next = l1next;
 
             l1 = l1next;
             l2 = l2next;
-
         }
-
     }
+
+    
 }
