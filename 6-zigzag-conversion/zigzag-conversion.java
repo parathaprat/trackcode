2b4c34616573eedebc07226de1a508351e9be930
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-25 18:10:00 -0500

    Time: 3 ms (91.59%) | Memory: 46.2 MB (36.17%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -3,22 +3,23 @@ class Solution {
 
         if(numRows == 1) return s;
 
-        StringBuilder sb = new StringBuilder();
         int cycle = 2 * (numRows - 1);
 
-        for(int row = 0; row < numRows; row++){
-            for(int j = row; j < s.length(); j+= cycle){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < numRows; i++){
+            for(int j = i; j < s.length(); j += cycle){
 
                 sb.append(s.charAt(j));
 
-                boolean isMiddle = row > 0 && row < numRows - 1;
-                int diagonalIndex = j + (cycle - 2 * row);
+                boolean isMiddle = i > 0 && i < numRows - 1;
+                int diagonalIndex = j + (cycle - 2 * i);
 
                 if(isMiddle && diagonalIndex < s.length()) sb.append(s.charAt(diagonalIndex));
+
             }
         }
-
-        return sb.toString();
         
+        return sb.toString();
     }
 }
