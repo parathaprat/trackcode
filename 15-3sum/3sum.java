8440d7fd1d4c784ae706159c2db2ab748d9c5c67
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-25 19:37:11 -0500

    Time: 34 ms (38.29%) | Memory: 59.1 MB (27.02%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -8,14 +8,15 @@ class Solution {
 
             if(i > 0 && nums[i] == nums[i - 1]) continue;
 
+            int target = -nums[i];
             int left = i + 1;
             int right = nums.length - 1;
-            int target = -nums[i];
 
             while(left < right){
-
+            
                 if(nums[left] + nums[right] == target){
-                    res.add(Arrays.asList(nums[left], nums[right], nums[i]));
+
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
                     while(left < right && nums[left] == nums[left + 1]) left++;
                     while(left < right && nums[right] == nums[right - 1]) right--;
@@ -23,14 +24,13 @@ class Solution {
                     left++;
                     right--;
                 }
-                else if(nums[left] + nums[right] < target){
-                    left++;
-                }
+                else if(nums[left] + nums[right] < target) left++;
                 else{
                     right--;
                 }
-            } 
+            }
         }
+        
 
         return res;
         
