2bba6f7fb79dc6d1924cb60e957754325a218d7c
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-25 21:14:06 -0500

    Time: 19 ms (46.01%) | Memory: 43.6 MB (54.75%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,42 +1,33 @@
 class Solution {
 
-    int left = 0;
-    int right = 0;
-    int ans = 0;
+    int max = 0;
+    int leftp = 0;
+    int rightp = 0;
 
     public String longestPalindrome(String s) {
-
-        //at each index, assume odd len and even len palindromes
-        //explore all pals and keep track of the longest length
-
+        
         for(int i = 0; i < s.length() - 1; i++){
+
             checkPal(i, i, s);
             checkPal(i, i + 1, s);
-        }  
-
-        return s.substring(left, right + 1); 
-
-        
-        
-    }
-
-    private void checkPal(int leftp, int rightp, String s){
-
-        if(leftp < 0 || rightp >= s.length() || s.charAt(leftp) != s.charAt(rightp)) return;
-
-        //if longer pal found, update index
-        if(ans < rightp - leftp + 1){
-
-            ans = rightp - leftp + 1;
-
-            left = leftp;
-            right = rightp;
-
         }
 
-        leftp--;
-        rightp++;
+        return s.substring(leftp, rightp + 1);
+    }
 
-        checkPal(leftp, rightp, s);
+    private void checkPal(int left, int right, String s){
+
+        if(left < 0 || right >= s.length() || s.charAt(left) != s.charAt(right)) return;
+
+        if(max < right - left + 1){
+            max = right - left + 1;
+            leftp = left;
+            rightp = right;
+        }
+
+        left--;
+        right++;
+        
+        checkPal(left, right, s);
     }
 }
