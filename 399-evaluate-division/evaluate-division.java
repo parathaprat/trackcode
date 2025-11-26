a61ce9a2718aba43a3685968aef2b804f44e5941
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-26 01:00:30 -0500

    Time: 1 ms (98.98%) | Memory: 47.2 MB (5.00%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,10 +1,11 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
 
-        //dividend -> divisor, values
-        //divisor -> dividend, 1/va;
+        //dividend -> divisor, value
+        //divisor -> dividend, 1/value
 
         HashMap<String, HashMap<String, Double>> gr = buildGraph(equations, values);
+
         double[] finalAns = new double[queries.size()];
 
         for(int i = 0; i < queries.size(); i++){
@@ -14,38 +15,34 @@ class Solution {
 
             if(!gr.containsKey(dividend) || !gr.containsKey(divisor)) finalAns[i] = -1.0;
             else{
+                
+               double[] ans = {-1.0};
+               double temp = 1.0;
+               HashSet<String> visit = new HashSet<>();
 
-                Set<String> visit = new HashSet<>();
-                double[] ans = {-1.0};
-                double temp = 1.0;
-
-                dfs(dividend, divisor, visit, ans, temp, gr);
-                finalAns[i] = ans[0];
+               dfs(dividend, divisor, ans, temp, visit, gr);
+               finalAns[i] = ans[0];
             }
         }
 
         return finalAns;
     }
 
-    private void dfs(String node, String dest, Set<String> visit, double[] ans, double temp, HashMap<String, HashMap<String, Double>> gr){
+    private void dfs(String node, String dest, double[] ans, double temp, HashSet<String> visit, HashMap<String, HashMap<String, Double>> gr){
 
         if(visit.contains(node)) return;
-        if(node.equals(dest)) ans[0] = temp;
 
+        if(node.equals(dest)) ans[0] = temp;
         visit.add(node);
 
-        for(Map.Entry<String, Double> entry : gr.get(node).entrySet()){
+        for(Map.Entry<String, Double> e : gr.get(node).entrySet()){
+            
+            String nei = e.getKey();
+            Double val = e.getValue();
 
-            String nei = entry.getKey();
-            Double val = entry.getValue();
-
-            dfs(nei, dest, visit, ans, temp * val, gr);
-
+            dfs(nei, dest, ans, temp * val, visit, gr);
         }
-
     }
-
-    
 
     private HashMap<String, HashMap<String, Double>> buildGraph(List<List<String>> equations, double[] values){
 
@@ -60,8 +57,7 @@ class Solution {
             gr.putIfAbsent(divisor, new HashMap<>());
 
             gr.get(dividend).put(divisor, values[i]);
-            gr.get(divisor).put(dividend, 1.0 / values[i]);
-
+            gr.get(divisor).put(dividend, 1 / values[i]);
         }
 
         return gr;
