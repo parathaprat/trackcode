f67ab2f6bf6c77a466676ccb37abce2cba1ba08d
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-25 19:52:27 -0500

    Time: 86 ms (31.60%) | Memory: 68.2 MB (23.11%) - LeetSync

--- 308-range-sum-query-2d-mutable/range-sum-query-2d-mutable.java
+++ 308-range-sum-query-2d-mutable/range-sum-query-2d-mutable.java
@@ -1,58 +1,56 @@
 class NumMatrix {
 
     int[][] matrix;
-    int[][] original;
+    int[][] prefixSum;
 
     public NumMatrix(int[][] matrix) {
 
-        original = new int[matrix.length][matrix[0].length];
+        this.matrix = matrix;
+        prefixSum = new int[matrix.length][matrix[0].length];
 
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
-                original[i][j] = matrix[i][j];
-            }
-        }
-
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 1; j < matrix[0].length; j++){
-                matrix[i][j] += matrix[i][j - 1];
+                prefixSum[i][j] = matrix[i][j];
             }
         }
 
         for(int i = 1; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
-                matrix[i][j] += matrix[i - 1][j];
+                prefixSum[i][j] += prefixSum[i - 1][j];
             }
         }
 
-        this.matrix = matrix;
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 1; j < matrix[0].length; j++){
+                prefixSum[i][j] += prefixSum[i][j - 1];
+            }
+        }
         
     }
     
     public void update(int row, int col, int val) {
 
-        int delta = val - original[row][col];
-        original[row][col] = val;
+        int delta = val - matrix[row][col];
+        matrix[row][col] = val;
 
         for(int i = row; i < matrix.length; i++){
             for(int j = col; j < matrix[0].length; j++){
-                matrix[i][j] += delta;
+                prefixSum[i][j] += delta;
             }
         }
         
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        
-        //total - extra
 
-        int total = matrix[row2][col2];
+        int total = prefixSum[row2][col2];
 
-        int extra = (row1 != 0 ? matrix[row1 - 1][col2] : 0) + 
-                    (col1 != 0 ? matrix[row2][col1 - 1] : 0) -
-                    (row1 != 0 && col1 != 0 ? matrix[row1 - 1][col1 - 1] : 0);
+        int extra = (row1 > 0 ? prefixSum[row1 - 1][col2] : 0) +
+                    (col1 > 0 ? prefixSum[row2][col1 - 1] : 0) -
+                    (row1 > 0 && col1 > 0 ? prefixSum[row1 - 1][col1 - 1] : 0);
 
         return total - extra;
+        
     }
 }
 
