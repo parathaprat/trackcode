d8bcc119468c83958891ab063fc81f7e0d88c443
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-25 21:44:09 -0500

    Time: 0 ms (100.00%) | Memory: 43.4 MB (20.61%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,25 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-        //element - freq
-        HashMap<Integer, Integer> freq = new HashMap<>();
+        int[] count = new int[3];
 
         for(int num : nums){
-
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            count[num]++;
         }
 
-        int pt = 0;
+        int p = 0;
 
-        for(Map.Entry<Integer, Integer> e : freq.entrySet()){
+        for(int i = 0; i < count.length; i++){
 
-            int iterations = e.getValue();
+            int k = count[i];
 
-            while(iterations > 0){
-                nums[pt] = e.getKey();
-                pt++;
-                iterations--;
+            while(k > 0){
+                nums[p] = i;
+                k--;
+                p++;
             }
-        } 
+        }
     }
 }
