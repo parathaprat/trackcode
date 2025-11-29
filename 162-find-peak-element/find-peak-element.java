cf3b701fd76500fea1189e7087ed7ba120440966
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-29 18:40:52 -0500

    Time: 0 ms (100.00%) | Memory: 44.2 MB (20.11%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -8,11 +8,11 @@ class Solution {
 
             int mid = left + (right - left)/2;
 
-            if(nums[mid] < nums[mid + 1]){
-                left = mid + 1;
+            if(nums[mid] > nums[mid + 1]){
+                right = mid;
             }
             else{
-                right = mid;
+                left = mid + 1;
             }
         }
 
