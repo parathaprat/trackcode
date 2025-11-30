7b311b540b7d4a2e994f39162f3a8d29bfae93af
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-30 14:42:13 -0500

    Time: 199 ms (18.77%) | Memory: 48.5 MB (34.30%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,41 +1,38 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
-        //BFS -> start word to end word and return steps
-        Queue<String> q = new LinkedList<>();
         Set<String> set = new HashSet<>(wordList);
 
-        if(beginWord.equals(endWord) || !wordList.contains(endWord)) return 0;
+        if(!set.contains(endWord) || beginWord.equals(endWord)) return 0;
 
+        Queue<String> q = new LinkedList<>();
         q.add(beginWord);
 
         int steps = 1;
-
         while(!q.isEmpty()){
 
             int level = q.size();
 
             for(int i = 0; i < level; i++){
-                
+
                 String word = q.poll();
                 if(word.equals(endWord)) return steps;
 
                 for(int j = 0; j < word.length(); j++){
+
                     for(char c = 'a'; c <= 'z'; c++){
 
                         if(c == word.charAt(j)) continue;
-
-                        String nei = word.substring(0, j) + c + word.substring(j + 1); 
+                        String nei = word.substring(0, j) + c + word.substring(j + 1);
 
                         if(set.contains(nei)){
-                            q.offer(nei);
+                            q.add(nei);
                             set.remove(nei);
                         }
                     }
                 }
-                
             }
-            
+
             steps++;
         }
 
