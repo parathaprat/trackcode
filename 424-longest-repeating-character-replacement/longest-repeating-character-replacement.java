32fc3f15581514b7cff8dad356a87ce99c57cb73
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-29 22:08:36 -0500

    Time: 12 ms (48.36%) | Memory: 46.1 MB (35.55%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,34 +1,30 @@
 class Solution {
     public int characterReplacement(String s, int k) {
 
-        //left and right pointer
-        //loop with right, leep track of maxFreq
-        //whenever window - maxFreq > k, left++
-        //size of window gives ans
+        //2 pointer, iterate w left
+        //windowsize - maxFreq > k, left++
+        //max windowsize is ans
 
         int left = 0;
         int ans = 0;
-        int maxFreq = 0;
 
         int[] freq = new int[26];
+        int maxFreq = 0;
 
         for(int right = 0; right < s.length(); right++){
 
             freq[s.charAt(right) - 'A']++;
             maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
 
-            int windowSize = right - left + 1;
-
-            if(windowSize - maxFreq > k){
+            if(right - left + 1 - maxFreq > k){
                 freq[s.charAt(left) - 'A']--;
                 left++;
             }
 
-            ans = Math.max(ans, right - left + 1);
+            ans = Math.max(right - left + 1, ans);
+            
         }
 
         return ans;
-
-        
     }
 }
