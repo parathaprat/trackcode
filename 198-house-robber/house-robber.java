2179e4f6807d70e44fb2cfdecffd41573c72d750
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-01 12:28:14 -0500

    Time: 0 ms (100.00%) | Memory: 42.7 MB (23.34%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -5,15 +5,19 @@ class Solution {
         if(nums.length == 1) return nums[0];
         if(nums.length == 2) return Math.max(nums[0], nums[1]);
 
-        int[] dp = new int[nums.length];
+        int[] dp = new int[nums.length + 1]; //dp[i] = max amount that can be robbed till ith house
+
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
 
         for(int i = 2; i < nums.length; i++){
+
             dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+
         }
 
         return dp[nums.length - 1];
+
         
     }
 }
