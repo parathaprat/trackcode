f00b9f60681c286a875d419301b180fac3445ca8
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-01 17:37:13 -0500

    Time: 1 ms (84.80%) | Memory: 43.3 MB (16.70%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -2,9 +2,9 @@ class Solution {
     public int numDecodings(String s) {
 
         int n = s.length();
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n + 1]; //dp[i] = no of ways to decode subtring from i
 
-        dp[n] = 1;
+        dp[n] = 1; //only 1 way to decode nothing
         dp[n - 1] = s.charAt(n - 1) == '0' ? 0 : 1;
 
         for(int i = n - 2; i >= 0; i--){
@@ -16,13 +16,13 @@ class Solution {
 
             dp[i] = dp[i + 1];
 
-            int check = Integer.parseInt(s.substring(i, i + 2));
-            if(check >= 10 && check <= 26){
+            int checkValid = Integer.parseInt(s.substring(i, i + 2));
+
+            if(checkValid >= 10 && checkValid <= 26){
                 dp[i] += dp[i + 2];
             }
         }
-
-        return dp[0];
         
+        return dp[0];   
     }
 }
