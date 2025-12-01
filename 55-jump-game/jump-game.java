56384e31746ebdb76dcffd8ddc4b6e6d2fb2011b
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-01 02:25:33 -0500

    Time: 2 ms (84.04%) | Memory: 48.1 MB (7.31%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
 
+        //toReach tracks the node from which teh end can be reached.
+        //toReach is at the end, iterate from 2nd last and check if it can reach toReach
+        //if it can, toReach = i
+        //if itoReach == 0, true 
+
         int toReach = nums.length - 1;
 
         for(int i = nums.length - 2; i >= 0; i--){
-
             if(nums[i] + i >= toReach){
                 toReach = i;
             }
         }
-
-        return toReach == 0;
         
+        return toReach == 0;
     }
-} 
+}
