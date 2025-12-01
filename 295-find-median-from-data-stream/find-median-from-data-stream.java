288a44d61554ddad09680fab9abd41328d6593ea
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-30 22:28:56 -0500

    Time: 134 ms (53.05%) | Memory: 111.4 MB (34.57%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,41 +1,36 @@
 class MedianFinder {
 
-    //1st half: maxHeap
-    //2nd half: minHeap
-
     PriorityQueue<Integer> maxHeap;
     PriorityQueue<Integer> minHeap;
 
     public MedianFinder() {
-
         maxHeap = new PriorityQueue<>((a, b) -> b - a);
         minHeap = new PriorityQueue<>((a, b) -> a - b);
-        
     }
     
     public void addNum(int num) {
 
-        maxHeap.offer(num);
+        maxHeap.add(num);
 
-        while(maxHeap.size() - minHeap.size() > 1 || !minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()){
-            minHeap.offer(maxHeap.poll());
+        if(maxHeap.size() - minHeap.size() > 1 || !minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()){
+            minHeap.add(maxHeap.poll());
         }
 
-        while(minHeap.size() - maxHeap.size() > 1){
-            maxHeap.offer(minHeap.poll());
+        if(minHeap.size() - maxHeap.size() > 1){
+            maxHeap.add(minHeap.poll());
         }
+        
     }
     
     public double findMedian() {
 
         double ans = 0;
 
-        if(maxHeap.size() == minHeap.size()) ans = (double)(maxHeap.peek() + minHeap.peek())/2;
-        else if(maxHeap.size() > minHeap.size()) ans = (double)maxHeap.peek();
-        else ans = minHeap.peek();
+        if(maxHeap.size() == minHeap.size()) ans = (double)(maxHeap.peek() + minHeap.peek()) / 2;
+        else if(minHeap.size() > maxHeap.size()) ans = minHeap.peek();
+        else ans = maxHeap.peek();
 
-        return (double)ans;
-        
+        return ans;
     }
 }
 
