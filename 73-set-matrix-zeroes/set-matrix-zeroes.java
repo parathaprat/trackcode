6b6a1244576bc66e1e36dea3dbd6eebe256278e7
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-01 13:13:38 -0500

    Time: 0 ms (100.00%) | Memory: 47.2 MB (34.68%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -3,12 +3,11 @@ class Solution {
 
         boolean zeroInFirstCol = false;
 
+        //populate 0s in place, extra space to handle overlap
         for(int row = 0; row < matrix.length; row++){
-            
             if(matrix[row][0] == 0) zeroInFirstCol = true;
 
             for(int col = 1; col < matrix[0].length; col++){
-
                 if(matrix[row][col] == 0){
                     matrix[row][0] = 0;
                     matrix[0][col] = 0;
@@ -16,18 +15,17 @@ class Solution {
             }
         }
 
-        //iterate from the back to avoid overwritting markers
+        //iterate from the back to avoid overwritting inplace 0s
         for(int row = matrix.length - 1; row >= 0; row--){
-            for(int col = matrix[0].length - 1; col >= 1; col--){
-                
+            for(int col = matrix[0].length - 1; col > 0; col--){
+
                 if(matrix[row][0] == 0 || matrix[0][col] == 0){
                     matrix[row][col] = 0;
                 }
             }
 
-            if(zeroInFirstCol){
-                matrix[row][0] = 0;
-            }
+            if(zeroInFirstCol) matrix[row][0] = 0;
         }
+        
     }
 }
