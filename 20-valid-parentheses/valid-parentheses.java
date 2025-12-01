62422dfa5b19fc9bcd23f5b23ce94781795c65f4
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-11-30 19:06:04 -0500

    Time: 3 ms (72.53%) | Memory: 43.2 MB (25.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,19 +5,22 @@ class Solution {
 
         for(char c : s.toCharArray()){
 
-            if(c == '[' || c == '(' || c == '{') st.push(c);
-            else if(st.isEmpty()) return false;
+            if(c == '(' || c == '[' || c == '{'){
+                st.push(c);
+            }
+            else if(st.isEmpty()){
+                return false;
+            }
             else if(
-                c == ']' && st.peek() == '[' || 
-                c == ')' && st.peek() == '(' || 
+                c == ')' && st.peek() == '(' ||
+                c == ']' && st.peek() == '[' ||
                 c == '}' && st.peek() == '{'
-            ) st.pop();
+            ){
+                st.pop();
+            }
             else return false;
-
-
         }
 
         return st.isEmpty();
-        
     }
 }
