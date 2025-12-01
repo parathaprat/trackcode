adbdb2362f68c0ac11acd2b1c5b1a8d55afae01a
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-01 18:50:00 -0500

    Time: 9 ms (40.52%) | Memory: 48.5 MB (16.08%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,20 +1,16 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
 
-        //pq with min end times to see if a room can be freed for next minMeetingRooms
-        //sort intervals by start time
-
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int[] in : intervals){
 
-            if(!pq.isEmpty() && in[0] >= pq.peek()){
+            if(!pq.isEmpty() && pq.peek() <= in[0]){
                 pq.poll();
             }
 
             pq.add(in[1]);
-
         }
 
         return pq.size();
