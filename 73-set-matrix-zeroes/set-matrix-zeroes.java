624d2e0cf28c1274e8d90fb02078b54952a6eb39
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-01 13:03:32 -0500

    Time: 1 ms (64.33%) | Memory: 47.3 MB (18.08%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        boolean zeroInFirstCol = false;
+
+        for(int row = 0; row < matrix.length; row++){
+            
+            if(matrix[row][0] == 0) zeroInFirstCol = true;
+
+            for(int col = 1; col < matrix[0].length; col++){
+
+                if(matrix[row][col] == 0){
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
+                }
+            }
+        }
+
+        //iterate from the back to avoid overwritting markers
+        for(int row = matrix.length - 1; row >= 0; row--){
+            for(int col = matrix[0].length - 1; col >= 1; col--){
+                
+                if(matrix[row][0] == 0 || matrix[0][col] == 0){
+                    matrix[row][col] = 0;
+                }
+            }
+
+            if(zeroInFirstCol){
+                matrix[row][0] = 0;
+            }
+        }
+    }
+}
