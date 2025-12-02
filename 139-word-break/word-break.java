e682ef7fa0b059bfc97143ae7754bf1c222f3492
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-02 13:41:09 -0500

    Time: 8 ms (43.20%) | Memory: 46.3 MB (13.09%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
 
+        //dp[i] = if substring till i is breakable, true
+
         Set<String> dict = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
-
-        dp[0] = true;
-
+        dp[0] = true; //nothing is breakable
+        
         for(int i = 0; i <= s.length(); i++){
+
             for(int j = 0; j < i; j++){
 
                 if(dp[j] && dict.contains(s.substring(j, i))){
