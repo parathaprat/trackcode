eb4930ce65bec2723ac0ead5b93b64b68ec67483
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-02 12:45:43 -0500

    Time: 3 ms (45.57%) | Memory: 46.4 MB (24.14%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -1,36 +1,35 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
 
+        //a valid tree with n nodes has n - 1 edges
         if(edges.length != n - 1) return false;
-        
+
         Map<Integer, List<Integer>> map = new HashMap<>();
 
         for(int i = 0; i < n; i++){
             map.put(i, new ArrayList<>());
         }
 
-        for(int[] e : edges){
-            map.get(e[0]).add(e[1]);
-            map.get(e[1]).add(e[0]);
+        for(int[] edge : edges){
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
         }
 
-        Set<Integer> visited = new HashSet<>();
-
-        if(!dfs(0, -1, map, visited)) return false;
-
-        return visited.size() == n;
+        Set<Integer> visit = new HashSet<>();
+        if(!dfs(0, -1, map, visit)) return false;
+         
+        return visit.size() == n;
     }
 
-    private boolean dfs(int node, int parent, Map<Integer, List<Integer>> map, Set<Integer> visited){
+    private boolean dfs(int node, int parent, Map<Integer, List<Integer>> map, Set<Integer> visit){
 
-        if(visited.contains(node)) return false;
+        if(visit.contains(node)) return false;
 
-        visited.add(node);
+        visit.add(node);
 
         for(int nei : map.get(node)){
             if(nei == parent) continue;
-
-            if(!dfs(nei, node, map, visited)) return false;
+            if(!dfs(nei, node, map, visit)) return false;
         }
 
         return true;
