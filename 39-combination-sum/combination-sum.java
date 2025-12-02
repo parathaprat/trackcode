d275318d2d1d3b277a290af3c15c52c0a54f891f
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-02 18:45:50 -0500

    Time: 3 ms (29.12%) | Memory: 45.7 MB (22.78%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,26 +1,31 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> set = new ArrayList<>();
 
-        backtrack(candidates, target, 0, result, set);
+        backtrack(0, res, set, candidates, target);
 
-        return result;
+        return res;
     }
 
-    private void backtrack(int[] candidates, int target, int index, List<List<Integer>> result, List<Integer> set){
+    private void backtrack(int ind, List<List<Integer>> res, List<Integer> set, int[] candidates, int target){
 
         if(target == 0){
-            result.add(new ArrayList<>(set));
+            res.add(new ArrayList<>(set));
             return;
         }
 
-        if(index == candidates.length || target < 0) return;
+        if(ind >= candidates.length || target <= 0) return;
 
-        set.add(candidates[index]);
-        backtrack(candidates, target - candidates[index], index, result, set); //include same
+        set.add(candidates[ind]);
+        backtrack(ind, res, set, candidates, target - candidates[ind]);
         set.remove(set.size() - 1);
-        backtrack(candidates, target, index + 1, result, set); //skip completly
+
+        backtrack(ind + 1, res, set, candidates, target);
+
+
     }
+
+
 }
