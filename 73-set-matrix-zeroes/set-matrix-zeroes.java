f7f79e30768f374060249eca79a389e9ac91f9d2
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-03 13:34:20 -0500

    Time: 0 ms (100.00%) | Memory: 47.4 MB (10.32%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,31 +1,35 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
 
-        boolean zeroInFirstCol = false;
+        //use 1st col and 1st row to store 0 index's for its corresponding r/c
+        //store the one overlapping index seperately
 
-        //populate 0s in place, extra space to handle overlap
-        for(int row = 0; row < matrix.length; row++){
-            if(matrix[row][0] == 0) zeroInFirstCol = true;
+        boolean firstColZero = false;
 
-            for(int col = 1; col < matrix[0].length; col++){
-                if(matrix[row][col] == 0){
-                    matrix[row][0] = 0;
-                    matrix[0][col] = 0;
+        for(int i = 0; i < matrix.length; i++){
+
+            if(matrix[i][0] == 0) firstColZero = true;
+
+            for(int j = 1; j < matrix[0].length; j++){
+
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        //iterate from the back to avoid overwritting inplace 0s
-        for(int row = matrix.length - 1; row >= 0; row--){
-            for(int col = matrix[0].length - 1; col > 0; col--){
+        //use those zeros to convert rows and cols into 0s
 
-                if(matrix[row][0] == 0 || matrix[0][col] == 0){
-                    matrix[row][col] = 0;
+        for(int i = matrix.length - 1; i >= 0; i--){
+            for(int j = matrix[0].length - 1; j > 0; j--){
+
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
                 }
             }
 
-            if(zeroInFirstCol) matrix[row][0] = 0;
+            if(firstColZero) matrix[i][0] = 0;
         }
-        
     }
 }
