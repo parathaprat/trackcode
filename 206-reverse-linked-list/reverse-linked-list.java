01da8aca87431a504f23028d3bef3a3908a8cbed
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-03 15:08:31 -0500

    Time: 0 ms (100.00%) | Memory: 44.1 MB (32.09%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -14,6 +14,7 @@ class Solution {
         ListNode prev = null;
 
         while(head != null){
+
             ListNode next = head.next;
 
             head.next = prev;
@@ -22,5 +23,6 @@ class Solution {
         }
 
         return prev;
+        
     }
 }
