1a368e2403dad8d54a0c7190adddcd367d9e56df
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-03 16:16:02 -0500

    Time: 1 ms (85.21%) | Memory: 43 MB (33.23%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,28 +1,25 @@
 class Solution {
     public int numDecodings(String s) {
 
-        int n = s.length();
+        int[] dp = new int[s.length() + 1]; //number of ways to decode from i till the end
+        dp[s.length()] = 1;
+        dp[s.length() - 1] = s.charAt(s.length() - 1) == '0' ? 0 : 1;
 
-        int[] dp = new int[n + 1]; //no of ways to decode subtring from i
-        dp[n] = 1; //one way to decode nothing
-        dp[n - 1] = s.charAt(n - 1) == '0' ? 0 : 1; //ways to decode 1 char
 
-        for(int i = n - 2; i >= 0; i--){
+        for(int i = s.length() - 2; i >= 0; i--){
 
-            if(s.charAt(i) == '0'){ //if 0, ways to decode is 0
+            if(s.charAt(i) == '0'){
                 dp[i] = 0;
                 continue;
             }
+            
+            dp[i] = dp[i + 1];
 
-            dp[i] = dp[i + 1]; //if valid, same as next index
+            int num = Integer.parseInt(s.substring(i, i + 2));
+            if(num >= 10 && num <= 26) dp[i] += dp[i + 2];
 
-            int check = Integer.parseInt(s.substring(i, i + 2));
-
-            if(check >= 10 && check <= 26){ //if 2 digits in range, add dp[i+2]
-                dp[i] += dp[i + 2];
-            }
         }
 
-        return dp[0]; //return dp for subtring from start
+        return dp[0];
     }
 }
