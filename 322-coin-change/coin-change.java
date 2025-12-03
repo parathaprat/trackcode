16e522882cf18f9d0e7d27eeb75d8e72ba00487b
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-03 01:27:16 -0500

    Time: 18 ms (40.79%) | Memory: 46.3 MB (36.60%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -8,14 +8,19 @@ class Solution {
 
         dp[0] = 0;
 
-        for(int i = 0; i <= amount; i++){
+        for(int i = 1; i <= amount; i++){
+
             for(int coin : coins){
 
                 if(coin <= i) dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
             }
         }
 
-        return dp[amount] != amount + 1 ? dp[amount] : -1;
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+
+
+
+
         
     }
 }
