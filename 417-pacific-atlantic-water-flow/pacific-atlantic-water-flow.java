9761a3fcf5954267e5f9714ecf4348373c555855
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-03 23:44:09 -0500

    Time: 4 ms (98.17%) | Memory: 47.5 MB (5.06%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -12,37 +12,39 @@ class Solution {
         boolean[][] pac = new boolean[row][col];
         boolean[][] atl = new boolean[row][col];
 
-        for(int r = 0; r < row; r++){
-            dfs(r, 0, pac, heights);
-            dfs(r, col - 1, atl, heights);
+        for(int i = 0; i < row; i++){
+            dfs(i, 0, pac, heights);
+            dfs(i, col - 1, atl, heights);
         }
-
-        for(int c = 0; c < col; c++){
-            dfs(0, c, pac, heights);
-            dfs(row - 1, c, atl, heights);
+        
+        for(int i = 0; i < col; i++){
+            dfs(0, i, pac, heights);
+            dfs(row - 1, i, atl, heights);
         }
 
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
-                if(atl[i][j] && pac[i][j]){
-                    res.add(Arrays.asList(i, j));
-                }
+                if(pac[i][j] && atl[i][j]) res.add(Arrays.asList(i, j));
             }
         }
 
         return res;
     }
 
-    private void dfs(int i, int j, boolean[][] vis, int[][] heights){
+    private void dfs(int i, int j, boolean[][] visit, int[][] heights){
 
-        vis[i][j] = true;
+        if(visit[i][j]) return;
+
+        visit[i][j] = true;
 
         for(int[] dir : directions){
             int nr = i + dir[0];
             int nc = j + dir[1];
 
-            if(nr >= 0 && nr < heights.length && nc >= 0 && nc < heights[0].length && heights[nr][nc] >= heights[i][j] && !vis[nr][nc]){
-                dfs(nr, nc, vis, heights);
+            if(nr >= 0 && nr < heights.length && nc >= 0 && nc < heights[0].length && heights[nr][nc] >= heights[i][j]){
+
+                dfs(nr, nc, visit, heights);
+
             }
         }
     }
