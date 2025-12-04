64a7f9c406d0c48be4beba53c51fbdc0b4a995b7
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-04 00:18:21 -0500

    Time: 1 ms (99.92%) | Memory: 47.6 MB (47.44%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -3,14 +3,12 @@ class Solution {
 
         int toReach = nums.length - 1;
 
-        for(int i = nums.length - 2; i >= 0; i--){
+        for(int i = nums.length - 1; i >= 0; i--){
 
-            if(nums[i] + i >= toReach){
-                toReach = i;
-            }
-
+            if(nums[i] + i >= toReach) toReach = i;
         }
 
         return toReach == 0;
+        
     }
 }
