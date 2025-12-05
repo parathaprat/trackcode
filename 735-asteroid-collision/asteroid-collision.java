12187b1036aacb985268683eec3644b883d29036
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-04 20:46:58 -0500

    Time: 6 ms (43.06%) | Memory: 46.9 MB (34.17%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,44 +1,40 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
 
-        //Stack
-        //st.peek +ve, incoming -ve
-        //-ve > +ve : st.pop, add -ve
-        //+ve > -ve : nothing, do not add -ve
-        //+ve == -ve : pop and do not add -ve
-        //use boolean to add -ve
-
         Stack<Integer> st = new Stack<>();
 
-        for(int a : asteroids){
+        for(int num : asteroids){
 
-            boolean isAlive = true;
+            boolean alive = true;
 
-            while(!st.isEmpty() && st.peek() > 0 && a < 0){
+            while(alive && num < 0 && !st.isEmpty() && st.peek() > 0){
 
-                if(st.peek() < -a){
-                    st.pop();
-                }
-                else if(st.peek() == -a){
-                    st.pop();
-                    isAlive = false;
+                int top = st.peek();
+
+                if(top > -num){
+                    alive = false;
                     break;
                 }
-                else{
-                    isAlive = false;
+                else if(top < -num){
+                    st.pop();
+                }
+                else if(top == -num){
+                    st.pop();
+                    alive = false;
                     break;
                 }
             }
 
-            if(isAlive) st.add(a);
+            if(alive) st.add(num);
         }
 
         int[] res = new int[st.size()];
-        
+
         for(int i = 0; i < st.size(); i++){
             res[i] = st.get(i);
         }
 
         return res;
+        
     }
 }
