fe33578c2eafc9333d3b4382e62a14f2e3f18a97
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-04 23:52:37 -0500

    Time: 6 ms (69.44%) | Memory: 49.7 MB (37.51%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -1,27 +1,31 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
 
-        int total = 0;
+        //if(totalSum % 3 == 0) return totalSum
+        //if(totalSum % 3 == 1) return totalSum - smallest_one
+        //if(totalSum % 3 == 2) return totalSum - smallest_two
+
+        int totalSum = 0;
         float smallest_one = Integer.MAX_VALUE;
         float smallest_two = Integer.MAX_VALUE;
 
         for(int num : nums){
-            total += num;
+
+            totalSum += num;
 
             if(num % 3 == 1){
-                smallest_two = Math.min(smallest_two, num + smallest_one);
+                smallest_two = Math.min(smallest_two, smallest_one + num);
                 smallest_one = Math.min(smallest_one, num);
             }
-
-            if(num % 3 == 2){
-                smallest_one = Math.min(smallest_one, smallest_two + num);
+            else if(num % 3 == 2){
+                smallest_one = Math.min(smallest_one, num + smallest_two);
                 smallest_two = Math.min(smallest_two, num);
             }
         }
 
-        if(total%3 == 0) return total;
-        if(total%3 == 1) return total - (int)smallest_one;
-        if(total%3 == 2) return total - (int)smallest_two;
+        if(totalSum % 3 == 0) return totalSum;
+        if(totalSum % 3 == 1) return totalSum - (int)smallest_one;
+        if(totalSum % 3 == 2) return totalSum - (int)smallest_two;
 
         return 0;
     }
