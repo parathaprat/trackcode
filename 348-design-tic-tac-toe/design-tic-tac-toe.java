d9d6d7e9395281d5d89af7e01727b8debe0fd105
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-04 22:50:07 -0500

    Time: 3 ms (99.91%) | Memory: 47.8 MB (14.07%) - LeetSync

--- 348-design-tic-tac-toe/design-tic-tac-toe.java
+++ 348-design-tic-tac-toe/design-tic-tac-toe.java
@@ -1,40 +1,40 @@
 class TicTacToe {
 
+    //maintain arrays for row count, col count, diag count and antidiag count
+
     int[] rowCount;
     int[] colCount;
-    int diagCount;
-    int antiDiagCount;
+    int diag;
+    int antiDiag;
     int n;
 
     public TicTacToe(int n) {
         this.n = n;
         rowCount = new int[n];
         colCount = new int[n];
-        diagCount = 0;
-        antiDiagCount = 0;
+        diag = 0;
+        antiDiag = 0;
     }
     
     public int move(int row, int col, int player) {
-
+        
         int add = player == 1 ? 1 : -1;
 
         rowCount[row] += add;
         colCount[col] += add;
-
-        if(row == col) diagCount += add;
-        if(row + col == n - 1) antiDiagCount += add;
+        if(row == col) diag += add;
+        if(row + col == n - 1) antiDiag += add;
 
         if(
             Math.abs(rowCount[row]) == n || 
             Math.abs(colCount[col]) == n ||
-            Math.abs(diagCount) == n || 
-            Math.abs(antiDiagCount) == n
+            Math.abs(diag) == n ||
+            Math.abs(antiDiag) == n
         ){
             return player;
         }
 
         return 0;
-        
     }
 }
 
