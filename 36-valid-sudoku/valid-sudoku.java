e78751195a05ff6b91f40d7f6861ce0a759027d1
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-10 01:04:47 -0500

    Time: 12 ms (17.38%) | Memory: 52.6 MB (6.01%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        
+
         Set<String> set = new HashSet<>();
 
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
 
                 if(board[i][j] != '.'){
 
-                    if(
-                        !set.add(board[i][j] + "row" + i) ||
-                        !set.add(board[i][j] + "col" + j) ||
-                        !set.add(board[i][j] + "block" + i/3 + j/3)
-                    )
-
-                    return false;
+                    if(!set.add("row" + i + board[i][j]) ||
+                    !set.add("col" + j + board[i][j]) || 
+                    !set.add("block" + i/3 + j/3 + board[i][j])){
+                        return false;
+                    }
                 }
             }
         }
