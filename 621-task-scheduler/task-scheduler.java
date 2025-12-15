e364ad8283b77e4217abb16adcd6733358c85c1e
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-15 16:21:34 -0500

    Time: 22 ms (55.12%) | Memory: 47.9 MB (67.92%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,39 +1,37 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a); //CPU sim, highest freq first
+
+        Queue<int[]> q = new LinkedList<>(); //cooldown q (task, time executable)
 
         int[] freq = new int[26];
 
         for(char c : tasks){
             freq[c - 'A']++;
         }
-
+        
         for(int fre : freq){
-            if(fre != 0) maxHeap.offer(fre);
+            if(fre != 0) pq.add(fre);
         }
-
-        Queue<int[]> q = new LinkedList<>();
 
         int time = 0;
 
-        while(!q.isEmpty() || !maxHeap.isEmpty()){
+        while(!q.isEmpty() || !pq.isEmpty()){
 
-            if(maxHeap.isEmpty()){
+            if(pq.isEmpty()){
                 time = q.peek()[1];
             }
 
-            if(!maxHeap.isEmpty()){
+            if(!pq.isEmpty()){
 
-                int task = maxHeap.poll();
-                task-= 1;
+                int task = pq.poll();
+                task -= 1;
                 if(task > 0) q.add(new int[]{task, time + n});
             }
 
             if(!q.isEmpty() && time == q.peek()[1]){
-
-                maxHeap.offer(q.poll()[0]);
+                pq.add(q.poll()[0]);
             }
 
             time++;
