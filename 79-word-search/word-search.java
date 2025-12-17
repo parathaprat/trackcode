e8a372a840bc49c9c5195d45b42809ad679542ef
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-16 21:57:01 -0500

    Time: 201 ms (22.37%) | Memory: 42.9 MB (48.06%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -7,7 +7,7 @@ class Solution {
         boolean[][] visited = new boolean[board.length][board[0].length];
 
         for(int i = 0; i < board.length; i++){
-            for(int j  = 0; j < board[0].length; j++){
+            for(int j = 0; j < board[0].length; j++){
 
                 if(dfs(i, j, 0, word, board, visited)) return true;
             }
@@ -35,7 +35,6 @@ class Solution {
         }
 
         visited[i][j] = false;
-
         return false;
     }
 }
