272ba2e457537820fa0231eb35d3ca351c58e818
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-17 00:50:37 -0500

    Time: 9 ms (25.18%) | Memory: 65 MB (69.91%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,16 +1,15 @@
 class Solution {
-    
     public List<List<String>> partition(String s) {
 
         List<List<String>> res = new ArrayList<>();
         List<String> list = new ArrayList<>();
 
-        dfs(res, list, 0, s);
+        backtrack(res, list, 0, s);
 
         return res;
     }
 
-    private void dfs(List<List<String>> res, List<String> list, int in, String s){
+    private void backtrack(List<List<String>> res, List<String> list, int in, String s){
 
         if(in >= s.length()){
             res.add(new ArrayList<>(list));
@@ -19,20 +18,26 @@ class Solution {
 
         for(int i = in; i < s.length(); i++){
 
-            if(isPalindrome(s, in, i)){
+            if(isPalindrome(s.substring(in, i + 1))){
+
                 list.add(s.substring(in, i + 1));
-                dfs(res, list, i + 1, s);
+                backtrack(res, list, i + 1, s);
                 list.remove(list.size() - 1);
             }
         }
     }
 
-    boolean isPalindrome(String s, int start, int end){
+    private boolean isPalindrome(String s){
 
-        while(start < end){
-            if(s.charAt(start) != s.charAt(end)) return false;
-            start++;
-            end--;
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+
+            if(s.charAt(left) != s.charAt(right)) return false;
+
+            left++;
+            right--;
         }
 
         return true;
