cb9ce50ac45dbddb03c6ba67a9c7e284a973c998
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-20 21:08:31 -0500

    Time: 8 ms (28.30%) | Memory: 47 MB (21.90%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,55 +1,39 @@
 class Solution {
-
-    int[] parent;
-    int[] rank;
-
     public int countComponents(int n, int[][] edges) {
-        
-        parent = new int[n];
-        rank = new int[n];
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
 
         for(int i = 0; i < n; i++){
-            parent[i] = i;
-            rank[i] = 1;
+            map.put(i, new ArrayList<>());
         }
 
-        int count = n;
-
         for(int[] e : edges){
-            if(union(e[0], e[1])) count--;
+            map.get(e[0]).add(e[1]);
+            map.get(e[1]).add(e[0]);
+        }
+
+        Set<Integer> vis = new HashSet<>();
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+
+            if(!vis.contains(i)){
+                dfs(i, map, vis);
+                count++;
+            }
         }
 
         return count;
     }
 
-    private boolean union(int u, int v){
+    private void dfs(int node, Map<Integer, List<Integer>> map, Set<Integer> vis){
 
-        int pu = find(u);
-        int pv = find(v);
+        if(vis.contains(node)) return;
 
-        if(pu == pv) return false;
+        vis.add(node);
 
-        if(rank[pv] > rank[pu]){
-            int temp = pu;
-            pu = pv;
-            pv = temp;
+        for(int nei : map.get(node)){
+            dfs(nei, map, vis);
         }
-
-        parent[pv] = pu;
-        rank[pu] += rank[pv];
-
-        return true;
-    }
-
-    private int find(int node){
-        
-        int cur = node;
-
-        while(cur != parent[cur]){
-            parent[cur] = parent[parent[cur]];
-            cur = parent[cur];
-        }
-
-        return cur;
     }
 }
