59f89c26d2da2466ed127697c8790e690d1314f7
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2025-12-25 13:48:25 -0500

    Time: 0 ms (100.00%) | Memory: 43 MB (14.87%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,8 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
 
-        //2 dp arrays, dp1 skips first, dp2 skips last, return max
-
         if(nums.length == 0) return 0;
         if(nums.length == 1) return nums[0];
         if(nums.length == 2) return Math.max(nums[0], nums[1]);
@@ -19,11 +17,11 @@ class Solution {
         int[] dp2 = new int[nums.length - 1];
         dp2[0] = nums[0];
         dp2[1] = Math.max(nums[0], nums[1]);
-        
+
         for(int i = 2; i < nums.length - 1; i++){
             dp2[i] = Math.max(dp2[i - 2] + nums[i], dp2[i - 1]);
         }
 
-        return Math.max(dp1[dp1.length - 1], dp2[dp2.length - 1]); 
+        return Math.max(dp1[dp1.length - 1], dp2[dp2.length - 1]);
     }
 }
