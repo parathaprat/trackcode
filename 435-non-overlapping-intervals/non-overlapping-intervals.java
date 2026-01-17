c87e757bbb0cf3e07dd47f4168be6b9d533020fb
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-01-17 07:19:00 -0500

    Time: 46 ms (70.56%) | Memory: 115.9 MB (45.54%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -4,9 +4,9 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
 
         int[] in = intervals[0];
-        int res = 0;
 
         int i = 1;
+        int res = 0;
 
         while(i < intervals.length){
 
@@ -17,7 +17,6 @@ class Solution {
             }
 
             in = intervals[i];
-            
             i++;
         }
 
