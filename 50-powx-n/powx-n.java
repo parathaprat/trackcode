5d6fe42ac666deed5b9cc4ddbc3a153e94097895
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-01-29 14:52:55 -0500

    Time: 0 ms (100.00%) | Memory: 48 MB (53.70%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,8 +1,8 @@
 class Solution {
     public double myPow(double x, int n) {
 
-        if(n == 0) return 1;
         if(x == 0) return 0;
+        if(n == 0) return 1;
 
         double ans = getAns(x, Math.abs((long)n));
 
