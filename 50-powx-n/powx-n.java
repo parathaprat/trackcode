2e5f8448e44bcd237d6d609010a3c6c66919c6f7
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-01-29 14:41:37 -0500

    Time: 0 ms (100.00%) | Memory: 47.9 MB (61.36%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -6,15 +6,15 @@ class Solution {
 
         double ans = getAns(x, Math.abs((long)n));
 
-        return n < 0 ? 1/ans : ans;
+        return n < 0 ? 1 / ans : ans;
     }
 
     private double getAns(double x, long n){
 
         if(n == 1) return x;
 
-        double half = getAns(x, n/2);
+        double half = getAns(x, n / 2);
 
-        return n%2 == 0? half*half : half*x*half;
+        return n % 2 == 0 ? half * half : half * x * half;
     }
 }
