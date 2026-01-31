22a304ee13d6ae3540205ca46aad029c484b3a1d
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-01-31 18:42:14 -0500

    Time: 11 ms (94.81%) | Memory: 47.6 MB (61.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,28 +1,41 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
+        //freq map 
+        //buckets representing freqs
+        //return top k from the back 
+
         Map<Integer, Integer> map = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
 
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for(int num : map.keySet()){
-            pq.add(num);
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
 
-            if(pq.size() > k) pq.poll();
+        for(int num : map.keySet()){
+
+            int freq = map.get(num);
+
+            if(buckets[freq] == null) buckets[freq] = new ArrayList<>();
+            buckets[freq].add(num);
         }
 
         int[] ans = new int[k];
+        int index = 0;
 
-        int i = 0;
-        while(!pq.isEmpty()){
-            ans[i] = pq.poll();
-            i++;
+        for(int i = buckets.length - 1; i >= 0; i--){
+
+            if(buckets[i] != null){
+
+                for(int num : buckets[i]){
+                    ans[index] = num;
+                    index++;
+                    if(index == k) return ans;
+                }
+            }
         }
 
         return ans;
-        
     }
 }
