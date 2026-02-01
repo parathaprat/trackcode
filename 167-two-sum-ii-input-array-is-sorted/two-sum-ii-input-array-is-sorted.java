17d8043317c99127349e8705188b1ed3c4b59332
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-01-31 20:47:33 -0500

    Time: 2 ms (96.28%) | Memory: 48.4 MB (54.73%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -4,7 +4,7 @@ class Solution {
         int left = 0;
         int right = numbers.length - 1;
 
-        while(left < right){
+        while(left <= right){
 
             if(numbers[left] + numbers[right] == target) return new int[]{left + 1, right + 1};
             else if(numbers[left] + numbers[right] < target) left++;
