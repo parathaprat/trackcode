255abd51bbb2b85696f4ad4d7c1fbbc039f90c94
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-03-21 01:52:35 -0400

    Time: 10 ms (84.19%) | Memory: 46.7 MB (88.49%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,14 +1,7 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        
-        //create a window of len = p.len
-        //move thru, create int array, create string key
-        //if key matches return index start of sliding window
 
-        //for sliding window, use pointers l, r; remove char(r) and add char(l)
-
-
-        int l = 0; 
+        int l = 0;
         int r = p.length() - 1;
 
         int[] pfreq = new int[26];
@@ -16,39 +9,31 @@ class Solution {
 
         List<Integer> result = new ArrayList<>();
 
-        //populate freq array for p
         for(char c : p.toCharArray()){
-
             pfreq[c - 'a']++;
-
         }
-
+        
         while(r < s.length()){
 
             if(l == 0){
                 for(int i = l; i <= r; i++){
-                    
                     sfreq[s.charAt(i) - 'a']++;
-
                 }
 
                 l++;
                 r++;
             }
             else{
-
                 sfreq[s.charAt(l - 1) - 'a']--;
-   
                 sfreq[s.charAt(r) - 'a']++;
 
                 l++;
                 r++;
             }
 
-            if(Arrays.equals(sfreq, pfreq)) result.add(l-1);
+            if(Arrays.equals(sfreq, pfreq)) result.add(l - 1);
         }
 
         return result;
-
     }
 }
