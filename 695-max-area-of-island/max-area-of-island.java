c6f514c078eaf89c7bafa5d83355dd325664d6d2
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-04-10 17:12:59 -0400

    Time: 1 ms (100.00%) | Memory: 46.6 MB (40.32%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -19,14 +19,14 @@ class Solution {
 
     private int dfs(int i, int j, int[][] grid){
 
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) return 0;
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1)return 0;
 
         grid[i][j] = 0;
 
         return 1 + 
-        dfs(i + 1, j, grid) + 
+        dfs(i + 1, j, grid) +
         dfs(i - 1, j, grid) + 
-        dfs(i, j + 1, grid) + 
-        dfs(i, j - 1, grid); 
+        dfs(i, j + 1, grid) +
+        dfs(i, j - 1, grid);
     }
 }
