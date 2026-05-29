91917c2438660f84aaaacf73cd2879f9c571000b
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-05-29 19:18:39 -0400

    Time: 15 ms (55.06%) | Memory: 47.7 MB (31.50%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,39 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        //bucket sort
-        //maxHeap, custom comp
-
-        //create a freq map
         Map<Integer, Integer> map = new HashMap<>();
-        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
 
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         for(int num : map.keySet()){
-
-            int freq = map.get(num);
-
-            if(buckets[freq] == null) buckets[freq] = new ArrayList<>();
-            buckets[freq].add(num);
+            maxHeap.add(num);
         }
 
         int[] ans = new int[k];
-
         int index = 0;
 
-        for(int i = buckets.length - 1; i >= 0; i--){
-
-            if(buckets[i] != null){
-                
-                for(int num : buckets[i]){
-                    ans[index] = num;
-                    index++;
-                    if(index == k) return ans;
-                }
-            }
+        while(index < k){
+            ans[index] = maxHeap.poll();
+            index++;
         }
 
         return ans;
