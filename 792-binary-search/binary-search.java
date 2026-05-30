f7fa14b18b34b24074920d465799db42ffa770b3
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-05-30 00:22:20 -0400

    Time: 0 ms (100.00%) | Memory: 48.6 MB (13.62%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -6,17 +6,18 @@ class Solution {
 
         while(left <= right){
 
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left)/2;
 
             if(nums[mid] == target){
                 return mid;
             }
-            else if(nums[mid] < target){
-                left = mid + 1;
+            else if(nums[mid] > target){
+                right = mid - 1;
             }
-            else right = mid - 1;
+            else left = mid + 1;
         }
 
-        return - 1;
+        return -1;
+        
     }
 }
