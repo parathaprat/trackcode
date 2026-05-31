37c4ac90eed42c3b286ef313b8704988fe96dd17
Prathamesh Dongritot <84290855+parathaprat@users.noreply.github.com>
2026-05-31 19:25:39 -0400

    Time: 18 ms (25.30%) | Memory: 44.2 MB (11.21%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        HashMap<Character, Integer> smap = new HashMap<>();
+        HashMap<Character, Integer> tmap = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+
+            if(!smap.containsKey(s.charAt(i))){
+                smap.put(s.charAt(i), i);
+            }
+
+            if(!tmap.containsKey(t.charAt(i))){
+                tmap.put(t.charAt(i), i);
+            }
+
+            if(!smap.get(s.charAt(i)).equals(tmap.get(t.charAt(i)))) return false;
+        }
+
+        return true;
+        
+    }
+}
